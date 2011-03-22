a240d5a18975b7a08df6d2b9b83e0de154f1e9c3
buckyballreaction <buckyballreaction@gmail.com>
2011-03-21 22:51:07 -0600

    fix botzone buffers for clockwise-winding polywalls

---
+++ master/schema/bitfighter.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 12, 2011 at 05:49 PM
+-- Server version: 5.1.49
+-- PHP Version: 5.3.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `bitfighter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `server`
+--
+
+CREATE TABLE IF NOT EXISTS `server` (
+  `server_id` int(11) NOT NULL AUTO_INCREMENT,
+  `server_name` text COLLATE utf8_unicode_ci,
+  `ip_address` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`server_id`),
+  UNIQUE KEY `name_ip_unique` (`server_name`(50),`ip_address`(15))
+)  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `server`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stats_game`
+--
+
+CREATE TABLE IF NOT EXISTS `stats_game` (
+  `stats_game_id` int(11) NOT NULL AUTO_INCREMENT,
+  `server_id` int(11) NOT NULL,
+  `game_type` text COLLATE utf8_unicode_ci NOT NULL,
+  `is_official` tinyint(1) NOT NULL,
+  `player_count` smallint(5) unsigned NOT NULL,
+  `duration_seconds` smallint(5) unsigned NOT NULL,
+  `level_name` text COLLATE utf8_unicode_ci NOT NULL,
+  `is_team_game` tinyint(1) NOT NULL,
+  `team_count` tinyint(3) unsigned DEFAULT NULL,
+  `insertion_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`stats_game_id`),
+  KEY `server_id` (`server_id`),
+  KEY `is_official` (`is_official`),
+  KEY `player_count` (`player_count`),
+  KEY `is_team_game` (`is_team_game`),
+  KEY `team_count` (`team_count`),
+  KEY `insertion_date` (`insertion_date`),
+  KEY `game_type` (`game_type`(20))
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `stats_game`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stats_player`
+--
+
+CREATE TABLE IF NOT EXISTS `stats_player` (
+  `stats_player_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stats_game_id` int(11) NOT NULL,
+  `player_name` text COLLATE utf8_unicode_ci NOT NULL,
+  `is_authenticated` tinyint(1) NOT NULL,
+  `is_robot` tinyint(1) NOT NULL,
+  `result` char(1) COLLATE utf8_unicode_ci NOT NULL,
+  `points` int(11) NOT NULL,
+  `kill_count` smallint(5) unsigned NOT NULL,
+  `death_count` smallint(5) unsigned NOT NULL,
+  `suicide_count` smallint(5) unsigned NOT NULL,
+  `switched_team_count` smallint(5) unsigned DEFAULT NULL,
+  `stats_team_id` int(11) DEFAULT NULL,
+  `insertion_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`stats_player_id`),
+  KEY `is_authenticated` (`is_authenticated`),
+  KEY `result` (`result`),
+  KEY `stats_team_id` (`stats_team_id`),
+  KEY `insertion_date` (`insertion_date`),
+  KEY `player_name` (`player_name`(50)),
+  KEY `is_robot` (`is_robot`),
+  KEY `stats_game_id` (`stats_game_id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `stats_player`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stats_player_shots`
+--
+
+CREATE TABLE IF NOT EXISTS `stats_player_shots` (
+  `stats_player_shots_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stats_player_id` int(11) NOT NULL,
+  `weapon` text COLLATE utf8_unicode_ci NOT NULL,
+  `shots` int(11) NOT NULL,
+  `shots_struck` int(11) NOT NULL,
+  PRIMARY KEY (`stats_player_shots_id`),
+  UNIQUE KEY `stats_player_id` (`stats_player_id`,`weapon`(10)),
+  KEY `weapon` (`weapon`(10))
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `stats_player_shots`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stats_team`
+--
+
+CREATE TABLE IF NOT EXISTS `stats_team` (
+  `stats_team_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stats_game_id` int(11) NOT NULL,
+  `team_name` text COLLATE utf8_unicode_ci,
+  `color_hex` text COLLATE utf8_unicode_ci NOT NULL,
+  `team_score` int(11) DEFAULT NULL,
+  `result` char(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `insertion_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`stats_team_id`),
+  KEY `stats_game_id` (`stats_game_id`),
+  KEY `result` (`result`),
+  KEY `insertion_date` (`insertion_date`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `stats_team`
+--
+
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `stats_game`
+--
+ALTER TABLE `stats_game`
+  ADD CONSTRAINT `stats_game_ibfk_1` FOREIGN KEY (`server_id`) REFERENCES `server` (`server_id`);
+
+--
+-- Constraints for table `stats_player`
+--
+ALTER TABLE `stats_player`
+  ADD CONSTRAINT `stats_player_ibfk_2` FOREIGN KEY (`stats_team_id`) REFERENCES `stats_team` (`stats_team_id`),
+  ADD CONSTRAINT `stats_player_ibfk_3` FOREIGN KEY (`stats_game_id`) REFERENCES `stats_game` (`stats_game_id`);
+
+--
+-- Constraints for table `stats_player_shots`
+--
+ALTER TABLE `stats_player_shots`
+  ADD CONSTRAINT `stats_player_shots_ibfk_1` FOREIGN KEY (`stats_player_id`) REFERENCES `stats_player` (`stats_player_id`);
+
+--
+-- Constraints for table `stats_team`
+--
+ALTER TABLE `stats_team`
+  ADD CONSTRAINT `stats_team_ibfk_1` FOREIGN KEY (`stats_game_id`) REFERENCES `stats_game` (`stats_game_id`);
---
+++ zap/barrier.h
@@ -0,0 +1,112 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _BARRIER_H_
+#define _BARRIER_H_
+
+#include "gameObject.h"
+#include "point.h"
+#include "tnlNetObject.h"
+
+namespace Zap
+{
+
+/// The Barrier class represents rectangular barriers that player controlled
+/// ships cannot pass through... i.e. walls  Barrier objects, once created, never
+/// change state, simplifying the pack/unpack update methods.  Barriers are
+/// constructed as an expanded line segment.
+class Barrier : public GameObject
+{
+public:
+   Vector<Point> mPoints; ///< The points of the barrier --> if only two, first will be start, second end of an old-school segment
+
+   bool mSolid;
+   // By precomputing and storing, we should ease the rendering cost
+   Vector<Point> mRenderFillGeometry; ///< Actual geometry used for rendering fill.
+   Vector<Point> mRenderOutlineGeometry; ///< Actual geometry used for rendering outline.
+   Vector<Point> mBotZoneBufferGeometry; ///< Geometry used for rendering the botzones.
+
+   F32 mWidth;
+
+   // MAX / MIN width only used to limit editor.
+   static const S32 MIN_BARRIER_WIDTH = 0;
+   static const S32 MAX_BARRIER_WIDTH = 2500;      // Geowar has walls at least 350 units wide, so going lower will break at least one level
+
+   static const S32 BarrierWidth = 50; ///< The default width of the barrier in game units
+
+   Vector<Point> mRenderLineSegments; ///< The clipped line segments representing this barrier.
+   Vector<Point> mBotZoneBufferLineSegments; ///< The line segments representing a buffered barrier.
+
+   /// Barrier constructor
+   Barrier(const Vector<Point> &points = Vector<Point>(), F32 width = BarrierWidth, bool solid = false);
+
+   /// Adds the server object to the net interface's scope always list
+   void onAddedToGame(Game *theGame);
+
+   /// renders this barrier by drawing the render line segments,
+   void render(S32 layer);
+
+   /// returns a sorting key for the object.  Barriers should sort behind other objects
+   S32 getRenderSortValue() { return 0; }
+
+   /// returns the collision polygon of this barrier, which is the boundary extruded from the start,end line segment.
+   bool getCollisionPoly(Vector<Point> &polyPoints);
+
+   /// collide always returns true for Barrier objects.
+   bool collide(GameObject *otherObject) { return true; }
+
+   // Takes a list of vertices and converts them into a list of lines representing the edges of an object
+   static void resetEdges(const Vector<Point> &corners, Vector<Point> &edges);
+
+   // Simply takes a segment and "puffs it out" to a rectangle of a specified width, filling cornerPoints.  Does not modify endpoints.
+   static void expandCenterlineToOutline(const Point &start, const Point &end, F32 width, Vector<Point> &cornerPoints);
+
+   // Takes a segment and "puffs it out" to a rectangle for bot zone generation.
+   // This rectangle is the width of the barrier plus the ship's collision radius added to the outside
+   static void bufferBarrierForBotZone(const Point &start, const Point &end, F32 barrierWidth, Vector<Point> &bufferedPoints);
+   static void bufferPolyWallForBotZone(const Vector<Point> &inputPoints, Vector<Point> &bufferedPoints);
+
+   /// clips the current set of render lines against the polygon passed as polyPoints, modifies lineSegmentPoints.
+   static void clipRenderLinesToPoly(const Vector<Point> &polyPoints, Vector<Point> &lineSegmentPoints);
+
+   static void constructBarrierEndPoints(const Vector<Point> &vec, F32 width, Vector<Point> &barrierEnds);
+
+   // Clean up edge geometry and get barriers ready for proper rendering
+   void prepareRenderingGeometry();       // orig
+   //void prepareRenderingGeometry2();      // sam's
+   
+   // Create geometry for botzones - adds a buffer around barriers
+   void prepareBotZoneGeometry();
+
+   void prepareRenderGeom(Vector<Point> &outlines, Vector<Point> &segs);
+
+
+   TNL_DECLARE_CLASS(Barrier);
+};
+
+};
+
+#endif
+
