65fef80c9b327f932e4bc2856312700d2f6fa02a
buckyballreaction <buckyballreaction@gmail.com>
2011-06-08 11:05:08 -0600

    Remove all mention of GLUT headers.  Also add SDL headers.  NOTE:  this code is extremly broken

---
+++ zap/UIMenuItems.h
@@ -0,0 +1,375 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _MENU_ITEMS_H_
+#define _MENU_ITEMS_H_
+
+#include "keyCode.h"       // For keyCodes!
+#include "teamInfo.h"      // For Team def
+#include "Color.h"
+#include "Colors.h"
+#include "stringUtils.h"   // For itos
+#include <string>
+
+
+namespace Zap
+{
+
+using namespace std;
+
+enum MenuItemTypes {
+   MenuItemType,
+   ToggleMenuItemType,
+   CounterMenuItemType,
+   TimeCounterMenuItemType,
+   EditableMenuItemType,   
+   PlayerMenuItemType,
+   TeamMenuItemType
+};
+
+enum PlayerType {
+   PlayerTypePlayer,
+   PlayerTypeAdmin,
+   PlayerTypeRobot,
+   PlayerTypeIrrelevant
+};
+
+////////////////////////////////////
+////////////////////////////////////
+
+class MenuItem
+{
+private:
+   string mPrompt;     // Text displayed on menu
+   string mHelp;       // An optional help string
+   S32 mIndex;
+
+protected:
+   Color mSelectedColor;
+   Color mUnselectedColor;
+
+   bool mEnterAdvancesItem;
+   void (*mCallback)(U32);
+
+   const char *mPromptAppendage;
+
+public:
+   MenuItem(); // Default constructor
+
+   // Constructor
+   MenuItem(S32 index, const string &prompt, void (*callback)(U32), const string &help, KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+
+   KeyCode key1;     // Allow two shortcut keys per menu item...
+   KeyCode key2;
+
+   virtual MenuItemTypes getItemType() { return MenuItemType; }
+
+   virtual void render(S32 ypos, S32 textsize, bool isSelected);              // Renders item horizontally centered on screen
+   virtual void render(S32 xpos, S32 ypos, S32 textsize, bool isSelected);    // Renders item horizontally centered on xpos
+
+   const Color *getColor(bool isSelected);
+
+   const char *getHelp() { return mHelp.c_str(); }
+   S32 getIndex() { return mIndex; }
+
+   virtual string getPrompt() { return mPrompt; }
+   virtual string getValue() { return mPrompt; } 
+
+   virtual void setSecret(bool secret) { /* Do nothing */ }
+
+   // When enter is pressed, should selection advance to the next item?
+   virtual void setEnterAdvancesItem(bool enterAdvancesItem) { mEnterAdvancesItem = enterAdvancesItem; }
+   
+   virtual const char *getSpecialEditingInstructions() { return ""; }
+   virtual string getValueForDisplayingInMenu() { return ""; }
+   virtual S32 getIntValue() { return 0; }
+   virtual string getValueForWritingToLevelFile() { return itos(getIntValue()); }
+   virtual void setValue(const string &val) { /* Do nothing */ }
+   virtual void setIntValue(S32 val) { /* Do nothing */ }
+
+   virtual bool handleKey(KeyCode keyCode, char ascii);
+   virtual void setFilter(LineEditor::LineEditorFilter filter) { /* Do nothing */ }
+   virtual void activatedWithShortcutKey() { handleKey(MOUSE_LEFT, 0); }
+
+   virtual bool enterAdvancesItem() { return mEnterAdvancesItem; }      
+
+   void setSelectedColor(const Color &color)   { mSelectedColor = color; }
+   void setUnselectedColor(const Color &color) { mUnselectedColor = color; }
+   virtual void setSelectedValueColor(const Color &color)   { /* Override in children */ }
+   virtual void setUnselectedValueColor(const Color &color) { /* Override in children */ }
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class MessageMenuItem : public MenuItem
+{
+public:
+   MessageMenuItem(string title, const Color &color) : MenuItem(-1, title, NULL, "")  
+   { 
+      mPromptAppendage = ""; 
+      mUnselectedColor = color; 
+   }
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Parent class for all things that have both a name and a value, i.e. anything that's not a regular menuItem
+// Provides some additional functionality
+class ValueMenuItem : public MenuItem
+{
+protected:
+   Color mSelectedValueColor;       // Color of value when selected
+   Color mUnselectedValueColor;     // Color of value when unselected
+
+   const Color *getValueColor(bool isSelected) { return isSelected ? &mSelectedValueColor : &mUnselectedValueColor; }
+   void setSelectedValueColor(const Color &color)   { mSelectedValueColor = color; }
+   void setUnselectedValueColor(const Color &color) { mUnselectedValueColor = color; }
+
+public:
+   ValueMenuItem(S32 index, const string &text, void (*callback)(U32), const string &help, KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class ToggleMenuItem : public ValueMenuItem
+{
+
+protected:
+   string mValue;
+   U32 mIndex;
+   bool mWrap;
+
+
+public:
+   ToggleMenuItem(string title, Vector<string> options, U32 currOption, bool wrap, void (*callback)(U32), string help, KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+
+   virtual MenuItemTypes getItemType() { return ToggleMenuItemType; }
+   virtual string getValueForDisplayingInMenu() { return mValue; }
+   virtual const char *getSpecialEditingInstructions() { return "Use [<-] and [->] keys to change value."; }
+   virtual S32 getValueIndex() { return mIndex; }
+   virtual void setValueIndex(U32 index) { mIndex = index; }
+   
+   virtual string getValue() { return mOptions[mIndex]; } 
+
+   virtual void render(S32 xpos, S32 ypos, S32 textsize, bool isSelected);
+   virtual bool handleKey(KeyCode keyCode, char ascii);
+
+   virtual void activatedWithShortcutKey() { /* Do nothing */ }
+
+   Vector<string> mOptions;
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class YesNoMenuItem : public ToggleMenuItem
+{
+public:
+   YesNoMenuItem(string title, bool currOption, void (*callback)(U32), string help, KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+
+   virtual string getValueForDisplayingInMenu() { return mIndex ? " Engineer" : ""; }
+   virtual string getValueForWritingToLevelFile() { return mIndex ? "yes" : "no"; }
+   virtual void setValue(const string &val) { mIndex = (val == "yes") ? 1 : 0; }
+   virtual S32 getIntValue() { return mIndex; }
+   virtual void setIntValue(S32 value) { mIndex = (value == 0) ? 0 : 1; }
+};
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class CounterMenuItem : public ValueMenuItem
+{
+protected:
+   S32 mValue;
+   S32 mStep;
+   S32 mMinValue;
+   S32 mMaxValue;
+   string mUnits;
+   string mMinMsg;
+
+   virtual void increment(S32 fact = 1); 
+   virtual void decrement(S32 fact = 1);
+   virtual S32 getBigIncrement() { return 10; }    // How much our counter is incremented when shift is down (multiplier)
+
+public:
+   CounterMenuItem(const string &title, S32 value, S32 step, S32 minVal, S32 maxVal, const string &units, const string &minMsg, const string &help, 
+                   KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+
+   virtual void render(S32 xpos, S32 ypos, S32 textsize, bool isSelected);
+
+   virtual MenuItemTypes getItemType() { return CounterMenuItemType; }
+   virtual string getValueForDisplayingInMenu() { return itos(mValue); }
+   virtual const char *getUnits() { return mUnits.c_str(); }
+   virtual S32 getIntValue() { return mValue; }
+   virtual void setValue(const string &val) { mValue = atoi(val.c_str()); }
+   virtual string getValue() { return itos(mValue); }
+   virtual void setIntValue(S32 val) { mValue = val; }
+   virtual const char *getSpecialEditingInstructions() { return "Use [<-] and [->] keys to change value.  Use [Shift] for bigger change."; }
+   virtual bool handleKey(KeyCode keyCode, char ascii);
+
+   virtual void snap() { /* Do nothing */ }
+
+   virtual void activatedWithShortcutKey() { /* Do nothing */ }
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class TimeCounterMenuItem : public CounterMenuItem
+{
+protected:
+   virtual S32 getBigIncrement() { return 12; }    // 12 * 5sec = 1 minute
+
+public:
+   TimeCounterMenuItem(const string &title, S32 value, S32 maxVal, const string &zeroMsg, const string &help, 
+                       S32 step = 5, KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+
+   virtual const char *getUnits() { return mValue >= 60 ? "mins" : "secs"; }
+
+   virtual MenuItemTypes getItemType() { return TimeCounterMenuItemType; }
+   virtual void setValue (const string &val) { mValue = S32((atof(val.c_str()) * 60 + 2.5) / 5) * 5 ; }     // Snap to nearest 5 second interval
+   virtual string getValueForDisplayingInMenu() { return (mValue < 60) ? itos(mValue) : 
+                                                   itos(mValue / 60) + ":" + ((mValue % 60) < 10 ? "0" : "") + itos(mValue % 60); }
+   virtual string getValueForWritingToLevelFile() { return ftos((F32)mValue / 60.0f, 3); }
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// As TimeCounterMenuItem, but reporting time in seconds, and with increments of 1 second, rather than 5
+class TimeCounterMenuItemSeconds : public TimeCounterMenuItem
+{
+protected:
+   virtual S32 getBigIncrement() { return 5; }
+
+public:
+   TimeCounterMenuItemSeconds(const string &title, S32 value, S32 maxVal, const string &zeroMsg, const string &help, 
+                   KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+
+   virtual void setValue (const string &val) { mValue = atoi(val.c_str()); } 
+   virtual string getValueForWritingToLevelFile() { return itos(mValue); }
+
+   virtual void snap() { mValue = S32((mValue / getBigIncrement()) * getBigIncrement()); }
+};
+
+////////////////////////////////////
+////////////////////////////////////
+
+class EditableMenuItem : public ValueMenuItem
+{
+private:
+   string mEmptyVal;
+
+protected:
+      LineEditor mLineEditor;
+      void (*mTextEditedCallback)(string);
+
+public:
+   // Contstuctor
+   EditableMenuItem(string title, string val, string emptyVal, string help, U32 maxLen, KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+
+   virtual MenuItemTypes getItemType() { return EditableMenuItemType; }
+   virtual void render(S32 xpos, S32 ypos, S32 textsize, bool isSelected);
+   virtual bool handleKey(KeyCode keyCode, char ascii);
+
+   LineEditor getLineEditor() { return mLineEditor; }
+   void setLineEditor(LineEditor editor) { mLineEditor = editor; }
+
+   virtual string getValueForWritingToLevelFile() { return mLineEditor.getString(); }
+   virtual string getValueForDisplayingInMenu() { return mLineEditor.getString(); }
+
+   virtual string getValue() { return mLineEditor.getString(); } 
+   void setValue(const string &val) { mLineEditor.setString(val); }
+
+   virtual void setFilter(LineEditor::LineEditorFilter filter) { mLineEditor.setFilter(filter); }
+
+   virtual void activatedWithShortcutKey() { /* Do nothing */ }
+
+   virtual void setTextEditedCallback(void (*callback)(string)) { mTextEditedCallback = callback; }
+
+
+   virtual void setSecret(bool secret) { mLineEditor.setSecret(secret); }
+};
+
+////////////////////////////////////
+////////////////////////////////////
+
+class MaskedEditableMenuItem : public EditableMenuItem
+{
+   MaskedEditableMenuItem(string title, string val, string emptyVal, string help, U32 maxLen, KeyCode k1 = KEY_UNKNOWN, KeyCode k2 = KEY_UNKNOWN);
+};
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+class PlayerMenuItem : public MenuItem
+{
+private:
+   PlayerType mType;    // Type of player, for name menu
+
+public:
+   // Constructor
+   PlayerMenuItem(S32 index, const char *text, void (*callback)(U32), KeyCode k1, PlayerType type);
+
+   virtual MenuItemTypes getItemType() { return PlayerMenuItemType; }
+   virtual void render(S32 xpos, S32 ypos, S32 textsize, bool isSelected);
+
+   virtual void activatedWithShortcutKey() { /* Do nothing */ }
+};
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+class TeamMenuItem : public MenuItem
+{
+private:
+   Team mTeam;
+   bool mIsCurrent;     // Is this a player's current team? 
+
+public:
+   TeamMenuItem(S32 index, Team team, void (*callback)(U32), KeyCode keyCode, bool isCurrent);
+
+   virtual MenuItemTypes getItemType() { return TeamMenuItemType; }
+   void render(S32 xpos, S32 ypos, S32 textsize, bool isSelected);
+
+   virtual void activatedWithShortcutKey() { /* Do nothing */ }
+
+};
+
+};
+#endif
+
+
---
+++ zap/textItem.cpp
@@ -0,0 +1,449 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "textItem.h"
+#include "UIEditorMenus.h"       // For TextItemEditorAttributeMenuUI def
+#include "gameNetInterface.h"
+#include "gameObjectRender.h"    // For renderPointVector()
+
+#include <math.h>
+
+namespace Zap
+{
+
+TNL_IMPLEMENT_NETOBJECT(TextItem);
+
+
+EditorAttributeMenuUI *TextItem::mAttributeMenuUI = NULL;
+
+
+// Constructor
+TextItem::TextItem() : SimpleLine(TextItemType)
+{
+   mNetFlags.set(Ghostable);
+   mObjectTypeMask = TextItemType | CommandMapVisType;     
+   mObjectTypeNumber = TextItemTypeNumber;
+
+   // Some default values
+   mSize = 20;
+   mTeam = Item::TEAM_NEUTRAL;
+}
+
+
+// Destructor
+TextItem::~TextItem()
+{
+   // Do nothing
+}
+
+
+void TextItem::newObjectFromDock(F32 gridSize)
+{
+   SimpleLine::newObjectFromDock(gridSize);
+
+   mText = "Your text here";
+   recalcTextSize();
+}
+
+
+// In game rendering
+void TextItem::render()
+{
+   ClientGame *game = dynamic_cast<ClientGame *>(getGame());
+   
+   // Don't render opposing team's text items
+   if(!game || !game->getConnectionToServer() || !game->getGameType())
+      return;
+
+   Ship *ship = dynamic_cast<Ship *>(game->getConnectionToServer()->getControlObject());
+   if( (!ship && mTeam != -1) || (ship && ship->getTeam() != mTeam && mTeam != -1) )
+      return;
+
+   renderTextItem(getVert(0), getVert(1), mSize, mText, game->getTeamColor(mTeam));
+}
+
+
+// Called by SimpleItem::renderEditor()
+void TextItem::renderEditorItem()
+{
+   renderTextItem(getVert(0), getVert(1), mSize, mText, getGame()->getTeamColor(mTeam));
+}
+
+
+// This object should be drawn below others
+S32 TextItem::getRenderSortValue()
+{
+   return 1;
+}
+
+
+// Create objects from parameters stored in level file
+// Entry looks like: TextItem 0 50 10 10 11 11 Message goes here
+bool TextItem::processArguments(S32 argc, const char **argv)
+{
+   if(argc < 7)
+      return false;
+
+   mTeam = atoi(argv[0]);
+
+   Point pos, dir;
+
+   pos.read(argv + 1);
+   pos *= getGame()->getGridSize();
+
+   dir.read(argv + 3);
+   dir *= getGame()->getGridSize();
+
+   setVert(pos, 0);
+   setVert(dir, 1);
+
+   mSize = atof(argv[5]);
+   mSize = max(min(mSize, (F32)MAX_TEXT_SIZE), (F32)MIN_TEXT_SIZE);      // Note that same line exists below, in recalcXXX()... combine?
+
+   // Assemble any remainin args into a string
+   mText = "";
+   for(S32 i = 6; i < argc; i++)
+   {
+      mText += argv[i];
+      if(i < argc - 1)
+         mText += " ";
+   }
+
+   computeExtent();
+
+   return true;
+}
+
+
+string TextItem::toString()
+{
+   F32 gs = getGame()->getGridSize();
+   Point pos = getVert(0) / gs;
+   Point dir = getVert(1) / gs;
+
+   return string(getClassName()) + " " + itos(mTeam) + " " + pos.toString() + " " + dir.toString() + " " + ftos(mSize, 3) + " " + mText;
+}
+
+
+// Editor
+void TextItem::recalcTextSize()
+{
+   const F32 dummyTextSize = 120;
+
+   F32 lineLen = getVert(0).distanceTo(getVert(1));      // In in-game units
+   F32 strWidth = F32(UserInterface::getStringWidth(dummyTextSize, mText.c_str())) / dummyTextSize; 
+   F32 size = lineLen / strWidth;
+
+   // Compute text size subject to min and max defined in TextItem
+   mSize = max(min(size, (F32)MAX_TEXT_SIZE), (F32)MIN_TEXT_SIZE);
+}
+
+
+void TextItem::onAddedToGame(Game *theGame)
+{
+   Parent::onAddedToGame(theGame);
+
+   if(!isGhost())
+      setScopeAlways();
+}
+
+
+// Bounding box for quick collision-possibility elimination, and display scoping purposes
+void TextItem::computeExtent()
+{
+   U32 len = UserInterface::getStringWidth(mSize, mText.c_str());
+   U32 buf = mSize / 2;     // Provides some room to accomodate descenders on letters like j and g.
+
+   F32 angle =  getVert(0).angleTo(getVert(1));
+   F32 sinang = sin(angle);
+   F32 cosang = cos(angle);
+
+   F32 descenderFactor = .35;    // To account for y, g, j, etc.
+   F32 h = mSize * (1 + descenderFactor);
+   F32 w = len * 1.05;           // 1.05 adds just a little horizontal padding for certain words with trailing ys or other letters that are just a tiny bit longer than calculated
+   F32 x = getVert(0).x + mSize * descenderFactor * sinang;
+   F32 y = getVert(0).y + mSize * descenderFactor * cosang;
+
+   F32 c1x = x - h * sinang * .5;
+   F32 c1y = y;
+
+   F32 c2x = x + w * cosang - h * sinang * .5;
+   F32 c2y = y + w * sinang;
+
+   F32 c3x = x + h * sinang * .5 + w * cosang;
+   F32 c3y = y - h * cosang + w * sinang;
+
+   F32 c4x = x + h * sinang * .5;
+   F32 c4y = y - h * cosang;
+
+   F32 minx = min(c1x, min(c2x, min(c3x, c4x)));
+   F32 miny = min(c1y, min(c2y, min(c3y, c4y)));
+   F32 maxx = max(c1x, max(c2x, max(c3x, c4x)));
+   F32 maxy = max(c1y, max(c2y, max(c3y, c4y)));
+
+   Rect extent(Point(minx, miny), Point(maxx, maxy));
+
+   setExtent(extent);
+}
+
+
+bool TextItem::getCollisionPoly(Vector<Point> &polyPoints)
+{
+   return false;
+}
+
+// Handle collisions with a TextItem.  Easy, there are none.
+bool TextItem::collide(GameObject *hitObject)
+{
+   return false;
+}
+
+void TextItem::idle(GameObject::IdleCallPath path)
+{
+   // Laze about, read a book, take a nap, whatever.
+}
+
+U32 TextItem::packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream)
+{
+   Point pos = getVert(0);
+   Point dir = getVert(1);
+
+   pos.write(stream);
+   dir.write(stream);
+
+   stream->writeRangedU32(mSize, 0, MAX_TEXT_SIZE);
+   stream->write(mTeam);
+
+   stream->writeString(mText.c_str(), (U8) mText.length());      // Safe to cast text.length to U8 because we've limited it's length to MAX_TEXTITEM_LEN
+
+   return 0;
+}
+
+
+void TextItem::unpackUpdate(GhostConnection *connection, BitStream *stream)
+{
+   char txt[MAX_TEXTITEM_LEN];
+
+   Point pos, dir;
+
+   pos.read(stream);
+   dir.read(stream);
+
+   setVert(pos, 0);
+   setVert(dir, 1);
+
+   mSize = stream->readRangedU32(0, MAX_TEXT_SIZE);
+   stream->read(&mTeam);
+
+   stream->readString(txt);
+
+   mText = txt;
+   computeExtent();
+}
+
+
+///// Editor Methods
+
+// Runs when text is being changed in the editor
+void TextItem::onAttrsChanging()
+{
+   onGeomChanged();
+}
+
+void TextItem::onAttrsChanged()
+{
+   onGeomChanged();
+}
+
+void TextItem::onGeomChanging()
+{
+   onGeomChanged();
+}
+
+void TextItem::onGeomChanged()
+{
+   recalcTextSize();
+}
+
+
+// Static method: Provide hook into the object currently being edited with the attrubute editor for callback purposes
+EditorObject *TextItem::getAttributeEditorObject()     
+{ 
+   return mAttributeMenuUI->getObject(); 
+}
+
+
+EditorAttributeMenuUI *TextItem::getAttributeMenu()
+{
+   // Lazily initialize this -- if we're in the game, we'll never need this to be instantiated
+   if(!mAttributeMenuUI)
+      mAttributeMenuUI = new TextItemEditorAttributeMenuUI;
+
+   // Udate the editor with attributes from our current object
+   mAttributeMenuUI->menuItems[0]->setValue(mText);
+
+   return mAttributeMenuUI;
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+TNL_IMPLEMENT_NETOBJECT(LineItem);
+
+// Why does GCC need this for signed int?
+#ifndef TNL_OS_WIN32
+const S32 LineItem::MIN_LINE_WIDTH;
+const S32 LineItem::MAX_LINE_WIDTH;
+#endif
+
+// Constructor
+LineItem::LineItem()
+{ 
+   mGeometry = boost::shared_ptr<Geometry>(new PolylineGeometry);
+   mNetFlags.set(Ghostable);
+   mObjectTypeMask |= LineType | CommandMapVisType;
+   mObjectTypeNumber = LineTypeNumber;
+}
+
+
+// Copy constructor -- make sure each copy gets its own geometry object
+LineItem::LineItem(const LineItem &li)
+{
+   mGeometry = boost::shared_ptr<Geometry>(new PolylineGeometry(*((PolylineGeometry *)li.mGeometry.get())));  
+}
+
+
+void LineItem::render()
+{
+   // Don't render opposing team's text items
+   if(!gClientGame || !gClientGame->getConnectionToServer())      // Not sure if this is really needed...
+      return;
+
+   Ship *ship = dynamic_cast<Ship *>(gClientGame->getConnectionToServer()->getControlObject());
+   if( (!ship && mTeam != -1) || (ship && ship->getTeam() != mTeam && mTeam != -1) )
+      return;
+
+   glColor(getGame()->getTeamColor(mTeam));
+   renderPointVector(getOutline(), GL_LINE_STRIP);
+}
+
+
+void LineItem::renderEditor(F32 currentScale)
+{
+   glColor(getGame()->getTeamColor(mTeam));
+   renderPointVector(getOutline(), GL_LINE_STRIP);
+
+   renderLinePolyVertices(currentScale);
+}
+
+
+// This object should be drawn below others
+S32 LineItem::getRenderSortValue()
+{
+   return 1;
+}
+
+
+// Create objects from parameters stored in level file
+// Entry looks like: LineItem 0 50 10 10 11 11 Message goes here
+bool LineItem::processArguments(S32 argc, const char **argv)
+{
+   if(argc < 6)
+      return false;
+
+   mTeam = atoi(argv[0]);
+   setWidth(max(min(atoi(argv[1]), MAX_LINE_WIDTH), MIN_LINE_WIDTH));
+   readGeom(argc, argv, 2, getGame()->getGridSize());
+
+   computeExtent();
+
+   return true;
+}
+
+
+string LineItem::toString()
+{
+   return string(getClassName()) + " " + itos(mTeam) + " " + itos(getWidth()) + " " + geomToString(getGame()->getGridSize());
+}
+
+
+void LineItem::onAddedToGame(Game *theGame)
+{
+   if(!isGhost())
+      setScopeAlways();
+
+   getGame()->mObjectsLoaded++;
+}
+
+
+// Bounding box for quick collision-possibility elimination, and display scoping purposes
+void LineItem::computeExtent()
+{
+   setExtent();
+}
+
+
+bool LineItem::getCollisionPoly(Vector<Point> &polyPoints)
+{
+   return false;
+}
+
+
+// Handle collisions with a LineItem.  Easy, there are none.
+bool LineItem::collide(GameObject *hitObject)
+{
+   return false;
+}
+
+
+void LineItem::idle(GameObject::IdleCallPath path)
+{
+   // Do nothing
+}
+
+
+U32 LineItem::packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream)
+{
+   //stream->writeRangedU32(mWidth, 0, MAX_LINE_WIDTH);
+   stream->write(mTeam);
+
+   packGeom(connection, stream);
+
+   return 0;
+}
+
+
+void LineItem::unpackUpdate(GhostConnection *connection, BitStream *stream)
+{
+   //mWidth = stream->readRangedU32(0, MAX_LINE_WIDTH);
+   stream->read(&mTeam);
+
+   unpackGeom(connection, stream);
+   setExtent();
+}
+
+
+};
