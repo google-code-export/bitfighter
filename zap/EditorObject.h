7cb7ae8bf2907fc16d31861b569aa5215fd59dfb
watusimoto <watusimoto@bitfighter.org>
2011-07-16 08:20:37 -0700

    Polywalls now properly merge with regular walls in editor

---
+++ zap/EditorObject.h
@@ -0,0 +1,239 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+
+#ifndef _EDITOROBJECT_H_
+#define _EDITOROBJECT_H_
+
+#include "gameObject.h"          // We inherit from this -- for BfObject, for now
+
+#include "Point.h"
+#include "tnlVector.h"
+#include "Color.h"
+
+
+using namespace std;
+using namespace TNL;
+
+namespace Zap
+{
+
+static const S32 NONE = -1;
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+enum ShowMode
+{
+   ShowAllButNavZones,
+   ShowAllObjects,
+   ShowWallsOnly,
+   NavZoneMode,
+   ShowModesCount
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class EditorAttributeMenuUI;
+class WallSegment;
+
+class EditorObject : virtual public BfObject   // Interface class  -- All editor objects need to implement this
+{
+
+private:
+   S32 mVertexLitUp;
+
+   bool mIsBeingEdited;
+   static S32 mNextSerialNumber;
+
+
+protected:
+   bool mDockItem;      // True if this item lives on the dock
+   bool mSelected;      // True if item is selected
+   bool mLitUp;         // True if user is hovering over the item and it's "lit up"
+
+   S32 mSerialNumber;   // Autoincremented serial number   
+   S32 mItemId;         // Item's unique id... 0 if there is none
+
+   Color getTeamColor(S32 teamId);
+   Color getDrawColor();
+
+   S32 mWidth;    // Walls, lines only
+
+
+public:
+   EditorObject(GameObjectType objectType = UnknownType);      // Constructor
+   virtual ~EditorObject();                                    // Virtual destructor
+   //virtual void copyAttrs(EditorObject *target);
+   virtual EditorObject *clone()  const = 0;
+
+   EditorObject *newCopy();         // Copies object        // TODO: Will become call to clone, delete method
+
+   virtual void addToDock(Game *game, const Point &point);
+
+
+   void renderDockItemLabel(const Point &pos, const char *label, F32 yOffset = 0);    // This could be moved anywhere... it's essentially a static method
+
+   // Offset lets us drag an item out from the dock by an amount offset from the 0th vertex.  This makes placement seem more natural.
+   virtual Point getInitialPlacementOffset(F32 gridSize) { return Point(0,0); }
+
+   // Account for the fact that the apparent selection center and actual object center are not quite aligned
+   virtual Point getEditorSelectionOffset(F32 currentScale);  
+
+   void renderAndLabelHighlightedVertices(F32 currentScale);   // Render selected and highlighted vertices, called from renderEditor
+   virtual void renderItemText(const char *text, S32 offset, F32 currentScale) { };    // Render some text, with specified vertical offset
+   virtual void renderEditor(F32 currentScale) { TNLAssert(false, "renderEditor not implemented!"); }
+
+   Point convertLevelToCanvasCoord(const Point &pt);
+
+   // Should we show item attributes when it is selected? (only overridden by TextItem)
+   virtual bool showAttribsWhenSelected() { return true; }                             
+
+   virtual F32 getEditorRenderScaleFactor(F32 mCurrentScale) { return 1; }             // Only overridden for point items
+   virtual void renderAttribs(F32 currentScale);
+
+   void unselect();
+
+   void setSnapped(bool snapped) { /* Do nothing */ }          // Overridden in EngineeredObject
+
+   virtual void newObjectFromDock(F32 gridSize) { /* Do nothing */ }   // Called when item dragged from dock to editor -- overridden by Polygon and SimpleLine
+
+
+   // Keep track which vertex, if any is lit up in the currently selected item
+   bool isVertexLitUp(S32 vertexIndex) { return mVertexLitUp == vertexIndex; }
+   void setVertexLitUp(S32 vertexIndex) { mVertexLitUp = vertexIndex; }
+
+   // Objects can be different sizes on the dock and in the editor.  We need to draw selection boxes in both locations,
+   // and these functions specify how big those boxes should be.  Override if implementing a non-standard sized item.
+   // (strictly speaking, only getEditorRadius needs to be public, but it make sense to keep these together organizationally.)
+   virtual S32 getDockRadius() { return 10; }                     // Size of object on dock 
+   virtual F32 getEditorRadius(F32 currentScale) { return 10; }   // Size of object in editor 
+   virtual const char *getVertLabel(S32 index) { return ""; }     // Label for vertex, if any... only overridden by SimpleLine objects
+
+
+   void saveItem(FILE *f, F32 gridSize);
+   virtual string toString(F32 gridSize) const = 0; 
+
+   Vector<Point> extendedEndPoints;                            // these are computed but not stored in barrier... not sure how to merge
+
+   // Dock item rendering methods
+   virtual void renderDock() { TNLAssert(false, "renderDock not implemented!"); };
+   virtual void labelDockItem();
+   virtual void highlightDockItem();
+
+   void renderLinePolyVertices(F32 scale, F32 alpha = 1.0);    // Only for polylines and polygons  --> move there
+  
+
+   // TODO: Get rid of this ==> most of this code already in polygon
+   void initializePolyGeom();     // Once we have our points, do some geom preprocessing ==> only for polygons
+
+   //// For walls only
+   void decreaseWidth(S32 amt);  // Wall only
+   void increaseWidth(S32 amt);  // Wall only
+   void setWidth(S32 width);
+   S32 getWidth() const { return mWidth; }
+   ////
+
+   void moveTo(const Point &pos, S32 snapVertex = 0);    // Move object to location, specifying (optional) vertex to be positioned at pos
+   void offset(const Point &offset);                     // Offset object by a certain amount
+
+   S32 repopDelay;        // For repair items, also used for engineered objects heal rate
+
+
+   //////
+   // Vertex management functions
+   void renderPolylineCenterline(F32 alpha);    // Draw barrier centerlines; wraps renderPolyline()  ==> lineItem, barrierMaker only
+
+   virtual void onGeomChanging();                        // Item geom is interactively changing
+   virtual void onGeomChanged() { /* To be =0 */ };      // Item changed geometry (or moved), do any internal updating that might be required
+
+   virtual void onItemDragging() { /* Do nothing */ };   // Item is being dragged around the screen
+
+   virtual void onAttrsChanging() { /* Do nothing */ };  // Attr is in the process of being changed (i.e. a char was typed for a textItem)
+   virtual void onAttrsChanged() { /* Do nothing */ };   // Attrs changed
+
+
+   /////
+   // Geometry operations  -- can we provide standard implementations of these?
+   void rotateAboutPoint(const Point &origin, F32 angle) { mGeometry->rotateAboutPoint(origin, angle); onGeomChanged(); }
+   void flipHorizontal(F32 minX, F32 maxX) { mGeometry->flipHorizontal(minX, maxX); onGeomChanged(); };
+   void flipVertical(F32 minY, F32 maxY) { mGeometry->flipVertical(minY, maxY); onGeomChanged(); };
+   virtual void scale(const Point &center, F32 scale) { mGeometry->scale(center, scale); onGeomChanged(); }
+
+   /////
+   S32 getItemId() { return mItemId; }
+   void setItemId(S32 itemId) { mItemId = itemId; }
+   
+   S32 getSerialNumber() { return mSerialNumber; }
+   void setSerialNumber(S32 serialNumber) { mSerialNumber = serialNumber; }
+
+
+   bool isSelected() { return mSelected; }
+   void setSelected(bool selected) { mSelected = selected; }
+
+   void setDockItem(bool isDockItem) { mDockItem = isDockItem; }
+
+   bool isLitUp() { return mLitUp; }
+   void setLitUp(bool litUp) { mLitUp = litUp; if(!litUp) setVertexLitUp(NONE); }
+
+   bool isBeingEdited() { return mIsBeingEdited; }
+   void setIsBeingEdited(bool isBeingEdited) { mIsBeingEdited = isBeingEdited; }
+
+
+   virtual void initializeEditor();
+
+   Point forceFieldEnd;      // Point where forcefield terminates.  Only used for forcefields.
+
+   S32 mScore;
+   S32 getScore() { return mScore; }     // goal zones only, return zone's score
+
+   virtual bool canBeHostile();
+   virtual bool canBeNeutral();
+   virtual bool hasTeam();
+
+   virtual const char *getEditorHelpString() = 0;     
+   virtual const char *getPrettyNamePlural() = 0;
+   virtual const char *getOnDockName() = 0;
+   virtual const char *getOnScreenName() = 0;   
+
+   virtual const char *getInstructionMsg() { return ""; }      // Message printed below item when it is selected
+
+   virtual EditorAttributeMenuUI *getAttributeMenu() { return NULL; }    // Override if child class has an attribute menu
+
+
+   //////////////
+
+   //TODO: Get rid of this altogether
+   void render(bool isScriptItem, bool showingReferenceShip, ShowMode showMode);
+
+};
+
+
+
+};
+#endif
---
+++ zap/barrier.h
@@ -0,0 +1,306 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _BARRIER_H_
+#define _BARRIER_H_
+
+#include "gameObject.h"
+#include "textItem.h"      // for LineItem def  TODO: get rid of this
+
+#include "Point.h"
+#include "tnlVector.h"
+#include "tnlNetObject.h"
+
+namespace Zap
+{
+
+/// The Barrier class represents rectangular barriers that player controlled
+/// ships cannot pass through... i.e. walls  Barrier objects, once created, never
+/// change state, simplifying the pack/unpack update methods.  Barriers are
+/// constructed as an expanded line segment.
+class Barrier : public GameObject
+{
+public:
+   Vector<Point> mPoints; ///< The points of the barrier --> if only two, first will be start, second end of an old-school segment
+
+   bool mSolid;
+   // By precomputing and storing, we should ease the rendering cost
+   Vector<Point> mRenderFillGeometry; ///< Actual geometry used for rendering fill.
+   Vector<Point> mRenderOutlineGeometry; ///< Actual geometry used for rendering outline.
+
+   F32 mWidth;
+
+   static const S32 MIN_BARRIER_WIDTH = 1;         // Clipper doesn't much like 0 width walls
+   static const S32 MAX_BARRIER_WIDTH = 2500;      // Geowar has walls at least 350 units wide, so going lower will break at least one level
+
+   static const S32 DEFAULT_BARRIER_WIDTH = 50; ///< The default width of the barrier in game units
+
+   static Vector<Point> mRenderLineSegments;    ///< The clipped line segments representing this barrier.
+   Vector<Point> mBotZoneBufferLineSegments;    ///< The line segments representing a buffered barrier.
+
+   /// Barrier constructor
+   Barrier(const Vector<Point> &points = Vector<Point>(), F32 width = DEFAULT_BARRIER_WIDTH, bool solid = false);
+
+   /// Adds the server object to the net interface's scope always list
+   void onAddedToGame(Game *theGame);
+
+   /// Renders barrier fill
+   void render(S32 layer);                // Renders barrier fill barrier-by-barrier
+   static void renderEdges(S32 layer);    // Renders all edges in one pass
+
+   /// Returns a sorting key for the object.  Barriers should be drawn first so as to appear behind other objects.
+   S32 getRenderSortValue() { return 0; }
+
+   /// returns the collision polygon of this barrier, which is the boundary extruded from the start,end line segment.
+   bool getCollisionPoly(Vector<Point> &polyPoints) const;
+
+   /// collide always returns true for Barrier objects.
+   bool collide(GameObject *otherObject) { return true; }
+
+   // Takes a list of vertices and converts them into a list of lines representing the edges of an object
+   static void resetEdges(const Vector<Point> &corners, Vector<Point> &edges);
+
+   // Simply takes a segment and "puffs it out" to a rectangle of a specified width, filling cornerPoints.  Does not modify endpoints.
+   static void expandCenterlineToOutline(const Point &start, const Point &end, F32 width, Vector<Point> &cornerPoints);
+
+   // Takes a segment and "puffs it out" to a polygon for bot zone generation.
+   // This polygon is the width of the barrier plus the ship's collision radius added to the outside
+   Vector<Point> getBufferForBotZone();
+
+   // Combines multiple barriers into a single complex polygon
+   static bool unionBarriers(const Vector<DatabaseObject *> &barriers, Vector<Vector<Point> > &solution);
+
+   /// Clips the current set of render lines against the polygon passed as polyPoints, modifies lineSegmentPoints.
+   static void clipRenderLinesToPoly(const Vector<DatabaseObject *> &barrierList, Vector<Point> &lineSegmentPoints);
+
+   static void constructBarrierEndPoints(const Vector<Point> *vec, F32 width, Vector<Point> &barrierEnds);
+
+   // Clean up edge geometry and get barriers ready for proper rendering
+   static void prepareRenderingGeometry(Game *game);
+
+
+   TNL_DECLARE_CLASS(Barrier);
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// TODO: Don't need this to be a GameObject  -- perhaps merge with barrier above?
+class WallItem : public LineItem
+{
+public:
+   WallItem();    // Constructor
+   WallItem *clone() const;
+   
+   virtual void onGeomChanged();
+   void processEndPoints();      
+
+   // Some properties about the item that will be needed in the editor
+   const char *getEditorHelpString() { return "Walls define the general form of your level."; }  
+   const char *getPrettyNamePlural() { return "Walls"; }
+   const char *getOnDockName() { return "Wall"; }
+   const char *getOnScreenName() { return "Wall"; }
+   bool hasTeam() { return false; }
+   bool canBeHostile() { return false; }
+   bool canBeNeutral() { return false; }
+
+   void scale(const Point &center, F32 scale);
+
+   string toString(F32 gridSize) const;
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class PolyWall : public EditorPolygon     // Don't need GameObject component of this...
+{
+private:
+   //typedef EditorObject Parent;
+   //void computeExtent();
+
+public:
+   PolyWall();      // Constructor
+   PolyWall *clone() const;
+  
+   bool processArguments(S32 argc, const char **argv, Game *game);
+
+   void processEndPoints();
+
+   void render();
+   void renderFill();
+   void renderDock();
+   void renderEditor(F32 currentScale);
+
+   S32 getRenderSortValue() { return -1; }
+
+   //bool getCollisionPoly(Vector<Point> &polyPoints);
+
+   /////
+   // Editor methods
+   const char *getEditorHelpString() { return "Polygonal wall item let you be creative with your wall design."; }
+   const char *getPrettyNamePlural() { return "PolyWalls"; }
+   const char *getOnDockName() { return "PolyWall"; }
+   const char *getOnScreenName() { return "PolyWall"; }
+   string toString(F32 gridSize) const;
+
+   /////
+   // Lua interface  ==>  don't need these!!
+
+   PolyWall(lua_State *L) { /* Do nothing */ };    //  Lua constructor
+   GameObject *getGameObject() { return this; }          // Return the underlying GameObject
+
+   static const char className[];                        // Class name as it appears to Lua scripts
+   static Lunar<PolyWall>::RegType methods[];
+
+   S32 getClassID(lua_State *L) { return returnInt(L, PolyWallType); }
+   TNL_DECLARE_CLASS(PolyWall);
+
+private:
+   void push(lua_State *L) { Lunar<PolyWall>::push(L, this); }
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class WallSegment : public DatabaseObject
+{
+private:
+   static GridDatabase *mGridDatabase;    // Database of segments
+   GridDatabase *getGridDatabase() { return mGridDatabase; }      
+
+   void init(S32 owner);
+
+public:
+   WallSegment(const Point &start, const Point &end, F32 width, S32 owner = -1);    // Normal wall segment
+   WallSegment(const Vector<Point> &points, S32 owner = -1);                        // PolyWall 
+   ~WallSegment();
+
+   Vector<Point> edges;    
+   Vector<Point> corners;
+   Vector<Point> triangulatedFillPoints;
+   S32 mOwner;
+
+   bool invalid;              // A flag for marking segments in need of processing
+
+   void resetEdges();         // Compute basic edges from corner points
+   void computeBoundingBox(); // Computes bounding box based on the corners, updates database
+   
+   void renderFill(bool renderLight, bool showingReferenceShip);
+
+   ////////////////////
+   //  DatabaseObject methods
+   static void setGridDatabase(GridDatabase *database) { mGridDatabase = database; }
+
+
+   // Note that the poly returned here is different than what you might expect -- it is composed of the edges,
+   // not the corners, and is thus in A-B, C-D, E-F format rather than the more typical A-B-C-D format returned
+   // by getCollisionPoly() elsewhere in the game.  Therefore, it needs to be handled differently.
+   bool getCollisionPoly(Vector<Point> &polyPoints) const { polyPoints = edges; return true; }  
+   bool getCollisionCircle(U32 stateIndex, Point &point, float &radius) const { return false; }
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Width of line representing centerline of barriers
+#define WALL_SPINE_WIDTH gLineWidth3
+
+
+class WallEdge : public DatabaseObject
+{
+private:
+   static GridDatabase mGridDatabase;
+
+   Point mStart, mEnd;
+
+public:
+   WallEdge(const Point &start = Point(), const Point &end = Point());
+   ~WallEdge();
+
+   Point *getStart() { return &mStart; }
+   Point *getEnd() { return &mEnd; }
+
+   GridDatabase *getGridDatabase() { return &mGridDatabase; }      // Can't be static because it's required as part of our interfaceTODO: make private
+   static GridDatabase *getWallEdgeDatabase() { return &mGridDatabase; }    // Provide static access to our database
+
+   // Note that the poly returned here is different than what you might expect -- it is composed of the edges,
+   // not the corners, and is thus in A-B, C-D, E-F format rather than the more typical A-B-C-D format returned
+   // by getCollisionPoly() elsewhere in the game.  Therefore, it needs to be handled differently.
+   bool getCollisionPoly(Vector<Point> &polyPoints) const { polyPoints.resize(2); polyPoints[0] = mStart; polyPoints[1] = mEnd; return true; }  
+   bool getCollisionCircle(U32 stateIndex, Point &point, float &radius) const { return false; }
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class EditorObject;
+
+class WallSegmentManager
+{
+private:
+   static GridDatabase *mGridDatabase;
+
+public:
+   WallSegmentManager()  { /* Do nothing */ }
+
+   static GridDatabase *getGridDatabase() { return mGridDatabase; } 
+   static void setGridDatabase(GridDatabase *gridDatabase) { mGridDatabase = gridDatabase; }
+
+   Vector<WallSegment *> mWallSegments;      
+   static Vector<WallEdge *> mWallEdges;        // For mounting forcefields/turrets
+   static Vector<Point> mWallEdgePoints;        // For rendering
+
+   void buildAllWallSegmentEdgesAndPoints();
+   void deleteSegments(S32 owner);              // Delete all segments owned by specified WorldItem
+   void deleteAllSegments();
+
+   // Recalucate edge geometry for all walls when item has changed
+   void computeWallSegmentIntersections(EditorObject *item); 
+
+   // Takes a wall, finds all intersecting segments, and marks them invalid
+   void invalidateIntersectingSegments(EditorObject *item);
+
+   void buildWallSegmentEdgesAndPoints(DatabaseObject *object);
+   void buildWallSegmentEdgesAndPoints(DatabaseObject *object, const Vector<DatabaseObject *> &engrObjects);
+   void recomputeAllWallGeometry();
+
+   // Populate wallEdges
+   static void clipAllWallEdges(const Vector<WallSegment *> &wallSegments, Vector<Point> &wallEdges);
+ 
+   ////////////////
+   // Render functions
+   void renderWalls(bool isBeingDragged, bool showingReferenceShip, bool showSnapVertices, F32 alpha);
+};
+
+
+};
+
+#endif
+
