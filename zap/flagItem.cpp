81d7420f767e2fde9554bea91d761eff5d749988
watusimoto <watusimoto@bitfighter.org>
2011-08-12 21:32:43 -0700

    Extended abstract spawn into itemSpawn, added test class CircleSpawn, fixed asteroid spawn bug

---
+++ zap/flagItem.cpp
@@ -0,0 +1,337 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "flagItem.h"
+#include "gameItems.h"     // For FlagSpawn def
+#include "gameType.h"
+#include "game.h"
+#include "stringUtils.h"
+
+#include "SDL/SDL_opengl.h"
+
+namespace Zap
+{
+
+TNL_IMPLEMENT_NETOBJECT(FlagItem);
+
+// C++ constructor
+FlagItem::FlagItem(Point pos) : EditorItem(pos, true, (F32)Ship::CollisionRadius) // radius was 20
+{
+   initialize();
+}
+
+// Alternate constructor, currently used by HuntersFlag
+FlagItem::FlagItem(Point pos, bool collidable, float radius, float mass) : EditorItem(pos, collidable, radius, mass)
+{
+   initialize();
+}
+
+// Alternate constructor, currently used by dropping flags in hunterGame
+FlagItem::FlagItem(Point pos, Point vel, bool useDropDelay) : EditorItem(pos, true, (F32)Ship::CollisionRadius, 4)
+{
+   initialize();
+
+   setActualVel(vel);
+   if(useDropDelay)
+      mDroppedTimer.reset(DROP_DELAY);
+}
+
+
+
+FlagItem *FlagItem::clone() const
+{
+   return new FlagItem(*this);
+}
+
+
+void FlagItem::initialize()
+{
+   mTeam = -1;
+   mFlagCount = 1;
+   mIsAtHome = true;    // All flags start off at home!
+
+   mNetFlags.set(Ghostable);
+   mObjectTypeNumber = FlagTypeNumber;
+   setZone(NULL);
+}
+
+
+const char FlagItem::className[] = "FlagItem";      // Class name as it appears to Lua scripts
+
+// Define the methods we will expose to Lua
+Lunar<FlagItem>::RegType FlagItem::methods[] =
+{
+   // Standard gameItem methods
+   method(FlagItem, getClassID),
+   method(FlagItem, getLoc),
+   method(FlagItem, getRad),
+   method(FlagItem, getVel),
+   method(FlagItem, getTeamIndx),
+
+   // item methods
+   method(FlagItem, isInCaptureZone),
+   method(FlagItem, getCaptureZone),
+   method(FlagItem, isOnShip),
+   method(FlagItem, getShip),
+
+   // Flag specific methods
+   method(FlagItem, isInInitLoc),
+
+   {0,0}    // End method list
+};
+
+
+void FlagItem::onAddedToGame(Game *theGame)
+{ 
+   Parent::onAddedToGame(theGame);
+   theGame->getGameType()->addFlag(this);    
+}
+
+
+static bool isTeamFlagSpawn(Game *game, S32 team)
+{
+   return game->getGameType()->isTeamFlagGame() && team >= 0 && team < game->getTeamCount();
+}
+
+
+bool FlagItem::processArguments(S32 argc, const char **argv, Game *game)
+{
+   if(argc < 3)         // FlagItem <team> <x> <y> {time}
+      return false;
+
+   mTeam = atoi(argv[0]);
+   //if(! getGame()->getGameType()->checkTeamRange(mTeam)) mTeam = -1;
+   
+   if(!Parent::processArguments(argc-1, argv+1, game))
+      return false;
+
+   S32 time = (argc >= 4) ? atoi(argv[4]) : 0;     // Flag spawn time is possible 4th argument.  This time only turns out to be important in Nexus games at the moment.
+
+   mInitialPos = mMoveState[ActualState].pos;
+
+   // Now add the flag starting point to the list of flag spawn points
+   GameType *gt = game->getGameType();
+   if(gt)
+   {
+      FlagSpawn spawn = FlagSpawn(mInitialPos, time);
+
+      if(isTeamFlagSpawn(game, mTeam))
+      {
+         Team *team = dynamic_cast<Team *>(game->getTeam(mTeam));
+         if(team)
+            team->addFlagSpawn(spawn);
+      }
+      else                                                                        
+         gt->addFlagSpawn(spawn);
+   }
+
+   return true;
+}
+
+
+string FlagItem::toString(F32 gridSize) const
+{
+   return string(getClassName()) + " " + itos(mTeam) + " " + geomToString(gridSize);
+}
+
+
+U32 FlagItem::packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream)
+{
+   U32 returnSomething = Parent::packUpdate(connection, updateMask, stream);
+   if(updateMask & InitialMask)
+      writeThisTeam(stream);
+   return returnSomething;
+}
+
+
+void FlagItem::unpackUpdate(GhostConnection *connection, BitStream *stream)
+{
+   Parent::unpackUpdate(connection, stream);
+   if(mInitial)
+      readThisTeam(stream);
+}
+
+
+void FlagItem::idle(GameObject::IdleCallPath path)
+{
+   Parent::idle(path);
+
+   if(isGhost()) 
+      return;
+}
+
+
+void FlagItem::mountToShip(Ship *theShip)
+{
+   Parent::mountToShip(theShip);
+
+   if(mIsMounted)    // Will be true unless something went wrong in mountToShip
+      mIsAtHome = false;
+}
+
+
+// Only called from from sendHome()
+const Vector<FlagSpawn> *FlagItem::getSpawnPoints()
+{
+   Game *game = getGame();
+   GameType *gt = game->getGameType();
+
+   TNLAssert(gt, "Invalid gameType!");
+
+   if(isTeamFlagSpawn(game, mTeam))    
+      return ((Team *)(game->getTeam(mTeam)))->getFlagSpawns();
+   else             
+      return gt->getFlagSpawns();
+}
+
+
+void FlagItem::sendHome()
+{
+   // Now that we have flag spawn points, we'll simply redefine "initial pos" as a random selection of the flag spawn points
+   // Everything else should remain as it was
+
+   // First, make a temp list of valid spawn points -- start with a list of all spawn points, then remove any occupied ones
+
+   Vector<FlagSpawn> spawnPoints = *getSpawnPoints();    // Makes a copy?  Hopefully!
+   Game *game = getGame();
+   GameType *gt = game->getGameType();
+
+   // Now remove the occupied spots from our list of potential spawns
+   for(S32 i = 0; i < gt->mFlags.size(); i++)
+   {
+      FlagItem *flag = gt->mFlags[i];
+      if(flag->isAtHome() && (flag->mTeam < 0 || flag->mTeam == mTeam || !gt->isTeamFlagGame()))
+      {
+         // Need to remove this flag's spawnpoint from the list of potential spawns... it's occupied, after all...
+         // Note that if two spawnpoints are on top of one another, this will remove the first, leaving the other
+         // on the unoccupied list, unless a second flag at this location removes it from the list on a subsequent pass.
+         for(S32 j = 0; j < spawnPoints.size(); j++)
+            if(spawnPoints[j].getPos() == flag->mInitialPos)
+            {
+               spawnPoints.erase_fast(j);
+               break;
+            }
+      }
+   }
+
+   if(spawnPoints.size() == 0)      // Protect from crash if this happens, which it shouldn't, but has
+   {
+      TNLAssert(false, "No flag spawn points!");
+      logprintf(LogConsumer::LogError, "LEVEL ERROR!! Level %s has no flag spawn points for team %d\n**Please submit this level to the devs!**", 
+         gServerGame->getCurrentLevelFileName().getString(), mTeam);
+      //mInitialPos = Point(0,0);      --> Leave mInitialPos as it was... it will probably be better than (0,0)
+   }
+   else
+   {
+      S32 spawnIndex = TNL::Random::readI() % spawnPoints.size();
+      mInitialPos = spawnPoints[spawnIndex].getPos();
+   }
+
+   mMoveState[ActualState].pos = mMoveState[RenderState].pos = mInitialPos;
+   mMoveState[ActualState].vel = mMoveState[RenderState].vel = Point(0,0);
+   mIsAtHome = true;
+   setMaskBits(PositionMask);
+   updateExtent();
+}
+
+
+void FlagItem::renderItem(Point pos)
+{
+   Point offset;
+
+   if(mIsMounted)
+      offset.set(15, -15);
+
+   renderFlag(pos + offset, getGame()->getTeamColor(mTeam));
+}
+
+
+void FlagItem::renderDock()
+{
+   glPushMatrix();
+      glTranslate(getVert(0));
+      glScale(0.6f);
+      renderFlag(0, 0, getGame()->getTeamColor(mTeam));
+   glPopMatrix();   
+}
+
+
+F32 FlagItem::getEditorRadius(F32 currentScale)
+{
+   return 18 * currentScale;
+}
+
+
+// Runs on both client and server
+bool FlagItem::collide(GameObject *hitObject)
+{
+   // Flag never collides if it is mounted or is set to be not collideable for some reason
+   if(mIsMounted || !mIsCollideable)
+      return false;
+
+   // Flag always collides with walls and forcefields
+   if(isFlagCollideableType(hitObject->getObjectTypeNumber()))
+      return true;
+
+   // No other collision detection happens on the client -- From here on out, it's server only!
+   if(isGhost())
+      return false;
+
+   // The only things we'll collide with (aside from walls and forcefields above) is ships and robots
+   if( !(isShipType(hitObject->getObjectTypeNumber())) )
+      return false;
+
+   // Ignore collisions that occur to recently dropped flags.  Make sure flag is ready to be picked up! 
+   if(mDroppedTimer.getCurrent())    
+      return false;
+
+   // We've hit a ship or robot  (remember, robot is a subtype of ship, so this will work for both)
+   // We'll need to make sure the ship is a valid entity and that it hasn't exploded
+   Ship *ship = dynamic_cast<Ship *>(hitObject);
+   if(!ship || (ship->hasExploded))
+      return false;
+
+   GameType *gt = getGame()->getGameType();
+
+   if(!gt)     // Something is wrong...
+      return false;     
+   
+   // Finally!
+   gt->shipTouchFlag(ship, this);
+
+   return false;
+}
+
+
+void FlagItem::onMountDestroyed()
+{
+   if(mMount->getOwner())
+      mMount->getOwner()->mStatistics.mFlagDrop++;
+
+   onItemDropped();
+}
+
+
+};
---
+++ zap/gridDB.h
@@ -0,0 +1,187 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _GRIDDB_H_
+#define _GRIDDB_H_
+
+
+#include "tnlTypes.h"
+#include "tnlDataChunker.h"
+#include "tnlVector.h"
+
+#include "Rect.h"
+#include "GeomUtils.h"
+
+
+using namespace TNL;
+
+namespace Zap
+{
+
+typedef bool (*TestFunc)(U8);
+
+// Interface for dealing with objects that can be in our spatial database.  Can be either GameObjects or
+// items in te
+class  GridDatabase;
+
+class DatabaseObject
+{
+friend class GridDatabase;
+
+private:
+   U32 mLastQueryId;
+   Rect mExtent;
+   GridDatabase *mDatabase;
+
+protected:
+   U8 mObjectTypeNumber;
+
+public:
+   DatabaseObject();                            // Constructor
+   DatabaseObject(const DatabaseObject &t);     // Copy constructor
+
+   void initialize();
+
+   U8 getObjectTypeNumber() { return mObjectTypeNumber; }
+   void setObjectTypeNumber(U8 objectTypeNumber) { mObjectTypeNumber = objectTypeNumber; }
+
+   GridDatabase *getDatabase() { return mDatabase; }     // Returns the database in which this object is stored, NULL if not in any database
+   void setDatabase(GridDatabase *database) { mDatabase = database; }
+
+   Rect getExtent() { return mExtent; }
+   void setExtent(const Rect &extentRect);
+
+   virtual bool getCollisionPoly(Vector<Point> &polyPoints) const = 0;
+   virtual bool getCollisionCircle(U32 stateIndex, Point &point, float &radius) const = 0;
+   
+   virtual bool isCollisionEnabled() { return true; }
+
+   bool isInDatabase() { return mDatabase != NULL; }
+   bool isDeleted();
+
+   void addToDatabase(GridDatabase *database);
+   void removeFromDatabase();
+
+   virtual bool isDatabasable() { return true; }      // Can this item actually be inserted into a database?
+};
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class GridDatabase
+{
+private:
+
+   void findObjects(U8 typeNumber, Vector<DatabaseObject *> &fillVector, const Rect *extents, S32 minx, S32 miny, S32 maxx, S32 maxy);
+   void findObjects(TestFunc testFunc, Vector<DatabaseObject *> &fillVector, const Rect *extents, S32 minx, S32 miny, S32 maxx, S32 maxy);
+   static U32 mQueryId;
+   static U32 mCountGridDatabase;
+
+protected:
+   Vector<DatabaseObject *> mAllObjects;
+
+public:
+   enum {
+      BucketRowCount = 16,    // Number of buckets per grid row, and number of rows; should be power of 2
+      BucketMask = BucketRowCount - 1,
+   };
+
+   struct BucketEntry
+   {
+      DatabaseObject *theObject;
+      BucketEntry *nextInBucket;
+   };
+
+   static ClassChunker<BucketEntry> *mChunker;  // if not a pointer, then somehow static get destroyed first then non-static when game is quitting, crashing it.
+
+   BucketEntry *mBuckets[BucketRowCount][BucketRowCount];
+
+   GridDatabase();                              // Constructor
+   virtual ~GridDatabase();                     // Destructor
+
+   static const S32 BucketWidth = 255;          // Width/height of each bucket in pixels
+   static const F32 widthDiv;
+
+   DatabaseObject *findObjectLOS(U8 typeNumber, U32 stateIndex, bool format, const Point &rayStart, const Point &rayEnd,
+                                 float &collisionTime, Point &surfaceNormal);
+   DatabaseObject *findObjectLOS(U8 typeNumber, U32 stateIndex, const Point &rayStart, const Point &rayEnd,
+                                 float &collisionTime, Point &surfaceNormal);
+   DatabaseObject *findObjectLOS(TestFunc testFunc, U32 stateIndex, bool format, const Point &rayStart, const Point &rayEnd,
+                                 float &collisionTime, Point &surfaceNormal);
+   DatabaseObject *findObjectLOS(TestFunc testFunc, U32 stateIndex, const Point &rayStart, const Point &rayEnd,
+                                 float &collisionTime, Point &surfaceNormal);
+   bool pointCanSeePoint(const Point &point1, const Point &point2);
+
+   void findObjects(Vector<DatabaseObject *> &fillVector);     // Returns all objects in the database
+   void findObjects(U8 typeNumber, Vector<DatabaseObject *> &fillVector);
+   void findObjects(U8 typeNumber, Vector<DatabaseObject *> &fillVector, const Rect &extents);
+   void findObjects(TestFunc testFunc, Vector<DatabaseObject *> &fillVector);
+   void findObjects(TestFunc testFunc, Vector<DatabaseObject *> &fillVector, const Rect &extents);
+
+   
+   virtual void addToDatabase(DatabaseObject *theObject, const Rect &extents);
+   virtual void removeFromDatabase(DatabaseObject *theObject, const Rect &extents);
+   void removeEverythingFromDatabase();
+
+   S32 getObjectCount() { return mAllObjects.size(); }      // Return the number of objects currently in the database
+   DatabaseObject *getObjectByIndex(S32 index);             // Kind of hacky, kind of useful
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class EditorObject;
+
+class EditorObjectDatabase : public GridDatabase
+{
+   typedef GridDatabase Parent;
+
+private:
+   Vector<EditorObject *> mAllEditorObjects;
+
+public:
+   EditorObjectDatabase();      // Constructor
+   EditorObjectDatabase(const EditorObjectDatabase &database);    // Copy constructor
+   EditorObjectDatabase &operator= (const EditorObjectDatabase &database);
+
+   void copy(const EditorObjectDatabase &database);       // Copy contents of source into this
+
+   const Vector<EditorObject *> *getObjectList();     
+
+   void addToDatabase(DatabaseObject *theObject, const Rect &extents);
+
+   void removeFromDatabase(DatabaseObject *theObject, const Rect &extents);
+};
+
+};
+
+// Reusable container for searching gridDatabases
+// putting it outside of Zap namespace seems to help with debugging showing whats inside fillVector  (debugger forgets to add Zap::)
+extern Vector<Zap::DatabaseObject *> fillVector;
+
+#endif
+
+
