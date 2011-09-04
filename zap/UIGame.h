f09c4c25fa511e384d91e8e519a8e7f5b3129f62
sam8641 <sam8641@localhost>
2011-09-04 16:19:47 -0500

    Fix problem with GridDatabase freeze problem.

---
+++ zap/UIGame.h
@@ -0,0 +1,358 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _UIGAME_H_
+#define _UIGAME_H_
+
+#include "UI.h"
+#include "quickChatHelper.h"
+#include "loadoutHelper.h"
+#include "engineerHelper.h"
+#include "Timer.h"
+#include "voiceCodec.h"
+#include "Point.h"
+#include "Color.h"
+#include "game.h"
+
+namespace Zap
+{
+
+enum ArgTypes {
+   NAME,    // Player name (can be tab-completed)
+   TEAM,    // Team name (can be tab-completed)
+   INT,     // Integer argument
+   STR,      // String argument
+   ARG_TYPES
+};
+
+enum HelpCategories {
+   ADV_COMMANDS,
+   LEVEL_COMMANDS,
+   ADMIN_COMMANDS,
+   DEBUG_COMMANDS,
+   COMMAND_CATEGORIES
+};
+
+class GameUserInterface;
+
+struct CommandInfo {
+   string cmdName;
+   void (*cmdCallback)(ClientGame *game, const Vector<string> &args);
+   ArgTypes cmdArgInfo[9];
+   S32 cmdArgCount;
+   HelpCategories helpCategory;
+   S32 helpGroup;
+   string helpArgString[9];
+   string helpTextString;
+};
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class GameUserInterface : public UserInterface
+{
+   typedef UserInterface Parent;
+
+private:
+   Move mCurrentMove;
+   Move mTransformedMove;
+   Point mMousePoint;
+
+   bool mLeftDisabled;
+   bool mRightDisabled;
+   bool mUpDisabled;
+   bool mDownDisabled;
+
+   void disableMovementKey(KeyCode keyCode);
+
+   // Related to display of in-game chat and status messages
+   static const S32 MessageDisplayCount = 6;           // How many server messages to display
+   static const S32 DisplayMessageTimeout = 3000;      // How long to display them (ms)
+
+   static const S32 ChatMessageStoreCount = 24;        // How many chat messages to store (only top MessageDisplayCount are normally displayed)
+   static const S32 ChatMessageDisplayCount = 5;       // How many chat messages to display
+   static const S32 DisplayChatMessageTimeout = 4000;  // How long to display them (ms)
+
+   enum MessageDisplayMode {
+      ShortTimeout,            // Traditional message display mode (6 MessageDisplayCount lines, messages timeout after DisplayMessageTimeout)
+      ShortFixed,              // Same length as ShortTimeout, but without timeout
+      LongFixed,               // Long form: Display MessageStoreCount messages, no timout
+      MessageDisplayModes
+   };
+
+   MessageDisplayMode mMessageDisplayMode;    // Our current message display mode
+
+   // These are our normal server messages, that "time out"
+   Color mDisplayMessageColor[MessageDisplayCount];
+   char mDisplayMessage[MessageDisplayCount][MAX_CHAT_MSG_LENGTH];
+
+   // These are our chat messages, that "time out"
+   Color mDisplayChatMessageColor[ChatMessageDisplayCount];
+   char mDisplayChatMessage[ChatMessageDisplayCount][MAX_CHAT_MSG_LENGTH];
+
+   // These are only displayed in the extended chat panel, and don't time out
+   Color mStoreChatMessageColor[ChatMessageStoreCount];
+   char mStoreChatMessage[ChatMessageStoreCount][MAX_CHAT_MSG_LENGTH];
+
+   Timer mDisplayMessageTimer;
+   Timer mDisplayChatMessageTimer;
+   Timer mShutdownTimer;
+
+   bool mMissionOverlayActive;      // Are game instructions (F2) visible?
+
+   enum ChatType {            // Types of in-game chat messages:
+      GlobalChat,             // Goes to everyone in game
+      TeamChat,               // Goes to teammates only
+      CmdChat                 // Entering a command
+   };
+
+   enum ShutdownMode {
+      None,                   // Nothing happening
+      ShuttingDown,           // Shutting down, obviously
+      Canceled                // Was shutting down, but are no longer
+   };
+
+   bool isCmdChat();          // Returns true if we're composing a command in the chat bar, false otherwise
+
+   ChatType mCurrentChatType; // Current in-game chat mode (global or local)
+   LineEditor mLineEditor;    // Message being composed
+
+   U32 mChatCursorPos;        // Position of composition cursor
+
+   bool mInScoreboardMode;
+   ShutdownMode mShutdownMode;
+
+   // Some rendering routines
+   void renderScoreboard(const GameType *gameType);
+
+   StringTableEntry mShutdownName;  // Name of user who iniated the shutdown
+   StringPtr mShutdownReason;       // Reason user provided for the shutdown
+   bool mShutdownInitiator;         // True if local client initiated shutdown (and can therefore cancel it)
+
+   bool mGotControlUpdate;
+
+   bool mFPSVisible;          // Are we displaying FPS info?
+   U32 mRecalcFPSTimer;       // Controls recalcing FPS running average
+
+   static const S32 WRONG_MODE_MSG_DISPLAY_TIME = 2500;
+   static const S32 FPS_AVG_COUNT = 32;
+
+   Timer mWrongModeMsgDisplay;               // Help if user is trying to use keyboard in joystick mode
+   Timer mInputModeChangeAlertDisplayTimer;  // Remind user that they just changed input modes
+   Timer mLevelInfoDisplayTimer;
+
+   void renderTimeLeft(const GameType *gameType);
+   void renderTalkingClients(const GameType *gameType);     // Render things related to voicechat
+   void renderDebugStatus(const GameType *gameType);        // Render things related to debugging
+
+
+   F32 mFPSAvg;
+   F32 mPingAvg;
+
+   U32 mIdleTimeDelta[FPS_AVG_COUNT];
+   U32 mPing[FPS_AVG_COUNT];
+   U32 mFrameIndex;
+
+   // Various helper objects
+   HelperMenu *mHelper;       // Current helper
+
+   QuickChatHelper mQuickChatHelper;
+   LoadoutHelper mLoadoutHelper;
+   EngineerHelper mEngineerHelper;
+
+   struct VoiceRecorder
+   {
+      private:
+         ClientGame *mGame;
+
+      public:
+         enum {
+            FirstVoiceAudioSampleTime = 250,
+            VoiceAudioSampleTime = 100,
+            MaxDetectionThreshold = 2048,
+         };
+
+      Timer mVoiceAudioTimer;
+      RefPtr<SoundEffect> mVoiceSfx;
+      RefPtr<VoiceEncoder> mVoiceEncoder;
+      bool mRecordingAudio;
+      U8 mWantToStopRecordingAudio;
+      S32 mMaxAudioSample;
+      S32 mMaxForGain;
+      ByteBufferPtr mUnusedAudio;
+
+      VoiceRecorder(ClientGame *game);
+      ~VoiceRecorder();
+
+      void idle(U32 timeDelta);
+      void process();
+      void start();
+      void stop();
+      void stopNow();
+      void render();
+   } mVoiceRecorder;
+
+   void dropItem();                       // User presses drop item key
+
+   bool mFiring;                          // Are we firing?
+   bool mModActivated[ShipModuleCount];
+
+   void setBusyChatting(bool busy);       // Tell the server we are (or are not) busy chatting
+
+   UIMode mCurrentMode;                   // Current game mode
+
+   static const S32 SERVER_MSG_FONT_SIZE = 14;
+   static const S32 SERVER_MSG_FONT_GAP = 4;
+   static const S32 CHAT_FONT_SIZE = 12;
+   static const S32 CHAT_FONT_GAP = 3;
+   static const S32 CHAT_MULTILINE_INDENT = 12;
+
+public:
+   GameUserInterface(ClientGame *game);    // Constructor
+   virtual ~GameUserInterface();                   // Destructor
+
+   bool displayInputModeChangeAlert;
+
+
+   bool isShowingMissionOverlay()  const { return mMissionOverlayActive; }    // Are game instructions (F2) visible?
+
+   void displayErrorMessage(const char *format, ...);
+   void displaySuccessMessage(const char *format, ...);
+
+   void displayMessage(const Color &msgColor, const char *message);
+   void displayMessagef(const Color &msgColor, const char *format, ...);
+   void displayChatMessage(const Color &msgColor, const char *format, ...);
+
+   void initializeLoadoutOptions(bool engineerAllowed) { mLoadoutHelper.initialize(engineerAllowed); }
+   void resetInputModeChangeAlertDisplayTimer(U32 timeInMs) { mInputModeChangeAlertDisplayTimer.reset(timeInMs); }
+
+   void render();                   // Render game screen
+   void renderReticle();            // Render crosshairs
+   void renderProgressBar();        // Render level-load progress bar
+   void renderMessageDisplay();     // Render incoming server msgs
+   void renderChatMessageDisplay(); // Render incoming chat msgs
+   void renderCurrentChat();        // Render chat msg user is composing
+   void renderLoadoutIndicators();  // Render indicators for the various loadout items
+   void renderShutdownMessage();    // Render an alert if server is shutting down
+   void renderLostConnectionMessage(); 
+
+   void renderBasicInterfaceOverlay(const GameType *gameType, bool scoreboardVisible);
+
+   void idle(U32 timeDelta);
+
+   void resetLevelInfoDisplayTimer() { mLevelInfoDisplayTimer.reset(6000); }     // 6 seconds 
+   void clearLevelInfoDisplayTimer() { mLevelInfoDisplayTimer.clear(); }
+
+   void runCommand(const char *input);
+   void issueChat();                // Send chat message (either Team or Global)
+   void cancelChat();
+
+   void shutdownInitiated(U16 time, const StringTableEntry &who, const StringPtr &why, bool initiator);
+   void cancelShutdown();
+
+   Vector<string> parseStringx(const char *str);    // Break a chat msg into parsable bits
+
+   void setVolume(VolumeType volType, const Vector<string> &words);
+
+   // Mouse handling
+   void onMouseDragged(S32 x, S32 y);
+   void onMouseMoved(S32 x, S32 y);
+   void onMouseMoved();
+
+   void onActivate();                 // Gets run when interface is first activated
+   void onReactivate();               // Gets run when interface is subsequently reactivated
+
+   
+   //ofstream mOutputFile;            // For saving downloaded levels
+   //FILE *mOutputFile;               // For saving downloaded levels
+
+   void onKeyDown(KeyCode keyCode, char ascii);
+   void onKeyUp(KeyCode keyCode);
+
+   void processPlayModeKey(KeyCode keyCode, char ascii);
+   void processChatModeKey(KeyCode keyCode, char ascii);
+
+
+   void advanceWeapon();            // Choose next weapon
+   void selectWeapon(U32 index);    // Choose weapon by its index
+
+   void suspendGame();
+   void unsuspendGame();
+
+   void enterMode(UIMode mode);     // Enter QuickChat, Loadout, or Engineer mode
+
+   void renderEngineeredItemDeploymentMarker(Ship *ship);
+
+   void receivedControlUpdate(bool recvd) { mGotControlUpdate = recvd; }
+
+   bool isInScoreboardMode() { return mInScoreboardMode; }
+
+   Move *getCurrentMove();
+   Timer mProgressBarFadeTimer;     // For fading out progress bar after level is loaded
+   bool mShowProgressBar;
+
+   // Message colors... (rest to follow someday)
+   static Color privateF5MessageDisplayedInGameColor;
+
+
+   // TODO: Move these to ClientGame???  They could really go anywhere!
+   static void mVolHandler(ClientGame *game, const Vector<string> &args);    
+   static void sVolHandler(ClientGame *game, const Vector<string> &args);    
+   static void vVolHandler(ClientGame *game, const Vector<string> &args);    
+   static void servVolHandler(ClientGame *game, const Vector<string> &args);  
+   static void getMapHandler(ClientGame *game, const Vector<string> &words);
+   static void nextLevelHandler(ClientGame *game, const Vector<string> &words);
+   static void prevLevelHandler(ClientGame *game, const Vector<string> &words);
+   static void restartLevelHandler(ClientGame *game, const Vector<string> &words);
+   static void shutdownServerHandler(ClientGame *game, const Vector<string> &words);
+   static void kickPlayerHandler(ClientGame *game, const Vector<string> &words);
+   static void adminPassHandler(ClientGame *game, const Vector<string> &words);
+   static void levelPassHandler(ClientGame *game, const Vector<string> &words);
+   static void showCoordsHandler(ClientGame *game, const Vector<string> &words);
+   static void showZonesHandler(ClientGame *game, const Vector<string> &words);
+   static void showPathsHandler(ClientGame *game, const Vector<string> &words);
+   static void pauseBotsHandler(ClientGame *game, const Vector<string> &words);
+   static void stepBotsHandler(ClientGame *game, const Vector<string> &words);
+   static void setAdminPassHandler(ClientGame *game, const Vector<string> &words);
+   static void setServerPassHandler(ClientGame *game, const Vector<string> &words);
+   static void setLevPassHandler(ClientGame *game, const Vector<string> &words);
+   static void setServerNameHandler(ClientGame *game, const Vector<string> &words);
+   static void setServerDescrHandler(ClientGame *game, const Vector<string> &words);
+   static void serverCommandHandler(ClientGame *game, const Vector<string> &words);
+   static void pmHandler(ClientGame *game, const Vector<string> &words);
+   static void muteHandler(ClientGame *game, const Vector<string> &words);
+   static void maxFpsHandler(ClientGame *game, const Vector<string> &words);
+   static void lineSmoothHandler(ClientGame *game, const Vector<string> &words);
+   static void lineWidthHandler(ClientGame *game, const Vector<string> &words);
+   static void suspendHandler(ClientGame *game, const Vector<string> &words);
+   static void deleteCurrentLevelHandler(ClientGame *game, const Vector<string> &words);
+   static void addTimeHandler(ClientGame *game, const Vector<string> &words);
+};
+
+
+};
+
+#endif
+
---
+++ zap/textItem.cpp
@@ -0,0 +1,539 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "textItem.h"
+#include "gameNetInterface.h"
+#include "gameObjectRender.h"    // For renderPointVector()
+#include "game.h"
+#include "item.h"
+#include "stringUtils.h"
+
+#ifndef ZAP_DEDICATED
+#include "ClientGame.h"
+#include "UIEditorMenus.h"       // For TextItemEditorAttributeMenuUI def
+#include "SDL/SDL_opengl.h"
+#endif
+
+
+
+#include <math.h>
+
+namespace Zap
+{
+
+TNL_IMPLEMENT_NETOBJECT(TextItem);
+
+
+#ifndef ZAP_DEDICATED
+EditorAttributeMenuUI *TextItem::mAttributeMenuUI = NULL;
+#endif
+
+// Constructor
+TextItem::TextItem()
+{
+   mNetFlags.set(Ghostable);
+   mObjectTypeNumber = TextItemTypeNumber;
+
+   // Some default values
+   mSize = 20;
+   mTeam = TEAM_NEUTRAL;
+}
+
+
+// Destructor
+TextItem::~TextItem()
+{
+   // Do nothing
+}
+
+
+TextItem *TextItem::clone() const
+{
+   return new TextItem(*this);
+
+   //copyAttrs(clone);
+
+   //return clone;
+}
+
+//
+//void TextItem::copyAttrs(TextItem *target)
+//{
+//   SimpleLine::copyAttrs(target);
+//
+//   target->mSize = mSize;
+//   target->mText = mText;
+//}
+
+
+void TextItem::newObjectFromDock(F32 gridSize)
+{
+   Parent::newObjectFromDock(gridSize);
+
+   mText = "Your text here";
+   recalcTextSize();
+}
+
+
+// In game rendering
+void TextItem::render()
+{
+#ifndef ZAP_DEDICATED
+   ClientGame *game = dynamic_cast<ClientGame *>(getGame());
+   
+   // Don't render opposing team's text items
+   if(!game || !game->getConnectionToServer() || !game->getGameType())
+      return;
+
+   Ship *ship = dynamic_cast<Ship *>(game->getConnectionToServer()->getControlObject());
+   if( (!ship && mTeam != -1) || (ship && ship->getTeam() != mTeam && mTeam != -1) )
+      return;
+
+   renderTextItem(getVert(0), getVert(1), mSize, mText, game->getTeamColor(mTeam));
+#endif
+}
+
+
+// Called by SimpleItem::renderEditor()
+void TextItem::renderEditorItem()
+{
+   renderTextItem(getVert(0), getVert(1), mSize, mText, getGame()->getTeamColor(mTeam));
+}
+
+
+// This object should be drawn below others
+S32 TextItem::getRenderSortValue()
+{
+   return 1;
+}
+
+
+// Create objects from parameters stored in level file
+// Entry looks like: TextItem 0 50 10 10 11 11 Message goes here
+bool TextItem::processArguments(S32 argc, const char **argv, Game *game)
+{
+   if(argc < 7)
+      return false;
+
+   mTeam = atoi(argv[0]);
+
+   Point pos, dir;
+
+   pos.read(argv + 1);
+   pos *= game->getGridSize();
+
+   dir.read(argv + 3);
+   dir *= game->getGridSize();
+
+   setVert(pos, 0);
+   setVert(dir, 1);
+
+   mSize = (F32)atof(argv[5]);
+   mSize = max(min(mSize, (F32)MAX_TEXT_SIZE), (F32)MIN_TEXT_SIZE);      // Note that same line exists below, in recalcXXX()... combine?
+
+   // Assemble any remainin args into a string
+   mText = "";
+   for(S32 i = 6; i < argc; i++)
+   {
+      mText += argv[i];
+      if(i < argc - 1)
+         mText += " ";
+   }
+
+   computeExtent();
+
+   return true;
+}
+
+
+string TextItem::toString(F32 gridSize) const
+{
+   return string(getClassName()) + " " + itos(mTeam) + " " + geomToString(gridSize) + " " + ftos(mSize, 3) + " " + mText;
+}
+
+
+// Editor
+void TextItem::recalcTextSize()
+{
+#ifndef ZAP_DEDICATED
+   const F32 dummyTextSize = 120;
+
+   F32 lineLen = getVert(0).distanceTo(getVert(1));      // In in-game units
+   F32 strWidth = F32(UserInterface::getStringWidth(dummyTextSize, mText.c_str())) / dummyTextSize; 
+   F32 size = lineLen / strWidth;
+
+   // Compute text size subject to min and max defined in TextItem
+   mSize = max(min(size, (F32)MAX_TEXT_SIZE), (F32)MIN_TEXT_SIZE);
+#endif
+}
+
+
+void TextItem::onAddedToGame(Game *theGame)
+{
+   Parent::onAddedToGame(theGame);
+
+   if(!isGhost())
+      setScopeAlways();
+}
+
+
+// Bounding box for quick collision-possibility elimination, and display scoping purposes
+void TextItem::computeExtent()
+{
+#ifdef ZAP_DEDICATED
+   // don't care much about it in server, as server won't render, and does not do colliding to textTiem
+	setExtent(Rect(getVert(0), getVert(1)));
+#else
+   F32 len = UserInterface::getStringWidth(mSize, mText.c_str());
+   //F32 buf = mSize / 2;     // Provides some room to accomodate descenders on letters like j and g.
+
+   F32 angle =  getVert(0).angleTo(getVert(1));
+   F32 sinang = sin(angle);
+   F32 cosang = cos(angle);
+
+   F32 descenderFactor = .35f;    // To account for y, g, j, etc.
+   F32 h = mSize * (1 + descenderFactor);
+   F32 w = len * 1.05f;           // 1.05 adds just a little horizontal padding for certain words with trailing ys or other letters that are just a tiny bit longer than calculated
+   F32 x = getVert(0).x + mSize * descenderFactor * sinang;
+   F32 y = getVert(0).y + mSize * descenderFactor * cosang;
+
+   F32 c1x = x - h * sinang * .5f;
+   F32 c1y = y;
+
+   F32 c2x = x + w * cosang - h * sinang * .5f;
+   F32 c2y = y + w * sinang;
+
+   F32 c3x = x + h * sinang * .5f + w * cosang;
+   F32 c3y = y - h * cosang + w * sinang;
+
+   F32 c4x = x + h * sinang * .5f;
+   F32 c4y = y - h * cosang;
+
+   F32 minx = min(c1x, min(c2x, min(c3x, c4x)));
+   F32 miny = min(c1y, min(c2y, min(c3y, c4y)));
+   F32 maxx = max(c1x, max(c2x, max(c3x, c4x)));
+   F32 maxy = max(c1y, max(c2y, max(c3y, c4y)));
+
+   Rect extent(Point(minx, miny), Point(maxx, maxy));
+
+   setExtent(extent);
+#endif
+}
+
+
+bool TextItem::getCollisionPoly(Vector<Point> &polyPoints) const
+{
+   return false;
+}
+
+// Handle collisions with a TextItem.  Easy, there are none.
+bool TextItem::collide(GameObject *hitObject)
+{
+   return false;
+}
+
+void TextItem::idle(GameObject::IdleCallPath path)
+{
+   // Laze about, read a book, take a nap, whatever.
+}
+
+U32 TextItem::packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream)
+{
+   Point pos = getVert(0);
+   Point dir = getVert(1);
+
+   pos.write(stream);
+   dir.write(stream);
+
+   stream->writeRangedU32((U32)mSize, 0, MAX_TEXT_SIZE);
+   writeThisTeam(stream);
+
+   stream->writeString(mText.c_str(), (U8) mText.length());      // Safe to cast text.length to U8 because we've limited it's length to MAX_TEXTITEM_LEN
+
+   return 0;
+}
+
+
+void TextItem::unpackUpdate(GhostConnection *connection, BitStream *stream)
+{
+   char txt[MAX_TEXTITEM_LEN];
+
+   Point pos, dir;
+
+   pos.read(stream);
+   dir.read(stream);
+
+   setVert(pos, 0);
+   setVert(dir, 1);
+
+   mSize = (F32)stream->readRangedU32(0, MAX_TEXT_SIZE);
+   readThisTeam(stream);
+
+   stream->readString(txt);
+
+   mText = txt;
+   computeExtent();
+}
+
+
+///// Editor Methods
+
+// Runs when text is being changed in the editor
+void TextItem::onAttrsChanging()
+{
+   onGeomChanged();
+}
+
+void TextItem::onAttrsChanged()
+{
+   onGeomChanged();
+}
+
+void TextItem::onGeomChanging()
+{
+   onGeomChanged();
+}
+
+void TextItem::onGeomChanged()
+{
+   recalcTextSize();
+}
+
+
+#ifndef ZAP_DEDICATED
+// Static method: Provide hook into the object currently being edited with the attrubute editor for callback purposes
+EditorObject *TextItem::getAttributeEditorObject()     
+{
+   return mAttributeMenuUI->getObject(); 
+}
+
+
+EditorAttributeMenuUI *TextItem::getAttributeMenu(ClientGame *game)
+{
+   // Lazily initialize this -- if we're in the game, we'll never need this to be instantiated
+   if(!mAttributeMenuUI)
+      mAttributeMenuUI = new TextItemEditorAttributeMenuUI(game);
+
+   // Udate the editor with attributes from our current object
+   mAttributeMenuUI->menuItems[0]->setValue(mText);
+
+   return mAttributeMenuUI;
+}
+#endif
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+TNL_IMPLEMENT_NETOBJECT(LineItem);
+
+// Why does GCC need this for signed int?
+#ifndef TNL_OS_WIN32
+const S32 LineItem::MIN_LINE_WIDTH;
+const S32 LineItem::MAX_LINE_WIDTH;
+#endif
+
+// Constructor
+LineItem::LineItem()
+{ 
+   mNetFlags.set(Ghostable);
+   mObjectTypeNumber = LineTypeNumber;
+}
+
+
+LineItem *LineItem::clone() const
+{
+   return new LineItem(*this);
+}
+
+//
+//// Copy constructor -- make sure each copy gets its own geometry object
+//LineItem::LineItem(const LineItem &li)
+//{
+//   mGeometry = boost::shared_ptr<Geometry>(new PolylineGeometry(*((PolylineGeometry *)li.mGeometry.get())));  
+//}
+//
+
+void LineItem::render()
+{
+#ifndef ZAP_DEDICATED
+   GameConnection *gc = dynamic_cast<ClientGame *>(getGame())->getConnectionToServer();
+   // Don't render opposing team's text items
+   if(!gc)      // Not sure if this is really needed...
+      return;
+
+   Ship *ship = dynamic_cast<Ship *>(gc->getControlObject());
+   if( (!ship && mTeam != -1) || (ship && ship->getTeam() != mTeam && mTeam != -1) )
+      return;
+
+   glColor(getGame()->getTeamColor(mTeam));
+   renderPointVector(getOutline(), GL_LINE_STRIP);
+#endif
+}
+
+
+void LineItem::renderEditor(F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+   if(!mSelected)
+      glColor(getEditorRenderColor());
+   else
+      glColor(SELECT_COLOR);
+
+   renderPointVector(getOutline(), GL_LINE_STRIP);
+
+   renderLinePolyVertices(currentScale);
+#endif
+}
+
+
+const Color *LineItem::getEditorRenderColor() const 
+{ 
+   return getGame()->getTeamColor(mTeam); 
+}
+
+
+// This object should be drawn below others
+S32 LineItem::getRenderSortValue()
+{
+   return 1;
+}
+
+
+// Create objects from parameters stored in level file
+// Entry looks like: LineItem 0 50 10 10 11 11 Message goes here
+bool LineItem::processArguments(S32 argc, const char **argv, Game *game)
+{
+   if(argc < 6)
+      return false;
+
+   mTeam = atoi(argv[0]);
+   setWidth(atoi(argv[1]));
+   readGeom(argc, argv, 2, game->getGridSize());
+
+   computeExtent();
+
+   return true;
+}
+
+
+string LineItem::toString(F32 gridSize) const
+{
+   return string(getClassName()) + " " + itos(mTeam) + " " + itos(getWidth()) + " " + geomToString(gridSize);
+}
+
+
+void LineItem::onAddedToGame(Game *game)
+{
+   Parent::onAddedToGame(game);
+
+   if(!isGhost())
+      setScopeAlways();
+}
+
+
+// Bounding box for quick collision-possibility elimination, and display scoping purposes
+void LineItem::computeExtent()
+{
+   setExtent();
+}
+
+
+bool LineItem::getCollisionPoly(Vector<Point> &polyPoints) const
+{
+   return false;
+}
+
+
+// Handle collisions with a LineItem.  Easy, there are none.
+bool LineItem::collide(GameObject *hitObject)
+{
+   return false;
+}
+
+
+void LineItem::idle(GameObject::IdleCallPath path)
+{
+   // Do nothing
+}
+
+
+U32 LineItem::packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream)
+{
+   //stream->writeRangedU32(mWidth, 0, MAX_LINE_WIDTH);
+   writeThisTeam(stream);
+
+   packGeom(connection, stream);
+
+   return 0;
+}
+
+
+void LineItem::unpackUpdate(GhostConnection *connection, BitStream *stream)
+{
+   //mWidth = stream->readRangedU32(0, MAX_LINE_WIDTH);
+   readThisTeam(stream);
+
+   unpackGeom(connection, stream);
+   setExtent();
+}
+
+
+void LineItem::setWidth(S32 width, S32 min, S32 max)
+{
+   // Bounds check
+   if(width < min)
+      width = min;
+   else if(width > max)
+      width = max; 
+
+   mWidth = width; 
+}
+
+
+void LineItem::setWidth(S32 width) 
+{         
+   setWidth(width, LineItem::MIN_LINE_WIDTH, LineItem::MAX_LINE_WIDTH);
+}
+
+
+void LineItem::changeWidth(S32 amt)
+{
+   S32 width = mWidth;
+
+   if(amt > 0)
+      width += amt - (S32) width % amt;    // Handles rounding
+   else
+   {
+      amt *= -1;
+      width -= ((S32) width % amt) ? (S32) width % amt : amt;      // Dirty, ugly thing
+   }
+
+   setWidth(width);
+   onGeomChanged();
+}
+
+
+};
