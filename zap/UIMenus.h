e0324da5395b28e0f0b5156abcba4bc079d4ddf7
watusimoto <watusimoto@bitfighter.org>
2011-09-22 00:18:09 +0200

    Make cmd line settings private -- no need for larger game to be worried about these.  Unfortunately, this requires a huge amount of cleanup.  Also try a more streamlined cmd line parameter handling system, with less tedious busywork.  Related moving stuff around as makes sense post refactor.  Rename a few methods.  Tons of comments; I was feeling verbose.

---
+++ zap/UIMenus.h
@@ -0,0 +1,367 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _UIMENUS_H_
+#define _UIMENUS_H_
+
+#ifndef ZAP_DEDICATED
+
+#include "UI.h"
+#include "input.h"         // For InputMode def
+#include "UIMenuItems.h"
+#include "tnlNetConnection.h" // for TerminationReason
+
+
+namespace Zap
+{
+
+using namespace std;
+
+
+// This class is the template for most all of our menus...
+class MenuUserInterface : public UserInterface
+{
+   typedef UserInterface Parent;
+
+private:
+   S32 getYStart();     // Get vert pos of first menu item
+   S32 getOffset(); 
+   Timer mScrollTimer;
+
+   // For detecting keys being held down
+   bool mRepeatMode;
+   bool mKeyDown;
+
+   virtual S32 getTextSize() { return 23; }              // Let menus set their own text size
+   virtual S32 getGap() { return 18; }
+
+   virtual void renderExtras() { /* Do nothing */ }      // For drawing something extra on a menu
+   void advanceItem();                                   // What happens when we move on to the next menu item?
+
+protected:
+   S32 currOffset;
+
+   bool mRenderInstructions;
+
+   // Handle keyboard input while a menu is displayed
+   virtual bool processMenuSpecificKeys(KeyCode keyCode, char ascii);
+   virtual bool processKeys(KeyCode keyCode, char ascii);
+
+public:
+   MenuUserInterface(ClientGame *game);      // Constructor
+
+   bool itemSelectedWithMouse;
+   Vector<boost::shared_ptr<MenuItem> > menuItems;
+
+   static const S32 MOUSE_SCROLL_INTERVAL = 100;
+
+   string mMenuTitle;
+   string mMenuSubTitle;
+
+   Color mMenuSubTitleColor;
+   bool mMenuFooterContainsInstructions;
+
+   void idle(U32 timeDelta); 
+
+   S32 selectedIndex;
+
+   void render();    // Draw the basic menu
+   void onKeyDown(KeyCode keyCode, char ascii);
+   void onKeyUp(KeyCode keyCode);
+   void onMouseMoved(S32 x, S32 y) { onMouseMoved(); }      // Redirect to argless version
+   void onMouseMoved();
+   void processMouse();
+
+   void onActivate();
+   void onReactivate();
+
+   virtual void onEscape();
+};
+
+//////////
+// <--- DO NOT SUBCLASS MainMenuUserInterface!! (unless you override onActivate) ---> //
+class MainMenuUserInterface : public MenuUserInterface
+{
+   typedef MenuUserInterface Parent;
+
+private:
+   char mMOTD[MOTD_LEN];
+   U32 motdArriveTime;
+   Timer mFadeInTimer;        // Track the brief fade in interval the first time menu is shown
+   Timer mColorTimer;
+   Timer mColorTimer2;
+   enum {
+      FadeInTime = 400,       // Time that fade in lasts (ms) 
+      ColorTime = 1000,
+      ColorTime2 = 1700,
+   };
+   bool mTransDir;
+   bool mTransDir2;
+   bool mNeedToUpgrade;       // True if client is out of date and needs to upgrade, false if we're on the latest version
+   bool mShowedUpgradeAlert;  // So we don't show the upgrade message more than once
+
+   void renderExtras();
+
+public:
+   MainMenuUserInterface(ClientGame *game);           // Constructor
+   void processSelection(U32 index);
+   void onEscape();
+   void render();
+   void idle(U32 timeDelta); 
+   void setMOTD(const char *motd);              // Message of the day, from Master
+   void onActivate();
+   void setNeedToUpgrade(bool needToUpgrade);   // Is client in need of an upgrade?
+
+   bool showAnimation;                          // Is this the first time the menu is shown?
+   bool mFirstTime;
+   void showUpgradeAlert();                     // Display message to the user that it is time to upgrade
+   bool getNeedToUpgrade();
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class OptionsMenuUserInterface : public MenuUserInterface
+{
+   typedef MenuUserInterface Parent;
+
+public:
+   OptionsMenuUserInterface(ClientGame *game);        // Constructor
+   void processSelection(U32 index) { }         // Process selected menu item when right arrow is pressed
+   void processShiftSelection(U32 index) { }    // And when the left arrow is pressed
+   void onEscape();
+   void setupMenus();
+   void onActivate();
+   void toggleDisplayMode();
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class HostMenuUserInterface : public MenuUserInterface
+{
+   enum MenuItems {
+      OPT_HOST,
+      OPT_NAME,
+      OPT_DESCR,
+      OPT_LVL_PASS,
+      OPT_ADMIN_PASS,
+      OPT_PASS,
+      OPT_GETMAP,
+      OPT_MAX_PLAYERS,
+      OPT_PORT,
+      OPT_COUNT
+   };
+
+   static const S32 FIRST_EDITABLE_ITEM = OPT_NAME;
+
+   typedef MenuUserInterface Parent;
+
+private:
+   Vector<string> mLevelLoadDisplayNames;    // For displaying levels as they're loaded in host mode
+   S32 mLevelLoadDisplayTotal;
+   S32 mEditingIndex;                        // Index of item we're editing, -1 if none
+
+public:
+   HostMenuUserInterface(ClientGame *game);        // Constructor
+   void onEscape();
+   void setupMenus();
+   void onActivate();
+   void render();
+   void saveSettings();       // Save parameters in INI file
+
+   // For displaying the list of levels as we load them:
+   Timer levelLoadDisplayFadeTimer;
+   bool levelLoadDisplayDisplay;
+   void addProgressListItem(string item);
+   void renderProgressListItems();
+   void clearLevelLoadDisplay();
+};
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class NameEntryUserInterface : public MenuUserInterface
+{
+private:
+   typedef MenuUserInterface Parent;
+   void renderExtras();
+   NetConnection::TerminationReason mReason;
+
+public:
+   NameEntryUserInterface(ClientGame *game);    // Constructor
+   void processSelection(U32 index) { }         // Process selected menu item when right arrow is pressed
+   void processShiftSelection(U32 index) { }    // And when the left arrow is pressed
+   void onEscape();
+   void setupMenu();
+   void onActivate();
+   void setReactivationReason(NetConnection::TerminationReason r);
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class GameType;
+
+class GameMenuUserInterface : public MenuUserInterface
+{
+   enum {
+      OPT_OPTIONS,
+      OPT_HELP,
+      OPT_ADMIN,
+      OPT_KICK,
+      OPT_LEVEL_CHANGE_PW,
+      OPT_CHOOSELEVEL,
+      OPT_ADD2MINS,
+      OPT_RESTART,
+      OPT_QUIT
+   };
+
+private:
+   typedef MenuUserInterface Parent;
+   SafePtr<GameType> mGameType;
+   InputMode lastInputMode;
+   void buildMenu();
+
+public:
+   GameMenuUserInterface(ClientGame *game);            // Constructor
+
+   void idle(U32 timeDelta);
+   void onActivate();
+   void onReactivate();
+   void onEscape();
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class LevelMenuUserInterface : public MenuUserInterface
+{
+private:
+   typedef MenuUserInterface Parent;
+
+public:
+   LevelMenuUserInterface(ClientGame *game);        // Constructor
+   void onActivate();
+   void onEscape();
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class LevelMenuSelectUserInterface : public MenuUserInterface
+{
+   typedef MenuUserInterface Parent;
+
+private:
+   Vector<string> mLevels;
+
+public:
+   LevelMenuSelectUserInterface(ClientGame *game);        // Constructor
+   string category;
+   void onActivate();
+   bool processMenuSpecificKeys(KeyCode keyCode, char ascii);  // Custom key handling for level selection menus
+
+   void processSelection(U32 index);
+   void onEscape();
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class AdminMenuUserInterface : public MenuUserInterface
+{
+   typedef MenuUserInterface Parent;
+
+private:
+   SafePtr<GameType> mGameType;
+
+public:
+   AdminMenuUserInterface();      // Constructor
+   void onActivate();
+   void processSelection(U32 index);
+   void processShiftSelection(U32 index);
+   void onEscape();
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class PlayerMenuUserInterface : public MenuUserInterface
+#else
+class PlayerMenuUserInterface
+#endif
+{
+#ifndef ZAP_DEDICATED
+   typedef MenuUserInterface Parent;
+
+public:
+
+
+   PlayerMenuUserInterface(ClientGame *game);        // Constructor
+
+   void render();
+   void playerSelected(U32 index);
+   void onEscape();
+#endif
+
+public:
+   enum Action { // This is needed in ZAP_DEDICATED for use in GameConnection::c2sAdminPlayerAction
+      Kick,
+      ChangeTeam,
+      ActionCount,
+   } action;
+};
+
+#ifndef ZAP_DEDICATED
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class TeamMenuUserInterface : public MenuUserInterface
+{
+   typedef MenuUserInterface Parent;
+
+public:
+   TeamMenuUserInterface(ClientGame *game);        // Constructor
+   void render();
+   void onEscape();
+   string nameToChange;
+
+   void processSelection(U32 index);
+};
+
+};
+
+#endif
+#endif
+
---
+++ zap/loadoutZone.cpp
@@ -0,0 +1,179 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "loadoutZone.h"
+#include "game.h"
+#include "stringUtils.h"
+
+namespace Zap
+{
+
+TNL_IMPLEMENT_NETOBJECT(LoadoutZone);
+
+const char LoadoutZone::className[] = "LoadoutZone";      // Class name as it appears to Lua scripts
+
+// Define the methods we will expose to Lua
+Lunar<LoadoutZone>::RegType LoadoutZone::methods[] =
+{
+   // Standard gameItem methods
+   method(LoadoutZone, getClassID),
+   method(LoadoutZone, getLoc),
+   method(LoadoutZone, getRad),
+   method(LoadoutZone, getVel),
+   method(LoadoutZone, getTeamIndx),
+
+   {0,0}    // End method list
+};
+
+
+// C++ constructor
+LoadoutZone::LoadoutZone()
+{
+   mTeam = 0;
+   mNetFlags.set(Ghostable);
+   mObjectTypeNumber = LoadoutZoneTypeNumber;
+}
+
+
+LoadoutZone *LoadoutZone::clone() const
+{
+   return new LoadoutZone(*this);
+}
+
+
+void LoadoutZone::render()
+{
+   renderLoadoutZone(getGame()->getTeamColor(getTeam()), getOutline(), getFill(), getCentroid(), getLabelAngle());
+}
+
+
+void LoadoutZone::renderEditor(F32 currentScale)
+{
+   render();
+   EditorPolygon::renderEditor(currentScale);
+}
+
+
+void LoadoutZone::renderDock()
+{
+  renderLoadoutZone(getGame()->getTeamColor(getTeam()), getOutline(), getFill());
+}
+
+
+S32 LoadoutZone::getRenderSortValue()
+{
+   return -1;
+}
+
+
+// Create objects from parameters stored in level file
+bool LoadoutZone::processArguments(S32 argc2, const char **argv2, Game *game)
+{
+   // Need to handle or ignore arguments that starts with letters,
+   // so a possible future version can add parameters without compatibility problem.
+   S32 argc = 0;
+   const char *argv[65]; // 32 * 2 + 1 = 65
+   for(S32 i = 0; i < argc2; i++)  // the idea here is to allow optional R3.5 for rotate at speed of 3.5
+   {
+      char c = argv2[i][0];
+      //switch(c)
+      //{
+      //case 'A': Something = atof(&argv2[i][1]); break;  // using second char to handle number
+      //}
+      if((c < 'a' || c > 'z') && (c < 'A' || c > 'Z'))
+      {
+         if(argc < 65)
+         {  argv[argc] = argv2[i];
+            argc++;
+         }
+      }
+   }
+
+   if(argc < 7)
+      return false;
+
+   mTeam = atoi(argv[0]);     // Team is first arg
+   readGeom(argc, argv, 1, game->getGridSize());
+
+   setExtent();
+
+   return true;
+}
+
+
+string LoadoutZone::toString(F32 gridSize) const
+{
+   return string(getClassName()) + " " + itos(mTeam) + " " + geomToString(gridSize);
+}
+
+
+void LoadoutZone::onAddedToGame(Game *theGame)
+{
+   Parent::onAddedToGame(theGame);
+
+   if(!isGhost())
+      setScopeAlways();
+}
+
+
+// More precise boundary for precise collision detection
+bool LoadoutZone::getCollisionPoly(Vector<Point> &polyPoints) const
+{
+   polyPoints = *getOutline();
+   return true;
+}
+
+// Only gets run on the server, never on client
+bool LoadoutZone::collide(GameObject *hitObject)
+{
+   // Anyone can use neutral loadout zones (team == -1)
+   if(!isGhost() &&
+         (hitObject->getTeam() == getTeam() || getTeam() == -1) &&
+         isShipType(hitObject->getObjectTypeNumber()) )
+      getGame()->getGameType()->updateShipLoadout(hitObject);      
+
+   return false;
+}
+
+
+U32 LoadoutZone::packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream)
+{
+   writeThisTeam(stream);
+   packGeom(connection, stream);
+   return 0;
+}
+
+
+void LoadoutZone::unpackUpdate(GhostConnection *connection, BitStream *stream)
+{
+   readThisTeam(stream);
+   unpackGeom(connection, stream);
+   setExtent();
+}
+
+
+};
+
+
