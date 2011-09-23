256e2d68350f040ee6ce5af8938620aa2b6c2adb
watusimoto <watusimoto@bitfighter.org>
2011-09-24 00:46:50 +0200

    Reduce churn of clientRef objects -- now that they're stored on the Game object, they don't need to be deleted and recreated every time we change a level.  Still some optimizations possible.  Also fixed name entry screen I broke earlier.  Fixed crash where user starts chat cmd by hitting "/" then hits tab.

---
+++ zap/SoundEffect.h
@@ -0,0 +1,54 @@
+/*
+ * SoundEffect.h
+ *
+ *  Created on: May 8, 2011
+ *      Author: dbuck
+ */
+
+#ifndef SOUNDEFFECT_H_
+#define SOUNDEFFECT_H_
+
+#include "Point.h"
+#include "SFXProfile.h"
+#include "tnlTypes.h"
+#include "tnlByteBuffer.h"
+
+// forward declarations
+
+namespace TNL {
+   template <class T> class RefPtr;
+   class Object;
+};
+
+using namespace TNL;
+
+namespace Zap {
+
+class SoundEffect : public TNL::Object
+{
+public:
+   static Point mListenerPosition;
+   static Point mListenerVelocity;
+   static F32 mMaxDistance;
+   static S32 mCaptureGain;
+
+   U32 mSFXIndex;
+   Point mPosition;
+   Point mVelocity;
+   ByteBufferPtr mInitialBuffer;
+   SFXProfile *mProfile;
+   F32 mGain;
+   S32 mSourceIndex;
+   F32 mPriority;
+
+   SoundEffect(U32 profileIndex, ByteBufferPtr ib, F32 gain, Point position, Point velocity);
+   virtual ~SoundEffect();
+
+   bool isPlaying() { return mSourceIndex != -1; }
+};
+
+typedef RefPtr<SoundEffect> SFXHandle;
+
+}
+
+#endif /* SOUNDEFFECT_H_ */
---
+++ zap/luaUtil.cpp
@@ -0,0 +1,100 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "luaUtil.h"
+
+namespace Zap
+{
+
+
+const char LuaUtil::className[] = "LuaUtil";      // Class name as it appears to Lua scripts
+
+
+// Lua Constructor
+LuaUtil::LuaUtil(lua_State *L)
+{
+   // Do nothing
+}
+
+
+// Define the methods we will expose to Lua... basically everything we want to use in lua code
+// like LuaUtil:blah needs to be defined here.
+Lunar<LuaUtil>::RegType LuaUtil::methods[] =
+{
+   method(LuaUtil, getMachineTime),
+   method(LuaUtil, logprint),
+   method(LuaUtil, getRandomNumber),
+
+   {0,0}    // End method list
+};
+
+
+// Write a message to the server logfile
+S32 LuaUtil::logprint(lua_State *L)
+{
+   static const char *methodName = "LuaUtil:logprint()";
+   checkArgCount(L, 2, methodName);
+
+   logprintf(LogConsumer::LuaBotMessage, "%s: %s", getString(L, 1, methodName), getString(L, 2, methodName));
+   return 0;
+}
+
+
+S32 LuaUtil::getRandomNumber(lua_State *L)
+{
+   S32 args = lua_gettop(L);
+
+   static const char *methodName = "LuaUtil:getRandomNumber()";
+   checkArgCount(L, 2, methodName);
+
+   if(lua_isnil(L, 1))
+   {
+      lua_pop(L, 1);
+      lua_pop(L, 2);
+
+      return returnFloat(L, TNL::Random::readF());
+   }
+
+   S32 min = 1;
+   S32 max = 0;
+
+   if(lua_isnil(L,2))
+      max = luaL_checkint(L, 1); 
+   else
+   {
+      min = luaL_checkint(L, 1);
+      max = luaL_checkint(L, 2);
+   }
+
+   lua_pop(L, 1);
+   lua_pop(L, 2);
+
+   return returnInt(L, TNL::Random::readI(min, max));
+}
+
+
+};
+
+
