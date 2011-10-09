14857413709316c597aab9aa64b36878f523f760
sam8641 <sam8641@localhost>
2011-10-09 13:42:41 -0500

    Make Mass work, move mMass to MoveObject

---
+++ resource/scripts/robot_helper_functions.lua
@@ -0,0 +1,146 @@
+-------------------------------------------------------------------------------
+--
+-- Bitfighter - A multiplayer vector graphics space game
+-- Based on Zap demo released for Torque Network Library by GarageGames.com
+--
+-- Copyright (C) 2008-2009 Chris Eykamp
+-- Other code copyright as noted
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful (and fun!),
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- These functions will be included with every robot automatically.
+-- Do not tinker with these unless you are sure you know what you are doing!!
+-- And even then, be careful!
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+--
+-- Create a reference to our bot
+--
+bot = LuaRobot(Robot)
+
+
+--
+-- Main function run when robot starts, before getName().  By default does nothing.  Override in bot scripts.
+--
+function _main()
+   if _declared("main") and type(main) == "function" then
+      main()
+   end
+end
+
+
+--
+-- Default robot name, can and should be overridden by user robots, but we need to have something...
+--
+function getName()
+    return("FancyNancy")
+end
+
+--
+-- Wrap getFiringSolution with some code that helps C++ sort out what type of item
+-- we're handing it
+--
+function getFiringSolution(item)
+    if(item == nil) then
+        return nil
+    end
+
+    local itemtype = item:getClassID()
+    if(itemtype == nil) then
+        return nil
+    end
+
+    return bot:getFiringSolution(itemtype, item)
+end
+
+
+--
+-- Convenience function: find closest item in a list of items
+-- Will return nil if items has 0 elements
+-- If teamIndx is specified, will only include items on team
+--
+function findClosest(items, teamIndx)
+
+   local closest = nil
+   local minDist = 999999999
+   local loc = bot:getLoc()
+
+   for indx, item in ipairs(items) do              -- Iterate over our list
+
+--logprint(tostring(teamIndx)..","..item:getTeamIndx())
+      if teamIndx == nil or item:getTeamIndx() == teamIndx then
+
+         -- Use distSquared because it is less computationally expensive
+         -- and works great for comparing distances
+         local d = loc:distSquared(item:getLoc())    -- Dist btwn robot and TestItem
+
+         if d < minDist then                         -- Is it the closest yet?
+            closest = item
+            minDist = d
+         end
+      end
+   end
+
+   return closest
+end
+
+
+--
+-- Convenience function... let user use logprint directly, without referencing luaUtil
+--
+function logprint(msg)
+    luaUtil:logprint("Robot", tostring(msg))
+end
+
+--
+-- And another
+--
+function subscribe(event)
+   bot:subscribe(event)
+end   
+
+
+
+--
+-- This will be called every tick... update timer, then call robot's onTick() method if it exists
+--
+function _onTick(self, deltaT)
+   Timer:_tick(deltaT)     -- Really should only be called once for all bots
+
+   if _declared("onTick") and type(onTick) == "function" then
+      onTick(self, deltaT)
+   end
+
+   -- TODO: Here for compatibility with older bots.  Remove this in a later release
+   if _declared("getMove") and type(getMove) == "function" then
+      getMove(self, deltaT)
+   end
+
+end
+
+
+--
+-- Let the log know that this file was processed correctly
+--
+logprint("Loaded robot helper functions...")
---
+++ zap/UIMenuItems.cpp
@@ -0,0 +1,488 @@
+
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+
+#include "UIMenuItems.h"
+#include "UI.h"
+#include "ScreenInfo.h"
+
+#include "SDL/SDL_opengl.h"
+
+namespace Zap
+{
+
+// Constructor
+MenuItem::MenuItem(ClientGame *game, S32 index, const string &prompt, void (*callback)(ClientGame *, U32), const string &help, KeyCode k1, KeyCode k2)
+{
+   mGame = game;
+   mPrompt = prompt;
+   key1 = k1;
+   key2 = k2;
+   mCallback = callback;
+   mHelp = help;
+   mIndex = (U32)index;
+   mEnterAdvancesItem = false;
+   mSelectedColor = Colors::yellow;
+   mUnselectedColor = Colors::white;
+   mPromptAppendage = " >";
+}
+
+
+// Destructor
+MenuItem::~MenuItem() 
+{ 
+   // Do nothing
+} 
+
+
+// Shouldn't need to be overridden -- all redering routines should include xpos
+void MenuItem::render(S32 ypos, S32 textsize, bool isSelected)
+{
+   render(gScreenInfo.getGameCanvasWidth() / 2, ypos, textsize, isSelected);
+}
+
+
+const Color *MenuItem::getColor(bool isSelected)
+{
+   return isSelected ? &mSelectedColor : &mUnselectedColor;
+}
+
+
+void MenuItem::render(S32 xpos, S32 ypos, S32 textsize, bool isSelected)
+{
+   glColor(*getColor(isSelected));
+   UserInterface::drawCenteredStringf(xpos, ypos, textsize, "%s%s", getPrompt().c_str(), mPromptAppendage);
+}
+
+
+S32 MenuItem::getWidth(S32 textsize)
+{
+   return UserInterface::getStringWidthf(textsize, "%s%s", getPrompt().c_str(), mPromptAppendage);
+}
+
+
+bool MenuItem::handleKey(KeyCode keyCode, char ascii)
+{
+   if(keyCode == KEY_ENTER || keyCode == KEY_SPACE || keyCode == KEY_RIGHT || keyCode == MOUSE_LEFT)
+   {
+      UserInterface::playBoop();
+      if(mCallback)
+         mCallback(mGame, mIndex);
+
+      return true;
+   }
+   else
+   {
+      // Check individual entries for any shortcut keys
+      return false;
+   }
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+// Constructor
+ValueMenuItem::ValueMenuItem(ClientGame *game, S32 index, const string &text, void (*callback)(ClientGame *, U32), 
+                             const string &help, KeyCode k1, KeyCode k2) :
+      Parent(game, index, text, callback, help, k1, k2)
+{
+   mSelectedValueColor = Colors::cyan;
+   mUnselectedValueColor = Colors::cyan;
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+ToggleMenuItem::ToggleMenuItem(ClientGame *game, string title, Vector<string> options, U32 currOption, bool wrap, void (*callback)(ClientGame *, U32), string help, KeyCode k1, KeyCode k2) :
+      ValueMenuItem(game, -1, title, callback, help, k1, k2)
+{
+   mValue = "";
+   mIndex = currOption;
+   mOptions = options;
+   mWrap = wrap;
+   mEnterAdvancesItem = true;
+}
+
+
+string ToggleMenuItem::getOptionText()
+{
+   return mIndex < U32(mOptions.size()) ? mOptions[mIndex] : "INDEX OUT OF RANGE";
+}
+
+
+void ToggleMenuItem::render(S32 xpos, S32 ypos, S32 textsize, bool isSelected)
+{
+   UserInterface::drawCenteredStringPair(xpos, ypos, textsize, *getColor(isSelected), *getValueColor(isSelected), 
+                                         getPrompt().c_str(), getOptionText().c_str());
+}
+
+
+S32 ToggleMenuItem::getWidth(S32 textsize)
+{
+   return UserInterface::getStringPairWidth(textsize, getPrompt().c_str(), getOptionText().c_str());
+}
+
+
+bool ToggleMenuItem::handleKey(KeyCode keyCode, char ascii)
+{
+   U32 nextValAfterWrap = mWrap ? 0 : mIndex;
+
+   if(keyCode == KEY_RIGHT || keyCode == MOUSE_LEFT)
+   {
+      mIndex = (mIndex == (U32)mOptions.size() - 1) ? nextValAfterWrap : mIndex + 1;
+
+      if(mCallback)
+         mCallback(getGame(), mIndex);
+
+      UserInterface::playBoop();
+      return true;
+   }
+   else if(keyCode == KEY_LEFT || keyCode == MOUSE_RIGHT)
+   {      
+      U32 nextValAfterWrap = mWrap ? mOptions.size() - 1 : mIndex;
+      mIndex = (mIndex == 0) ? nextValAfterWrap : mIndex - 1;
+      
+      if(mCallback)
+         mCallback(getGame(), mIndex);
+
+      UserInterface::playBoop();
+      return true;
+   }
+
+   else if(keyCode == KEY_ENTER || keyCode == KEY_SPACE)
+   {
+      mIndex = (mIndex == (U32)mOptions.size() - 1) ? nextValAfterWrap : mIndex + 1;
+
+      if(mCallback)
+         mCallback(getGame(), mIndex);
+
+      UserInterface::playBoop();
+      return true;
+   }
+
+   else if(ascii)     // Check for the first key of a menu entry. 
+      for(S32 i = 0; i < mOptions.size(); i++)
+      {
+         S32 index = (i + mIndex + 1) % mOptions.size();
+         if(tolower(ascii) == tolower(mOptions[index].data()[0]))
+         {
+            mIndex = index;
+            
+            if(mCallback)
+               mCallback(getGame(), mIndex);
+
+            UserInterface::playBoop();
+            return true;
+         }
+      }
+
+   return false;
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+
+YesNoMenuItem::YesNoMenuItem(ClientGame *game, string title, bool currOption, void (*callback)(ClientGame *, U32), string help, KeyCode k1, KeyCode k2) :
+      ToggleMenuItem(game, title, Vector<string>(), currOption, true, callback, help, k1, k2)
+{
+   mValue = "";
+   mIndex = currOption;
+   mEnterAdvancesItem = true;
+
+   mOptions.push_back("No");     // 0
+   mOptions.push_back("Yes");    // 1
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+CounterMenuItem::CounterMenuItem(ClientGame *game, const string &title, S32 value, S32 step, S32 minVal, S32 maxVal, const string &units, 
+                                 const string &minMsg, const string &help, KeyCode k1, KeyCode k2) :
+   Parent(game, -1, title, NULL, help, k1, k2)
+{
+   mValue = value;
+   mStep = step;
+   mMinValue = minVal;
+   mMaxValue = maxVal;
+   mUnits = units;
+   mMinMsg = minMsg;   
+
+   mEnterAdvancesItem = true;
+}
+
+
+string CounterMenuItem::getOptionText()
+{
+   return (mValue == mMinValue && mMinMsg != "") ? mMinMsg : getValueForDisplayingInMenu() + " " + getUnits();
+}
+
+
+void CounterMenuItem::render(S32 xpos, S32 ypos, S32 textsize, bool isSelected)
+{
+   UserInterface::drawCenteredStringPair(xpos, ypos, textsize, *getColor(isSelected), *getValueColor(isSelected), 
+                                         getPrompt().c_str(), getOptionText().c_str());
+}
+
+
+S32 CounterMenuItem::getWidth(S32 textsize)
+{
+   return UserInterface::getStringPairWidth(textsize, getPrompt().c_str(), getOptionText().c_str());
+}
+
+
+bool CounterMenuItem::handleKey(KeyCode keyCode, char ascii)
+{
+   if(keyCode == KEY_RIGHT || keyCode == MOUSE_LEFT)  
+   {
+      if(checkModifier(KEY_SHIFT))
+      {
+         increment(getBigIncrement());
+         snap();
+      }
+      else
+         increment(1);
+
+      return true;
+   }
+   else if(keyCode == KEY_LEFT || keyCode == MOUSE_RIGHT)
+   {
+      if(checkModifier(KEY_SHIFT))
+      {
+         decrement(getBigIncrement());
+         snap();
+      }
+      else
+         decrement(1);
+
+      return true;
+   }
+
+   return false;
+}
+
+
+void CounterMenuItem::increment(S32 fact) 
+{ 
+   mValue += mStep * fact; 
+
+   if(mValue > mMaxValue) 
+      mValue = mMaxValue; 
+}
+
+
+void CounterMenuItem::decrement(S32 fact) 
+{ 
+   mValue -= mStep * fact; 
+
+   if(mValue < mMinValue) 
+      mValue = mMinValue; 
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+TimeCounterMenuItem::TimeCounterMenuItem(ClientGame *game, const string &title, S32 value, S32 maxVal, const string &zeroMsg, const string &help,
+                    S32 step, KeyCode k1, KeyCode k2) :
+   CounterMenuItem(game, title, value, step, 0, maxVal, "", zeroMsg, help, k1, k2)
+{
+   // Do nothing
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+TimeCounterMenuItemSeconds::TimeCounterMenuItemSeconds(ClientGame *game, const string &title, S32 value, S32 maxVal, const string &zeroMsg, 
+                                                       const string &help, KeyCode k1, KeyCode k2) :
+   TimeCounterMenuItem(game, title, value, maxVal, zeroMsg, help, 1, k1, k2)
+{
+   // Do nothing
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+PlayerMenuItem::PlayerMenuItem(ClientGame *game, S32 index, const char *text, void (*callback)(ClientGame *, U32), KeyCode k1, PlayerType type) :
+      MenuItem(game, index, text, callback, "", k1, KEY_UNKNOWN)
+{
+   mType = type;
+}
+
+
+string PlayerMenuItem::getOptionText()
+{
+   string text = getPrompt();
+
+   // Add a player type prefix if requested
+   if(mType == PlayerTypePlayer)
+      text = "[Player] " + text;
+   else if(mType == PlayerTypeAdmin)
+      text = "[Admin] " + text;
+   else if(mType == PlayerTypeRobot)
+      text = "[Robot] " + text;
+
+   return text;
+}
+
+
+void PlayerMenuItem::render(S32 xpos, S32 ypos, S32 textsize, bool isSelected)
+{
+   glColor(*getColor(isSelected));
+   UserInterface::drawCenteredString(xpos, ypos, textsize, getOptionText().c_str());
+}
+
+
+S32 PlayerMenuItem::getWidth(S32 textsize)
+{
+   return UserInterface::getStringWidth(textsize, getOptionText().c_str());
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+TeamMenuItem::TeamMenuItem(ClientGame *game, S32 index, AbstractTeam *team, void (*callback)(ClientGame *, U32), KeyCode keyCode, bool isCurrent) :
+               MenuItem(game, index, team->getName().getString(), callback, "", keyCode, KEY_UNKNOWN)
+{
+   mTeam = team;
+   mIsCurrent = isCurrent;
+   mUnselectedColor = team->getColor();
+   mSelectedColor = team->getColor();
+}
+
+
+string TeamMenuItem::getOptionText()
+{
+   Team *team = (Team *)mTeam;
+
+   // Static may help reduce allocation/deallocation churn at the cost of 2K memory; not sure either are really a problem
+   static char buffer[2048];  
+   dSprintf(buffer, sizeof(buffer), "%s%s [%d /%d]", mIsCurrent ? "-> " : "", getPrompt().c_str(), team->getPlayerCount(), team->getScore());
+
+   return buffer;
+}
+
+
+void TeamMenuItem::render(S32 xpos, S32 ypos, S32 textsize, bool isSelected)
+{
+   glColor(*getColor(isSelected));
+   UserInterface::drawCenteredStringf(xpos, ypos, textsize, getOptionText().c_str());
+}
+
+
+S32 TeamMenuItem::getWidth(S32 textsize)
+{
+   return UserInterface::getStringWidth(textsize, getOptionText().c_str());
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+EditableMenuItem::EditableMenuItem(ClientGame *game, string title, string val, string emptyVal, string help, U32 maxLen, KeyCode k1, KeyCode k2) :
+         ValueMenuItem(game, -1, title, NULL, help, k1, k2),
+         mLineEditor(LineEditor(maxLen, val))
+{
+   mEmptyVal = emptyVal;
+   mEnterAdvancesItem = true;
+   mTextEditedCallback = NULL;
+}
+
+
+string EditableMenuItem::getOptionText()
+{
+   return mLineEditor.getString() != "" ? mLineEditor.getDisplayString() : mEmptyVal;
+}
+
+
+void EditableMenuItem::render(S32 xpos, S32 ypos, S32 textsize, bool isSelected)
+{
+   Color textColor;     
+   if(mLineEditor.getString() == "" && mEmptyVal != "")
+      textColor.set(.4);
+   else if(isSelected)
+      textColor.set(Colors::red);
+   else
+      textColor.set(Colors::cyan);
+
+   S32 xpos2 = UserInterface::drawCenteredStringPair(xpos, ypos, textsize, *getColor(isSelected), textColor, 
+                                                     getPrompt().c_str(), getOptionText().c_str());
+
+   glColor(Colors::red);      // Cursor is always red
+   if(isSelected)
+      mLineEditor.drawCursor(xpos2, ypos, textsize);
+}
+
+
+S32 EditableMenuItem::getWidth(S32 textsize)
+{
+   return UserInterface::getStringPairWidth(textsize, getPrompt().c_str(), getOptionText().c_str());
+}
+
+
+bool EditableMenuItem::handleKey(KeyCode keyCode, char ascii) 
+{ 
+   if(keyCode == KEY_DELETE || keyCode == KEY_BACKSPACE)
+   {
+      mLineEditor.handleBackspace(keyCode); 
+
+      if(mTextEditedCallback)
+         mTextEditedCallback(mLineEditor.getString());
+
+      return true;
+   }
+   else if(ascii > 0)
+   {
+      mLineEditor.addChar(ascii);
+
+      if(mTextEditedCallback)
+         mTextEditedCallback(mLineEditor.getString());
+
+      return true;
+   }
+   
+   return false;
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+
+MaskedEditableMenuItem::MaskedEditableMenuItem(ClientGame *game, string title, string val, string emptyVal, string help, U32 maxLen, KeyCode k1, KeyCode k2) :
+   EditableMenuItem(game, title, val, emptyVal, help, maxLen, k1, k2)
+{
+   mLineEditor.setSecret(true);
+}
+
+};
+
