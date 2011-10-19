2ce463449513d53f67937b4257ec000af9b85dd5
watusimoto <watusimoto@bitfighter.org>
2011-10-20 01:45:11 +0200

    BUGFIX: Splitting line in editor now works

---
+++ zap/Event.cpp
@@ -0,0 +1,514 @@
+/*
+ * Input.cpp
+ *
+ *  Created on: Jun 12, 2011
+ *      Author: noonespecial
+ */
+
+#include "Event.h"
+
+#include "oglconsole.h"
+#include "config.h"
+#include "UIMenus.h"
+#include "UIDiagnostics.h"
+#include "IniFile.h"
+#include "ScreenShooter.h"
+#include "ScreenInfo.h"
+#include "Joystick.h"
+#include "ClientGame.h"
+
+#include "SDL/SDL_opengl.h"
+
+#include <cmath>
+
+namespace Zap
+{
+
+Event::Event()
+{
+   // Do nothing -- never called?
+}
+
+
+Event::~Event()
+{
+   // Do nothing
+}
+
+
+void Event::setMousePos(S32 x, S32 y, DisplayMode reportedDisplayMode)
+{
+   // Handle special case of editor... would be better handled elsewhere?
+   if(UserInterface::current->getMenuID() == EditorUI && reportedDisplayMode == DISPLAY_MODE_FULL_SCREEN_UNSTRETCHED)
+      reportedDisplayMode = DISPLAY_MODE_FULL_SCREEN_STRETCHED;
+
+   gScreenInfo.setMousePos(x, y, reportedDisplayMode);
+}
+
+// Argument of axisMask is one of the 4 axes:
+//    MoveAxisLeftRightMask, MoveAxisUpDownMask, ShootAxisLeftRightMask, ShootAxisUpDownMask
+void Event::updateJoyAxesDirections(U32 axisMask, S16 value)
+{
+   // Get our current joystick-axis-direction and its opposite on the same axis
+   U32 detectedAxesDirectionMask = 0;
+   U32 oppositeDetectedAxesDirectionMask = 0;
+   if (value < 0)
+   {
+      detectedAxesDirectionMask = axisMask & NegativeAxesMask;
+      oppositeDetectedAxesDirectionMask = axisMask & PositiveAxesMask;
+   }
+   else
+   {
+      detectedAxesDirectionMask = axisMask & PositiveAxesMask;
+      oppositeDetectedAxesDirectionMask = axisMask & NegativeAxesMask;
+   }
+
+   // Get the specific axes direction index (and opposite) from the mask we've detected
+   // from enum JoystickAxesDirections
+   U32 axesDirectionIndex = 0;
+   U32 oppositeAxesDirectionIndex = 0;
+   for (S32 i = 0; i < MaxAxesDirections; i++)
+   {
+      if(Joystick::JoystickInputData[i].axesMask & detectedAxesDirectionMask)
+         axesDirectionIndex = i;
+      if(Joystick::JoystickInputData[i].axesMask & oppositeDetectedAxesDirectionMask)
+         oppositeAxesDirectionIndex = i;
+   }
+
+
+   // Update our global joystick input data, use a sensitivity threshold to take care of calibration issues
+   // Also, normalize the input value to a floating point scale of 0 to 1
+   F32 normalValue;
+   S32 absValue = abs(value);
+   if(absValue < Joystick::LowerSensitivityThreshold)
+      normalValue = 0.0f;
+   else if(absValue > Joystick::UpperSensitivityThreshold)
+      normalValue = 1.0f;
+   else
+      normalValue = (F32)(absValue - Joystick::LowerSensitivityThreshold) / (F32)(Joystick::UpperSensitivityThreshold - Joystick::LowerSensitivityThreshold);
+
+//   logprintf("value: %d;\tnormalValue: %f", value, normalValue);
+
+   Joystick::JoystickInputData[axesDirectionIndex].value = normalValue;
+
+
+   // Now set the opposite axis back to zero
+   Joystick::JoystickInputData[oppositeAxesDirectionIndex].value = 0.0f;
+
+
+   // Determine what to set the InputCode state, it is binary so set the threshold has half -> 0.5
+   // Set the mask if it is above the digital threshold
+   bool inputCodeState = false;
+   U32 currentInputCodeMask = 0;
+
+   for (S32 i = 0; i < MaxAxesDirections; i++)
+      if (fabs(Joystick::JoystickInputData[i].value) > 0.5)
+      {
+         inputCodeState = true;
+         currentInputCodeMask |= (1 << i);
+      }
+
+
+   // Only change InputCode state if the axis has changed.  Time to be tricky..
+   U32 inputCodeDownDeltaMask = currentInputCodeMask & ~Joystick::AxesInputCodeMask;
+   U32 inputCodeUpDeltaMask = ~currentInputCodeMask & Joystick::AxesInputCodeMask;
+
+   for (S32 i = 0; i < MaxAxesDirections; i++)
+   {
+      // If the current axes direction is set in the inputCodeDownDeltaMask, set the input code down
+      if (Joystick::JoystickInputData[i].axesMask & inputCodeDownDeltaMask)
+      {
+         inputCodeDown(Joystick::JoystickInputData[i].inputCode, 0);
+         continue;
+      }
+
+      // If the current axes direction is set in the inputCodeUpDeltaMask, set the input code up
+      if (Joystick::JoystickInputData[i].axesMask & inputCodeUpDeltaMask)
+         inputCodeUp(Joystick::JoystickInputData[i].inputCode);
+   }
+
+
+   // Finally alter the global axes InputCode mask to reflect the current inputCodeState
+   Joystick::AxesInputCodeMask = currentInputCodeMask;
+}
+
+
+void Event::inputCodeUp(InputCode inputCode)
+{
+   setInputCodeState(inputCode, false);
+
+   if(UserInterface::current)
+      UserInterface::current->onKeyUp(inputCode);
+}
+
+
+void Event::inputCodeDown(InputCode inputCode, char ascii)
+{
+   setInputCodeState(inputCode, true);
+
+   if(UserInterface::current)
+      UserInterface::current->onKeyDown(inputCode, ascii);
+}
+
+
+void Event::onEvent(ClientGame *game, SDL_Event* event)
+{
+   switch (event->type)
+   {
+      case SDL_KEYDOWN:
+         onKeyDown(game, event->key.keysym.sym, event->key.keysym.mod, event->key.keysym.unicode);
+         break;
+
+      case SDL_KEYUP:
+         onKeyUp(event->key.keysym.sym, event->key.keysym.mod, event->key.keysym.unicode);
+         break;
+
+      case SDL_MOUSEMOTION:
+            onMouseMoved(event->motion.x, event->motion.y, game->getSettings()->getIniSettings()->displayMode);
+         break;
+
+      case SDL_MOUSEBUTTONDOWN:
+         switch (event->button.button)
+         {
+            case SDL_BUTTON_LEFT:
+               onMouseButtonDown(event->button.x, event->button.y, MOUSE_LEFT, game->getSettings()->getIniSettings()->displayMode);
+               break;
+
+            case SDL_BUTTON_RIGHT:
+               onMouseButtonDown(event->button.x, event->button.y, MOUSE_RIGHT, game->getSettings()->getIniSettings()->displayMode);
+               break;
+
+            case SDL_BUTTON_MIDDLE:
+               onMouseButtonDown(event->button.x, event->button.y, MOUSE_MIDDLE, game->getSettings()->getIniSettings()->displayMode);
+               break;
+            case SDL_BUTTON_WHEELUP:
+               onMouseWheel(true, false);
+               break;
+            case SDL_BUTTON_WHEELDOWN:
+               onMouseWheel(false, true);
+               break;
+         }
+         break;
+
+      case SDL_MOUSEBUTTONUP:
+         switch (event->button.button)
+         {
+            case SDL_BUTTON_LEFT:
+               onMouseButtonUp(event->button.x, event->button.y, MOUSE_LEFT, game->getSettings()->getIniSettings()->displayMode);
+               break;
+
+            case SDL_BUTTON_RIGHT:
+               onMouseButtonUp(event->button.x, event->button.y, MOUSE_RIGHT, game->getSettings()->getIniSettings()->displayMode);
+               break;
+
+            case SDL_BUTTON_MIDDLE:
+               onMouseButtonUp(event->button.x, event->button.y, MOUSE_MIDDLE, game->getSettings()->getIniSettings()->displayMode);
+               break;
+         }
+         break;
+
+      case SDL_JOYAXISMOTION:
+         onJoyAxis(game->getSettings()->getIniSettings()->joystickType, event->jaxis.which, event->jaxis.axis, event->jaxis.value);
+         break;
+
+         // TODO:  Do we need joyball and joyhat?
+      case SDL_JOYBALLMOTION:
+         onJoyBall(event->jball.which, event->jball.ball, event->jball.xrel, event->jball.yrel);
+         break;
+
+      case SDL_JOYHATMOTION:
+         onJoyHat(event->jhat.which, event->jhat.hat, event->jhat.value);
+         break;
+
+      case SDL_JOYBUTTONDOWN:
+         onJoyButtonDown(event->jbutton.which, event->jbutton.button, game->getSettings()->getIniSettings()->joystickType);
+         break;
+
+      case SDL_JOYBUTTONUP:
+         onJoyButtonUp(event->jbutton.which, event->jbutton.button, game->getSettings()->getIniSettings()->joystickType);
+         break;
+
+      case SDL_SYSWMEVENT:
+         //Ignore
+         break;
+
+      case SDL_VIDEORESIZE:
+         onResize(event->resize.w, event->resize.h, game->getSettings()->getIniSettings()->winSizeFact);
+         break;
+
+      case SDL_VIDEOEXPOSE:
+         onExpose();
+         break;
+
+      case SDL_ACTIVEEVENT:
+         switch (event->active.state)
+         {
+            case SDL_APPMOUSEFOCUS:
+               if (event->active.gain)
+                  onMouseFocus();
+               else
+                  onMouseBlur();
+
+               break;
+
+            case SDL_APPINPUTFOCUS:
+               if (event->active.gain)
+                  onInputFocus();
+               else
+                  onInputBlur();
+
+               break;
+
+            case SDL_APPACTIVE:
+               if (event->active.gain)
+                  onRestore();
+               else
+                  onMinimize();
+
+               break;
+         }
+         break;
+
+      default:
+         onUser(event->user.type, event->user.code, event->user.data1, event->user.data2);
+         break;
+   }
+}
+
+void Event::onInputFocus()
+{
+   // Do nothing
+}
+
+
+void Event::onInputBlur()     // <=== what does this do??
+{
+   // Do nothing
+}
+
+
+void Event::onKeyDown(ClientGame *game, SDLKey key, SDLMod mod, U16 unicode)
+{
+   // Use getInputCodeState() instead of checking the mod flag to prevent hyper annoying case
+   // of user pressing and holding Alt, selecting another window, releasing Alt, returning to
+   // Bitfighter window, and pressing enter, and having this block think we pressed alt-enter.
+   // GetKeyState() looks at the actual current state of the key, which is what we want.
+
+   // ALT + ENTER --> toggles window mode/full screen
+   if(key == SDLK_RETURN && getInputCodeState(KEY_ALT))
+      game->getUIManager()->getOptionsMenuUserInterface()->toggleDisplayMode();
+
+   // CTRL + Q --> screenshot!
+   else if(key == SDLK_q && getInputCodeState(KEY_CTRL))
+      ScreenShooter::saveScreenshot(game->getSettings()->getFolderManager()->screenshotDir);
+
+   // The rest
+   else
+   {
+      InputCode inputCode = sdlKeyToInputCode(key);
+      inputCodeDown(inputCode, keyToAscii(unicode, inputCode));
+   }
+}
+
+
+void Event::onKeyUp(SDLKey key, SDLMod mod, U16 unicode)
+{
+   inputCodeUp(sdlKeyToInputCode(key));
+}
+
+
+void Event::onMouseFocus()
+{
+   // Do nothing
+}
+
+
+void Event::onMouseBlur()
+{
+   // Do nothing
+}
+
+
+void Event::onMouseMoved(S32 x, S32 y, DisplayMode mode)
+{
+   setMousePos(x, y, mode);
+
+   if(UserInterface::current)
+      UserInterface::current->onMouseMoved(x, y);
+}
+
+
+void Event::onMouseWheel(bool Up, bool Down)
+{
+   if(Up)
+   {
+      inputCodeDown(MOUSE_WHEEL_UP);
+      inputCodeUp(MOUSE_WHEEL_UP);
+   }
+   if(Down)
+   {
+      inputCodeDown(MOUSE_WHEEL_DOWN);
+      inputCodeUp(MOUSE_WHEEL_DOWN);
+   }
+   // Do nothing
+}
+
+
+void Event::onMouseButtonDown(S32 x, S32 y, InputCode inputCode, DisplayMode mode)
+{
+   setMousePos(x, y, mode);
+
+   inputCodeDown(inputCode, 0);
+}
+
+
+void Event::onMouseButtonUp(S32 x, S32 y, InputCode inputCode, DisplayMode mode)
+{
+   setMousePos(x, y, mode);
+
+   inputCodeUp(inputCode);
+}
+
+
+void Event::onJoyAxis(U32 joystickType, U8 whichJoystick, U8 axis, S16 value)
+{
+//   logprintf("SDL Axis number: %u, value: %d", axis, value);
+
+   if(axis < Joystick::rawAxisCount)
+      Joystick::rawAxis[axis] = (F32)value / (F32)S16_MAX;
+
+   // If we are using a predefined controller, get the appropriate axis
+   if(joystickType < ControllerTypeCount)
+   {
+      // Left/Right movement axis
+      if(axis == Joystick::PredefinedJoystickList[joystickType].moveAxesSdlIndex[0])
+         updateJoyAxesDirections(MoveAxisLeftRightMask,  value);
+
+      // Up/down movement axis
+      if(axis == Joystick::PredefinedJoystickList[joystickType].moveAxesSdlIndex[1])
+         updateJoyAxesDirections(MoveAxisUpDownMask,  value);
+
+      // Left/Right shooting axis
+      if(axis == Joystick::PredefinedJoystickList[joystickType].shootAxesSdlIndex[0])
+         updateJoyAxesDirections(ShootAxisLeftRightMask, value);
+
+      // Up/down shooting axis
+      if(axis == Joystick::PredefinedJoystickList[joystickType].shootAxesSdlIndex[1])
+         updateJoyAxesDirections(ShootAxisUpDownMask, value);
+   }
+   // Else just use the first 4 axis for fun
+   else {
+      if (axis == 0)
+         updateJoyAxesDirections(MoveAxisLeftRightMask,  value);
+      if (axis == 1)
+         updateJoyAxesDirections(MoveAxisUpDownMask,  value);
+      if (axis == 2)
+         updateJoyAxesDirections(ShootAxisLeftRightMask, value);
+      if (axis == 3)
+         updateJoyAxesDirections(ShootAxisUpDownMask, value);
+   }
+}
+
+extern InputCode joyButtonToInputCode(int buttonIndex);
+
+
+void Event::onJoyButtonDown(U8 which, U8 button, U32 joystickType)
+{
+//   logprintf("SDL button down number: %u", button);
+   inputCodeDown(joyButtonToInputCode(Joystick::remapJoystickButton(joystickType, button)), 0);
+   Joystick::ButtonMask |= BIT(button);
+}
+
+
+void Event::onJoyButtonUp(U8 which, U8 button, U32 joystickType)
+{
+//   logprintf("SDL button up number: %u", button);
+   inputCodeUp(joyButtonToInputCode(Joystick::remapJoystickButton(joystickType, button)));
+   Joystick::ButtonMask = Joystick::ButtonMask & ~BIT(button);
+}
+
+
+extern InputCode joyHatToInputCode(int hatDirectionMask);
+
+// See SDL_Joystick.h for the SDL_HAT_* mask definitions
+void Event::onJoyHat(U8 which, U8 hat, U8 directionMask)
+{
+//   logprintf("SDL Hat number: %u, value: %u", hat, directionMask);
+
+   InputCode inputCode;
+   U32 inputCodeDownDeltaMask = directionMask & ~Joystick::HatInputCodeMask;
+   U32 inputCodeUpDeltaMask = ~directionMask & Joystick::HatInputCodeMask;
+
+   for (S32 i = 0; i < MaxHatDirections; i++)
+   {
+      inputCode = joyHatToInputCode(BIT(i));  // BIT(i) corresponds to a defined SDL_HAT_*  value
+
+      // If the current hat direction is set in the inputCodeDownDeltaMask, set the input code down
+      if (inputCodeDownDeltaMask & BIT(i))
+         inputCodeDown(inputCode, 0);
+
+      // If the current hat direction is set in the inputCodeUpDeltaMask, set the input code up
+      if (inputCodeUpDeltaMask & BIT(i))
+         inputCodeUp(inputCode);
+   }
+
+   // Finally alter the global hat InputCode mask to reflect the current input code State
+   Joystick::HatInputCodeMask = directionMask;
+}
+
+void Event::onJoyBall(U8 which, U8 ball, S16 xrel, S16 yrel)
+{
+//   logprintf("SDL Ball number: %u, relative x: %d, relative y: %d", ball, xrel, yrel);
+}
+
+
+void Event::onMinimize()
+{
+   // Do nothing
+}
+
+
+void Event::onRestore()
+{
+   // Do nothing
+}
+
+
+// We don't need to worry about this event in fullscreen modes because it is never fired with SDL
+void Event::onResize(S32 width, S32 height, F32 winSizeFact)
+{
+   S32 canvasHeight = gScreenInfo.getGameCanvasHeight();
+   S32 canvasWidth = gScreenInfo.getGameCanvasWidth();
+
+   // Constrain window to correct proportions...
+   if((width - canvasWidth) > (height - canvasHeight))      // Wider than taller  (is this right? mixing virtual and physical pixels)
+      winSizeFact = max((F32) height / (F32)canvasHeight, gScreenInfo.getMinScalingFactor());
+   else
+      winSizeFact = max((F32) width / (F32)canvasWidth, gScreenInfo.getMinScalingFactor());
+
+   S32 newWidth  = (S32)floor(canvasWidth  * winSizeFact + 0.5f);   // virtual * (physical/virtual) = physical, fix rounding problem
+   S32 newHeight = (S32)floor(canvasHeight * winSizeFact + 0.5f);
+
+   S32 flags = 0;
+   flags = gScreenInfo.isHardwareSurface() ? SDL_OPENGL | SDL_HWSURFACE | SDL_RESIZABLE : SDL_OPENGL | SDL_RESIZABLE;
+   SDL_SetVideoMode(newWidth, newHeight, 0, flags);
+   gScreenInfo.setWindowSize(newWidth, newHeight);
+
+   glViewport(0, 0, gScreenInfo.getWindowWidth(), gScreenInfo.getWindowHeight());
+   OGLCONSOLE_Reshape();
+
+   gINI.SetValueF("Settings", "WindowScalingFactor", winSizeFact, true);
+}
+
+
+void Event::onExpose()
+{
+   // Do nothing
+}
+
+void Event::onUser(U8 type, S32 code, void* data1, void* data2)
+{
+
+}
+
+}
---
+++ zap/Spawn.cpp
@@ -0,0 +1,447 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef ZAP_DEDICATED
+#  include "SDL/SDL_opengl.h"
+#  include "UIMenuItems.h"
+#  include "UIEditorMenus.h"
+#  include "ClientGame.h"
+#endif
+
+#include "Spawn.h"
+#include "game.h"
+
+#include "stringUtils.h"         // For itos()
+#include "gameObjectRender.h"    // For renderSquareItem(), renderFlag(), drawCircle()
+#include "moveObject.h"          // For Circle, Asteroid class defs
+
+
+namespace Zap
+{
+
+// Statics:
+#ifndef ZAP_DEDICATED
+   EditorAttributeMenuUI *AbstractSpawn::mAttributeMenuUI = NULL;
+#endif
+
+AbstractSpawn::AbstractSpawn(const Point &pos, S32 time)
+{
+   setVert(pos, 0);
+   setRespawnTime(time);
+};
+
+
+AbstractSpawn::AbstractSpawn(const AbstractSpawn &copy) : EditorPointObject(copy), mTimer(copy.mTimer)
+{
+   mSpawnTime = copy.mSpawnTime;
+}
+
+
+AbstractSpawn::~AbstractSpawn()
+{
+   // Do nothing
+}
+
+
+void AbstractSpawn::setRespawnTime(S32 time)       // in seconds
+{
+   mSpawnTime = time;
+   mTimer.setPeriod(time * 1000);
+   mTimer.reset();
+}
+
+
+F32 AbstractSpawn::getEditorRadius(F32 currentScale)
+{
+   return 12;     // Constant size, regardless of zoom
+}
+
+
+bool AbstractSpawn::processArguments(S32 argc, const char **argv, Game *game)
+{
+   if(argc < 2)
+      return false;
+
+   Point pos;
+   pos.read(argv);
+   pos *= game->getGridSize();
+
+   setVert(pos, 0);
+
+   S32 time = (argc > 2) ? atoi(argv[2]) : getDefaultRespawnTime();
+
+   setRespawnTime(time);
+
+   return true;
+}
+
+
+string AbstractSpawn::toString(F32 gridSize) const
+{
+   // <<spawn class name>> <x> <y> <spawn timer>
+   return string(getClassName()) + " " + geomToString(gridSize) + " " + itos(mSpawnTime);
+}
+
+
+#ifndef ZAP_DEDICATED
+
+EditorAttributeMenuUI *AbstractSpawn::getAttributeMenu()
+{
+   if(getDefaultRespawnTime() == -1)  // No editing RespawnTimer for Ship Spawn
+      return NULL;
+
+   if(!mAttributeMenuUI)
+   {
+      ClientGame *clientGame = (ClientGame *)getGame();
+
+      mAttributeMenuUI = new EditorAttributeMenuUI(clientGame);
+
+      CounterMenuItem *menuItem = new CounterMenuItem("Spawn Timer:", 999, 1, 0, 1000, "secs", "Never spawns", 
+                                                      "Time it takes for each item to be spawned");
+      mAttributeMenuUI->addMenuItem(menuItem);
+
+      // Add our standard save and exit option to the menu
+      mAttributeMenuUI->addSaveAndQuitMenuItem();
+   }
+
+   return mAttributeMenuUI;
+}
+
+
+void AbstractSpawn::startEditingAttrs(EditorAttributeMenuUI *attributeMenu)
+{
+   attributeMenu->getMenuItem(0)->setIntValue(mSpawnTime);
+}
+
+
+void AbstractSpawn::doneEditingAttrs(EditorAttributeMenuUI *attributeMenu)
+{
+   mSpawnTime = attributeMenu->getMenuItem(0)->getIntValue();
+}
+
+
+// Render some attributes when item is selected but not being edited
+void AbstractSpawn::renderAttributeString(F32 currentScale)
+{
+
+   if(getDefaultRespawnTime() == -1)
+      return;
+
+   string txt;
+
+   if(mSpawnTime == 0)
+      txt = "Disabled";
+   else
+      txt = "Spawn time: " + itos(mSpawnTime) + " sec" + ( mSpawnTime != 1 ? "s" : "");
+
+   renderItemText(txt.c_str(), 1, currentScale);
+}
+
+#endif
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Constructor
+Spawn::Spawn(const Point &pos) : AbstractSpawn(pos)
+{
+   mObjectTypeNumber = ShipSpawnTypeNumber;
+}
+
+
+// Destructor
+Spawn::~Spawn()
+{
+   // Do nothing
+}
+
+
+Spawn *Spawn::clone() const
+{
+   return new Spawn(*this);
+}
+
+
+bool Spawn::processArguments(S32 argc, const char **argv, Game *game)
+{
+   if(argc < 3)
+      return false;
+
+   S32 teamIndex = atoi(argv[0]);
+   setTeam(teamIndex);
+
+   Point pos;
+   pos.read(argv + 1);
+   pos *= game->getGridSize();
+
+   setVert(pos, 0);
+
+   return true;
+}
+
+
+string Spawn::toString(F32 gridSize) const
+{
+   // Spawn <team> <x> <y> 
+   return string(getClassName()) + " " + itos(mTeam) + " " + geomToString(gridSize);
+}
+
+
+void Spawn::renderEditor(F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+   Point pos = getVert(0);
+
+   glPushMatrix();
+      glTranslatef(pos.x, pos.y, 0);
+      glScalef(1/currentScale, 1/currentScale, 1);    // Make item draw at constant size, regardless of zoom
+      renderSquareItem(Point(0,0), getGame()->getTeamColor(mTeam), 1, &Colors::white, 'S');
+   glPopMatrix();
+#endif
+}
+
+
+void Spawn::renderDock()
+{
+   renderEditor(1);
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Constructor
+ItemSpawn::ItemSpawn(const Point &pos, S32 time) : Parent(pos, time)
+{
+   // Do nothing
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Constructor
+AsteroidSpawn::AsteroidSpawn(const Point &pos, S32 time) : Parent(pos, time)
+{
+   mObjectTypeNumber = AsteroidSpawnTypeNumber;
+}
+
+
+AsteroidSpawn::~AsteroidSpawn()
+{
+   // Do nothing
+}
+
+
+AsteroidSpawn *AsteroidSpawn::clone() const
+{
+   return new AsteroidSpawn(*this);
+}
+
+
+void AsteroidSpawn::spawn(Game *game, const Point &pos)
+{
+   Asteroid *asteroid = dynamic_cast<Asteroid *>(TNL::Object::create("Asteroid"));   // Create a new asteroid
+
+   F32 ang = TNL::Random::readF() * Float2Pi;
+
+   asteroid->setPosAng(pos, ang);
+
+   asteroid->addToGame(game, game->getGameObjDatabase());              // And add it to the list of game objects
+}
+
+
+static void renderAsteroidSpawn(const Point &pos)
+{
+#ifndef ZAP_DEDICATED
+   F32 scale = 0.8f;
+   static const Point p(0,0);
+
+   glPushMatrix();
+      glTranslatef(pos.x, pos.y, 0);
+      glScalef(scale, scale, 1);
+      renderAsteroid(p, 2, .1f);
+
+      glColor(Colors::white);
+      drawCircle(p, 13);
+   glPopMatrix();  
+#endif
+}
+
+
+void AsteroidSpawn::renderEditor(F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+   Point pos = getVert(0);
+
+   glPushMatrix();
+      glTranslate(pos);
+      glScale(1/currentScale);    // Make item draw at constant size, regardless of zoom
+      renderAsteroidSpawn(Point(0,0));
+   glPopMatrix();   
+#endif
+}
+
+
+void AsteroidSpawn::renderDock()
+{
+   renderAsteroidSpawn(getVert(0));
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Constructor
+CircleSpawn::CircleSpawn(const Point &pos, S32 time) : Parent(pos, time)
+{
+   mObjectTypeNumber = CircleSpawnTypeNumber;
+}
+
+
+CircleSpawn *CircleSpawn::clone() const
+{
+   return new CircleSpawn(*this);
+}
+
+
+void CircleSpawn::spawn(Game *game, const Point &pos)
+{
+   for(S32 i = 0; i < 10; i++)
+   {
+      Circle *circle = dynamic_cast<Circle *>(TNL::Object::create("Circle"));   // Create a new Circle
+      F32 ang = TNL::Random::readF() * Float2Pi;
+
+      circle->setPosAng(pos, ang);
+
+      circle->addToGame(game, game->getGameObjDatabase());              // And add it to the list of game objects
+   }
+}
+
+
+static void renderCircleSpawn(const Point &pos)
+{
+#ifndef ZAP_DEDICATED
+   F32 scale = 0.8f;
+   static const Point p(0,0);
+
+   glPushMatrix();
+      glTranslatef(pos.x, pos.y, 0);
+      glScalef(scale, scale, 1);
+      drawCircle(p, 8);
+
+      glColor(Colors::white);
+      drawCircle(p, 13);
+   glPopMatrix();  
+#endif
+}
+
+
+void CircleSpawn::renderEditor(F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+   Point pos = getVert(0);
+
+   glPushMatrix();
+      glTranslate(pos);
+      glScale(1/currentScale);    // Make item draw at constant size, regardless of zoom
+      renderCircleSpawn(Point(0,0));
+   glPopMatrix();   
+#endif
+}
+
+
+void CircleSpawn::renderDock()
+{
+   renderCircleSpawn(getVert(0));
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Constructor
+FlagSpawn::FlagSpawn(const Point &pos, S32 time) : AbstractSpawn(pos, time)
+{
+   mObjectTypeNumber = FlagSpawnTypeNumber;
+}
+
+
+FlagSpawn::~FlagSpawn()
+{
+   // Do nothing
+}
+
+
+FlagSpawn *FlagSpawn::clone() const
+{
+   return new FlagSpawn(*this);
+}
+
+
+void FlagSpawn::renderEditor(F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+   Point pos = getVert(0);
+
+   glPushMatrix();
+      glTranslatef(pos.x + 1, pos.y, 0);
+      glScalef(0.4f/currentScale, 0.4f/currentScale, 1);
+      Color color = getTeamColor(mTeam);  // To avoid taking address of temporary
+      renderFlag(0, 0, &color);
+
+      glColor(Colors::white);
+      drawCircle(-4, 0, 26);
+   glPopMatrix();
+#endif
+}
+
+
+void FlagSpawn::renderDock()
+{
+   renderEditor(1);
+}
+
+
+bool FlagSpawn::processArguments(S32 argc, const char **argv, Game *game)
+{
+   if(argc < 1)
+      return false;
+
+   mTeam = atoi(argv[0]);                                            // Read team
+   return Parent::processArguments(argc - 1, argv + 1, game);        // then read the rest of the args
+}
+
+
+string FlagSpawn::toString(F32 gridSize) const
+{
+   // FlagSpawn <team> <x> <y> <spawn timer for nexus> -- squeezing in team number from AbstractSpawn::toString
+   string str1 = Parent::toString(gridSize);
+   size_t firstarg = str1.find(' ');
+   return str1.substr(0, firstarg) + " " + itos(mTeam) + str1.substr(firstarg);
+}
+
+
+};    // namespace
