2d86d94201a21f70e5a414d80492031f5ea35107
sam8641 <sam8641@localhost>
2012-02-01 18:35:07 -0600

    Voice chat now saves bandwidth if your option's voiceChat is mute, also added a way for server to disable a client from voice chatting

---
+++ zap/SimpleLine.cpp
@@ -0,0 +1,162 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "SimpleLine.h"
+#include "gameObjectRender.h"
+#include "Colors.h"
+#include "config.h"
+
+#include <math.h>
+
+#ifndef ZAP_DEDICATED
+#include "SDL/SDL_opengl.h"
+#include "UI.h"
+#include "UIEditor.h"
+#endif
+
+namespace Zap
+{
+
+
+ // Constructor
+SimpleLine::SimpleLine()
+{ 
+   // Do nothing
+}
+
+
+S32 SimpleLine::getDockRadius()
+{
+   return 8;
+}
+
+
+F32 SimpleLine::getEditorRadius(F32 currentScale)
+{
+   return 7;
+}
+
+
+void SimpleLine::renderDock()
+{
+#ifndef ZAP_DEDICATED
+   glColor(getEditorRenderColor());       // Blue for TextItem, red for GoFast, etc.
+   drawFilledSquare(getVert(0), 5);       // Draw origin of item to give user something to grab on the dock
+#endif
+}
+
+
+const F32 INITIAL_ITEM_LENGTH = 1.0;
+
+// Called when we create a brand new object and insert it in the editor, like when we drag a new item from the dock
+void SimpleLine::newObjectFromDock(F32 gridSize) 
+{
+#ifndef ZAP_DEDICATED
+   setVert(Point(0,0), 0);
+   setVert(Point(1,0) * INITIAL_ITEM_LENGTH * gridSize, 1);
+
+   EditorParent::newObjectFromDock(gridSize);
+#endif
+}
+
+
+// Offset lets us drag an item out from the dock by an amount offset from the 0th vertex.  This makes placement seem more natural.
+// In this case, we'll drag these items by their slender midriff.
+Point SimpleLine::getInitialPlacementOffset(F32 gridSize) 
+{ 
+   return Point(INITIAL_ITEM_LENGTH * gridSize / 2, 0); 
+}
+
+
+// TODO: Put in editor ??
+static const Color INSTRUCTION_TEXTCOLOR(Colors::white);
+static const S32 INSTRUCTION_TEXTSIZE = 9;      
+static const S32 INSTRUCTION_TEXTGAP = 3;
+
+
+// Draw arrow that serves as the core of SimpleLine items in the editor
+// Subclasses will fill in the rest
+void SimpleLine::renderEditor(F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+   Point pos = getVert(0);
+   Point dest = getVert(1);
+
+   for(S32 i = 1; i >= 0; i--)
+   {
+      // Draw heavy colored line with colored core
+      glLineWidth(i ? gLineWidth4 : gDefaultLineWidth);                
+      glColor(getEditorRenderColor(), i ? .35f : 1);         // Get color from child class
+
+      F32 ang = pos.angleTo(dest);
+      const F32 al = 15;                // Length of arrow-head, in editor units (15 pixels)
+      const F32 angoff = .5;            // Pitch of arrow-head prongs
+
+      glBegin(GL_LINES);
+         glVertex2f(dest.x, dest.y);    // Draw arrowhead
+         glVertex2f(dest.x - cos(ang + angoff) * al, dest.y - sin(ang + angoff) * al);
+         glVertex2f(dest.x, dest.y);
+         glVertex2f(dest.x - cos(ang - angoff) * al, dest.y - sin(ang - angoff) * al);
+
+         // Draw highlighted core on 2nd pass if item is selected, but not while it's being edited
+         if(!i && (mSelected || mLitUp))
+            glColor(mSelected ? *SELECT_COLOR : *HIGHLIGHT_COLOR);
+
+         glVertex(pos);                 // Draw connecting line
+         glVertex(dest);
+      glEnd();
+   }
+
+   renderEditorItem();
+#endif
+}
+
+
+// Offset: negative below the item, positive above
+void SimpleLine::renderItemText(const char *text, S32 offset, F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+
+   EditorParent::renderItemText(text, offset, currentScale);
+   return;
+
+   glColor(INSTRUCTION_TEXTCOLOR);
+
+   UserInterface::drawStringf_2pt(getVert(0), getVert(1) , F32(INSTRUCTION_TEXTSIZE) / currentScale, -3.f / currentScale, text);
+   UserInterface::drawStringf_2pt(getVert(0), getVert(1) , F32(INSTRUCTION_TEXTSIZE) / currentScale, -21.f / currentScale, "[Enter] to edit");
+#endif
+}
+
+
+void SimpleLine::prepareForDock(ClientGame *game, const Point &point, S32 teamIndex)
+{
+#ifndef ZAP_DEDICATED
+   setVert(point, 0);
+   EditorParent::prepareForDock(game, point, teamIndex);
+#endif
+}
+
+
+};
---
+++ zap/polygon.cpp
@@ -0,0 +1,186 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "polygon.h"
+#include "GeomUtils.h"           // For polygon triangulation
+#include "gameObjectRender.h"
+#include "Colors.h"
+
+#ifndef ZAP_DEDICATED
+#include "SDL/SDL_opengl.h"
+#include "UI.h"
+#endif
+
+namespace Zap
+{
+
+// TODO: Put in editor ??
+static const Color INSTRUCTION_TEXTCOLOR = Colors::white;
+static const S32 INSTRUCTION_TEXTSIZE = 9;      
+static const S32 INSTRUCTION_TEXTGAP = 3;
+
+// Offset: negative below the item, positive above
+void EditorPolygon::renderItemText(const char *text, S32 offset, F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+   EditorParent::renderItemText(text, offset, currentScale);
+   return;
+
+   glColor(INSTRUCTION_TEXTCOLOR);
+   S32 off = (INSTRUCTION_TEXTSIZE + INSTRUCTION_TEXTGAP) * offset - 10 - ((offset > 0) ? 5 : 0);
+   Point pos = getVert(0) * currentScale;
+   UserInterface::drawCenteredString(pos.x, pos.y - off, INSTRUCTION_TEXTSIZE, text);
+#endif
+}
+
+
+void EditorPolygon::onItemDragging()
+{
+   onGeomChanged();
+}
+
+
+// Tell the geometry that things have changed
+void EditorPolygon::onGeomChanged() 
+{ 
+   onPointsChanged(); 
+   EditorParent::onGeomChanged();
+}  
+
+
+void EditorPolygon::renderEditor(F32 currentScale)
+{
+#ifndef ZAP_DEDICATED
+   if(mSelected || mLitUp)
+      renderPolyHighlight();
+
+   renderLinePolyVertices(currentScale);
+#endif
+}
+
+
+void EditorPolygon::renderDock()
+{
+   renderEditor(1);
+}
+
+
+void EditorPolygon::highlightDockItem() 
+{   
+   renderPolyHighlight(); 
+}
+
+
+void EditorPolygon::renderPolyHighlight()
+{
+#ifndef ZAP_DEDICATED
+   glLineWidth(gLineWidth3);
+   glColor(mSelected ? *SELECT_COLOR : *HIGHLIGHT_COLOR);
+   renderPolygonOutline(getOutline());
+   glLineWidth(gDefaultLineWidth);
+#endif
+}
+
+
+void EditorPolygon::labelDockItem()
+{
+#ifndef ZAP_DEDICATED
+   renderDockItemLabel(getCentroid(), getOnDockName(), -2);
+#endif
+}
+
+
+void EditorPolygon::prepareForDock(ClientGame *game, const Point &point, S32 teamIndex)
+{
+#ifndef ZAP_DEDICATED
+   F32 h = 16;    // Entire height
+   F32 w = 20;    // Half the width
+
+   clearVerts();
+   addVert(point + Point(-w, 0)); 
+   addVert(point + Point( w, 0)); 
+   addVert(point + Point( w, h)); 
+   addVert(point + Point(-w, h)); 
+
+   EditorObject::prepareForDock(game, point, teamIndex);
+#endif
+}
+
+
+static const F32 INITIAL_HEIGHT = 0.9f;
+static const F32 INITIAL_WIDTH = 0.3f;
+
+// Called when we create a brand new object and insert it in the editor, like when we drag a new item from the dock
+void EditorPolygon::newObjectFromDock(F32 gridSize)
+{
+#ifndef ZAP_DEDICATED
+   F32 w = INITIAL_HEIGHT * gridSize / 2;
+   F32 h = INITIAL_WIDTH * gridSize / 2;
+
+   setVert(Point(-w, -h), 0);
+   setVert(Point(-w,  h), 1);
+   setVert(Point( w,  h), 2);
+   setVert(Point( w, -h), 3);
+
+   EditorParent::newObjectFromDock(gridSize);
+#endif
+}
+
+
+// Offset lets us drag an item out from the dock by an amount offset from the 0th vertex.  This makes placement seem more natural.
+Point EditorPolygon::getInitialPlacementOffset(F32 gridSize)
+{ 
+   return Point(INITIAL_HEIGHT * gridSize / 2, INITIAL_WIDTH * gridSize / 2); 
+}
+
+
+/////
+// Former LuaPolygon methods
+S32 EditorPolygon::getLoc(lua_State *L)
+{
+   return LuaObject::returnPoint(L, getCentroid());
+}
+
+
+S32 EditorPolygon::getRad(lua_State *L)
+{
+   return LuaObject::returnInt(L, 0);
+}
+
+
+S32 EditorPolygon::getVel(lua_State *L)
+{
+   return LuaObject::returnPoint(L, Point(0,0));
+}
+
+
+S32 EditorPolygon::getTeamIndx(lua_State *L)
+{
+   return LuaObject::returnInt(L, getTeam() + 1);
+}
+
+
+};
+
