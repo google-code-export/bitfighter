f4d949f3ff3d47029cabe97bcfb5ca93ccbf082a
watusimoto <watusimoto@bitfighter.org>
2012-06-09 17:43:35 +0200

    LuaW classes can now have multiple parents.  yay!

---
+++ zap/sparkManager.h
@@ -0,0 +1,158 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _FXMANAGER_H_
+#define _FXMANAGER_H_
+
+#include "Point.h"
+#include "Color.h"
+#include "tnlVector.h"
+
+namespace Zap
+{
+
+class FXManager
+{
+   enum SparkType
+   {
+      SparkTypePoint = 0,
+      SparkTypeLine,
+      SparkTypeCount
+   };
+
+   struct Spark
+   {
+      Point pos;
+      Color color;
+      F32 alpha;
+      S32 ttl;     // Milliseconds
+      Point vel;
+   };
+
+   struct DebrisChunk
+   {
+      Vector<Point> points;
+      Color color;
+
+      Point pos;
+      Point vel;
+      S32 ttl;     // Milliseconds
+      F32 angle;
+      F32 rotation;
+
+      void idle(U32 timeDelta);
+      void render();
+   };
+
+   Vector<DebrisChunk> mDebrisChunks;
+
+
+   struct TextEffect
+   {
+      string text;
+      Color color;
+      Point pos;
+      Point vel;
+      F32 size;
+      F32 growthRate;
+      S32 ttl;    // Milliseconds
+      
+      void idle(U32 timeDelta);
+      void render();
+   };
+
+   Vector<TextEffect> mTextEffects;
+
+   struct TeleporterEffect;
+   TeleporterEffect *teleporterEffects;
+
+   static const U32 MAX_SPARKS = 8192;    // Make this an even number
+
+   U32 firstFreeIndex[SparkTypeCount];            // Tracks next available slot when we have fewer than MAX_SPARKS 
+   U32 lastOverwrittenIndex[SparkTypeCount];      // Keep track of which spark we last overwrote
+
+   Spark gSparks[SparkTypeCount][MAX_SPARKS];     // Our sparks themselves... two types, each with room for MAX_SPARKS
+
+public:
+   FXManager();
+   void emitSpark(const Point &pos, const Point &vel, const Color &color, S32 ttl = 0, SparkType = SparkTypePoint);
+   void emitExplosion(const Point &pos, F32 size, const Color *colorArray, U32 numColors);
+   void emitBurst(const Point &pos, const Point &scale, const Color &color1, const Color &color2);
+   void emitBurst(const Point &pos, const Point &scale, const Color &color1, const Color &color2, U32 count);
+   void emitBlast(const Point &pos, U32 size);
+
+   void emitDebrisChunk(const Vector<Point> &points, const Color &color, const Point &pos, const Point &vel, S32 ttl, F32 angle, F32 rotation);
+   void emitTextEffect(string text, Color color, Point pos);
+
+   void emitTeleportInEffect(Point pos, U32 type);
+   void idle(U32 timeDelta);
+   void render(S32 renderPass);
+   void clearSparks();
+};
+
+class FXTrail
+{
+private:
+   struct TrailNode
+   {
+      Point pos;
+      S32   ttl;    // Milliseconds
+      bool  boosted;
+      bool  invisible;
+   };
+
+   Vector<TrailNode> mNodes;
+
+   U32 mDropFreq;
+   S32 mLength;
+
+   FXTrail *mNext;
+
+   static FXTrail *mHead;
+   void registerTrail();
+   void unregisterTrail();
+
+public:
+   FXTrail(U32 dropFrequency = 32, U32 len = 15);
+   ~FXTrail();
+
+   /// Update the point this trail is attached to.
+   void update(Point pos, bool boosted = false, bool invisible = false);
+
+   void idle(U32 timeDelta);
+
+   void render();
+
+   void reset();
+
+   Point getLastPos();
+
+   static void renderTrails();
+};
+
+};
+
+#endif
+
