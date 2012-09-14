47317aa7556b2d6bf6ada1927af7a0e181438604
buckyballreaction <buckyballreaction@gmail.com>
2012-09-14 17:00:35 -0600

    Fix broken right mouse button on Mac.  I had to revert SDL rev b91e7565e877 and recompile the framework

---
+++ luadoc.pl
@@ -0,0 +1,160 @@
+# This script preprocesses our C++ code and generates fodder for Doxygen which makes pretty documentation
+# for our Lua scripters out there
+
+use strict;            # Require vars to be declared!
+use File::Basename;
+use List::Util 'first';
+
+# Need to be in the doc directory
+chdir "doc" || die "Could not change to doc folder: $!";
+
+# Create doc/lua/ directory -- this should always exist
+my $luadir = "lua";      
+if (! -d $luadir) {
+   mkdir($luadir) || die "Could not create folder $luadir: $!";
+}
+
+
+# Iterate over all cpp folders in our working dir converting Doxygen comments to Luadoc comments
+my @files = <../zap/*.cpp>;
+# my @files = ("../zap/BfObject.cpp", "../zap/teleporter.cpp");
+foreach my $file (@files) {
+
+   open my $IN, "<", $file || die "Could not open $file for reading: $!";
+
+   my $writeFile = 0;
+
+   # Various modes we could be in
+   my $collectingMethods = 0;
+   # my $collectingLongDescr = 0;
+   my $collectingLongComment = 0;
+
+   my @methods = ();
+   my @comments = ();
+   my %classes = ();        # Will be a hash of arrays
+
+   my $shortClassDescr = "";
+   my $longClassDescr  = "";
+
+   # Visit each line of the source cpp file
+   foreach my $line (<$IN>) {
+
+      # Lua base classes
+      if( $line =~ m|REGISTER_LUA_CLASS\( *(.+?) *\)| ) {
+         my $class = $1;
+         unshift(@{$classes{$class}}, "$shortClassDescr\n$longClassDescr\nclass $class { \n public:\n");   # unshift adds element to beginning of array
+         $writeFile = 1;
+         next;
+      }
+
+      # Lua subclasses
+      if( $line =~ m|REGISTER_LUA_SUBCLASS\( *(.+?) *, *(.+?) *\)| ) {
+         my $class = $1;
+         my $parent = $2;
+         unshift(@{$classes{$class}}, "$shortClassDescr\n$longClassDescr\nclass $class : public $parent { \n public:\n");    
+         $writeFile = 1;
+         $shortClassDescr = "";
+         $longClassDescr = "";
+         next;
+      }
+
+      if( $line =~ m|define +LUA_METHODS\(CLASS, *METHOD\)| ) {
+         $collectingMethods = 1;
+         next;
+      }
+
+      if( $collectingMethods ) {
+         if( $line =~ m|METHOD\( *CLASS, *(.+?) *,| ) {                 # Signals class declaration... methods will follow
+            my $method = $1;
+            push(@methods, $method);
+            next;
+         }
+
+         if( $line =~ m|GENERATE_LUA_METHODS_TABLE\( *(.+?) *,| ) {     # Signals we have all methods for this class, gives us class name; now generate code
+            my $class = $1;
+
+            foreach my $method (@methods) {
+               push(@{$classes{$class}}, "void $method() { }\n");
+            }
+
+            @methods = ();
+            $collectingMethods = 0;
+            next;
+         }
+      }
+
+      if( $line =~ m|/\*\*| ) {              # /** signals the beginning of a long comment block we need to pay attention to
+         $collectingLongComment = 1;
+         push(@comments, "/*!\n");
+         next;
+      }
+
+      if( $collectingLongComment ) {
+
+         # Look for closing */ to terminate our long comment
+         if( $line =~ m|\*/| ) {
+            push(@comments, "*/\n");
+            $collectingLongComment = 0;
+            next;
+         }
+
+         $line =~ s|^\ *\* *||;  # Strip off leading *s and spaces
+
+         # Check for some special custom tags
+         if( $line =~ m|\@luafunc +(.*)$| ) {
+            push(@comments, " \\fn $1\n");
+
+            $line =~ m| (.+?)::(.+?)\((.+)\)|;    # Grab class, method, and args from line that looks like: @luafunc Teleporter::addDest(dest)
+            my $class = $1;
+            my $method = $2;
+            my $args = $3;
+
+            # Find the original class definition and delete it
+            my $index = first { ${$classes{$class}}[$_] eq "void $method() { }\n" } 0..$#{$classes{$class}};
+            splice(@{$classes{$class}}, $index, 1);       # Delete element at $index
+
+            # Add our new sig to the list
+            push(@{$classes{$class}}, "void $method($args) { }\n");
+            
+
+            next;
+         }
+
+         if( $line =~ m|\@luaclass +(.*)$| ) {
+            push(@comments, " \\class $1\n");
+            next;
+         }
+
+         if( $line =~ m|\@descr +(.*)$| ) {
+            push(@comments, "\n $1\n");
+            next;
+         }
+
+         # Otherwise keep the line unaltered
+         push(@comments, $line);
+      }
+   }
+
+   # If we added any lines to keepers, write it out... otherwise skip it!
+   if($writeFile)
+   {
+      my ($name, $path, $suffix) = fileparse($file, (".cpp"));
+      my $outpath = "lua/";
+
+      # Write the simulated .h file
+      my $outfile = $outpath . basename($name) . ".h";
+      open my $OUT, '>', $outfile || die "Can't open $outfile for writing: $!";
+
+      print $OUT "// This file was generated automatically from the C++ source to feed doxygen.  It will be overwritten.\n\n\n";
+
+      foreach my $key ( keys %classes ) {
+         print $OUT @{$classes{$key}};    # Main body of class
+         print $OUT "};\n";               # Close the class
+      }
+
+      print $OUT @comments;
+      
+      close $OUT;           
+   }
+   close $IN;
+}
---
+++ zap/luaLevelGenerator.cpp
@@ -0,0 +1,384 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "luaLevelGenerator.h"
+#include "gameType.h"
+#include "config.h"              // For definition of FolderManager struct
+#include "game.h"
+#include "stringUtils.h"
+#include "ServerGame.h"
+#include "LuaWrapper.h"
+
+namespace Zap
+{
+
+// Default constructor
+LuaLevelGenerator::LuaLevelGenerator() { TNLAssert(false, "Don't use this constructor!"); }
+
+// Standard constructor
+LuaLevelGenerator::LuaLevelGenerator(const string &scriptName, const Vector<string> &scriptArgs, F32 gridSize, 
+                                     GridDatabase *gridDatabase, LevelLoader *caller)
+{
+   TNLAssert(fileExists(scriptName), "Files should be checked before we get here -- something has gone wrong!");
+
+   mScriptName = scriptName;
+   mScriptArgs = scriptArgs;
+
+   mGridDatabase = gridDatabase;
+
+   mGridSize = gridSize;
+   mCaller = caller;
+   mErrorMsgPrefix = "***LEVELGEN ERROR***";
+
+   LUAW_CONSTRUCTOR_INITIALIZATIONS;
+}
+
+
+// Fire up a Lua interprer, load the script, and execute the chunk to get it in memory
+bool LuaLevelGenerator::loadScript()
+{
+   return startLua() && LuaScriptRunner::loadScript();    
+}
+
+
+bool LuaLevelGenerator::runScript()
+{
+   return loadScript() && runMain(); 
+}
+
+
+string LuaLevelGenerator::getScriptName()
+{
+   return mScriptName;
+}
+
+
+#ifndef ZAP_DEDICATED
+/* This function not used anywhere?
+      static ToggleMenuItem *getMenuItem(lua_State *L, S32 index)
+      {
+         LuaUtil::dumpStack(L);
+
+        //luaL_checktype(L, index, LUA_TUSERDATA);      // Confirm the item at index is a full userdata
+        ToggleMenuItem *pushedMenuItem = static_cast<ToggleMenuItem *>(lua_touserdata(L, index));
+        if(pushedMenuItem == NULL) 
+           luaL_typerror(L, index, "ToggleMenuItem");
+
+        //MenuItem im = *pushedMenuItem;
+        //if(!pushedMenuItem)
+        //  luaL_error(L, "null menuItem");
+
+        return pushedMenuItem;
+      }
+*/
+#endif
+
+
+
+const char LuaLevelGenerator::className[] = "LuaLevelGenerator";      // Class name as it appears to Lua scripts
+
+// Used in addItem() below...
+static const char *argv[LevelLoader::MAX_LEVEL_LINE_ARGS];
+
+
+// Destructor
+LuaLevelGenerator::~LuaLevelGenerator()
+{
+   logprintf(LogConsumer::LogLuaObjectLifecycle, "deleted LuaLevelGenerator (%p)\n", this);
+   LUAW_DESTRUCTOR_CLEANUP;
+}
+
+// TODO: Provide mechanism to modify basic level parameters like game length and teams.
+
+
+// Note that this uses rawgeti and therefore bypasses any metamethods set on the table
+S32 getIntegerFromTable(lua_State *L, int tableIndex, int key)
+{
+   lua_rawgeti(L, tableIndex, key);    // Push value onto stack
+   if(lua_isnil(L, -1))
+   {
+      lua_pop(L, 1);
+      return 0;
+   }
+
+   S32 rtn = (S32)lua_tointeger(L, -1);
+   lua_pop(L, 1);    // Clear value from stack
+   return rtn;
+}
+
+
+// TODO: get rid of this function!!!!
+// Pop a vec object off stack, check its type, and return it
+static Point getCheckedVec(lua_State *L, S32 index, const char *methodName)
+{
+   if(!lua_isvec(L, index))
+   {
+      char msg[256];
+      dSprintf(msg, sizeof(msg), "%s expected vector arg at position %d", methodName, index);
+      logprintf(LogConsumer::LogError, msg);
+
+      throw LuaException(msg);
+   }
+
+   const F32 *vec = lua_tovec(L, index);
+   return Point(vec[0], vec[1]);
+}
+
+
+Point LuaLevelGenerator::getPointFromTable(lua_State *L, int tableIndex, int key, const char *methodName)
+{
+   lua_rawgeti(L, tableIndex, key);    // Push Point onto stack
+   if(lua_isnil(L, -1))
+   {
+      lua_pop(L, 1);
+      return Point(0,0);
+   }
+
+   Point point = getCheckedVec(L, -1, methodName);
+   lua_pop(L, 1);    // Clear value from stack
+
+   return point;
+}
+
+
+S32 LuaLevelGenerator::addWall(lua_State *L)
+{
+   static const char *methodName = "LevelGeneratorEditor:addWall()";
+
+   string line = "BarrierMaker";
+
+   try
+   {
+      F32 width = getCheckedFloat(L, 1, methodName);      // Width is first arg
+      line += " " + ftos(width, 1);
+
+      // Third arg is a table of coordinate values in "editor space" (i.e. these will be multiplied by gridsize before being used)
+      S32 points = (S32)lua_objlen(L, 3);    // Get the number of points in our table of points
+
+      for(S32 i = 1; i <= points; i++)       // Remember, Lua tables start with index 1
+      {
+         Point p = getPointFromTable(L, 3, i, methodName);
+         line = line + " " + ftos(p.x, 2) + " " + ftos(p.y, 2);
+      }
+   }
+
+   catch(LuaException &e)
+   {
+      logError(e.what());
+   }
+
+   mCaller->parseLevelLine(line.c_str(), mGridDatabase, "Levelgen script: " + mScriptName);
+
+   return 0;
+}
+
+
+// Simply grabs parameters from the Lua stack, and passes them off to processLevelLoadLine().  Unfortunately,
+// this involves packing everything into an array of char strings, which is frightfully prone to programmer
+// error and buffer overflows and such...
+S32 LuaLevelGenerator::addItem(lua_State *L)
+{
+   static const char *methodName = "LevelGenerator:addItem()";
+
+   S32 argc = min(lua_gettop(L), LevelLoader::MAX_LEVEL_LINE_ARGS);     // Never more that MaxArgc args, please.
+
+   if(argc == 0)
+   {
+      logError("Object had no parameters, ignoring...");
+      return 0;
+   }
+
+   for(S32 i = 0; i < argc; i++)      // argc was already bounds checked above
+      argv[i] = getCheckedString(L, i + 1, methodName);
+
+   processLevelLoadLine(argc, 0, argv, mGridDatabase, "Levelgen script: " + mScriptName);      // For now, all ids are 0!
+
+   return 0;
+}
+
+
+// Let someone else do the work!
+void LuaLevelGenerator::processLevelLoadLine(int argc, U32 id, const char **argv, GridDatabase *database, const string &levelFileName)
+{
+   mCaller->processLevelLoadLine(argc, id, argv, database, levelFileName);
+}
+
+
+// Feed a raw level line into the level processor
+S32 LuaLevelGenerator::addLevelLine(lua_State *L)
+{
+   static const char *methodName = "LevelGenerator:addLevelLine()";
+
+   checkArgCount(L, 1, methodName);
+   const char *line = getCheckedString(L, 1, methodName);
+
+   mCaller->parseLevelLine(line, mGridDatabase, "Levelgen script: " + mScriptName);
+
+   return 0;
+}
+
+
+// Set the duration of this level
+S32 LuaLevelGenerator::setGameTime(lua_State *L)
+{
+   static const char *methodName = "LevelGenerator:setGameTime()";
+
+   checkArgCount(L, 1, methodName);
+   F32 time = getCheckedFloat(L, 1, methodName);
+
+   mCaller->setGameTime(time);
+
+   return 0;
+}
+
+
+S32 LuaLevelGenerator::pointCanSeePoint(lua_State *L)
+{
+   static const char *methodName = "LevelGenerator:pointCanSeePoint()";
+
+   checkArgCount(L, 2, methodName);
+
+   // Still need mGridSize because we deal with the coordinates used in the level file, which have to be multiplied by
+   // GridSize to get in-game coordinates
+   Point p1 = getCheckedVec(L, 1, methodName) *= mGridSize;    
+   Point p2 = getCheckedVec(L, 2, methodName) *= mGridSize;
+
+   return returnBool(L, mGridDatabase->pointCanSeePoint(p1, p2));
+}
+
+
+// Write a message to the server logfile
+S32 LuaLevelGenerator::logprint(lua_State *L)
+{
+   static const char *methodName = "LuaLevelGenerator:logprint()";
+   checkArgCount(L, 1, methodName);
+
+   logprintf(LogConsumer::LuaLevelGenerator, "LuaLevelGenerator: %s", getCheckedString(L, 1, methodName));
+   return 0;
+}
+
+
+S32 LuaLevelGenerator::getGridSize(lua_State *L)
+{
+   return returnFloat(L, mGridSize);
+}
+
+
+S32 LuaLevelGenerator::getPlayerCount(lua_State *L)
+{
+   return returnInt(L, gServerGame ? gServerGame->getPlayerCount() : 1 );
+}
+
+
+///// Initialize levelgen specific stuff
+bool LuaLevelGenerator::prepareEnvironment()
+{
+   LuaScriptRunner::prepareEnvironment();
+
+   if(!loadAndRunGlobalFunction(L, LUA_HELPER_FUNCTIONS_KEY) || !loadAndRunGlobalFunction(L, LEVELGEN_HELPER_FUNCTIONS_KEY))
+      return false;
+
+   lua_getfield(L, LUA_REGISTRYINDEX, getScriptId());    // Put script's env table onto the stack  -- env_table
+
+   // TODO: Do we still really need GRID_SIZE?           
+   lua_pushnumber(L, mGridSize);                         //                                        -- env_table, mGridSize
+   lua_pushliteral(L, "_GRID_SIZE");                     //                                        -- env_table, mGridSize, "_GRID_SIZE"
+   lua_rawset(L, -3);                                    // env_table["_GRID_SIZE"] = *this        -- env_table
+                                                         
+   lua_pushliteral(L, "levelgen");                       //                                        -- env_table, "levelgen"
+   luaW_push(L, this);                                   //                                        -- env_table, "levelgen", *this
+   lua_rawset(L, -3);                                    // env_table["levelgen"] = *this          -- env_table
+
+   lua_pop(L, 1);                                        // Cleanup                                -- <<empty stack>>
+
+   TNLAssert(lua_gettop(L) == 0 || LuaObject::dumpStack(L), "Stack not cleared!");
+
+   return true;
+}
+
+
+// Send message to all players
+S32 LuaLevelGenerator::globalMsg(lua_State *L)
+{
+   static const char *methodName = "LuaLevelGenerator:globalMsg()";
+   checkArgCount(L, 1, methodName);
+
+   const char *message = getCheckedString(L, 1, methodName);
+
+   GameType *gt = gServerGame->getGameType();
+   if(gt)
+   {
+      gt->sendChatFromController(message);
+
+      // Fire our event handler
+      EventManager::get()->fireEvent(getScriptId(), EventManager::MsgReceivedEvent, message, NULL, true);
+   }
+
+   return 0;
+}
+
+
+// Register our connector types with Lua
+void LuaLevelGenerator::registerClasses()
+{
+   // General classes
+   LuaScriptRunner::registerClasses();    // LuaScriptRunner is a parent class
+
+   // Specific classes needed for LevelGen scripts
+   //Lunar<LuaLevelGenerator>::Register(L);
+}
+
+
+//// Lua methods
+const char *LuaLevelGenerator::luaClassName = "LuaLevelGenerator";
+
+const luaL_reg LuaLevelGenerator::luaMethods[] =
+{
+   { "addWall",          luaW_doMethod<LuaLevelGenerator, &LuaLevelGenerator::addWall>          },
+   { "addItem",          luaW_doMethod<LuaLevelGenerator, &LuaLevelGenerator::addItem>          },
+   { "addLevelLine",     luaW_doMethod<LuaLevelGenerator, &LuaLevelGenerator::addLevelLine>     },
+                                                                                                
+   { "getGridSize",      luaW_doMethod<LuaLevelGenerator, &LuaLevelGenerator::getGridSize>      },
+   { "getPlayerCount",   luaW_doMethod<LuaLevelGenerator, &LuaLevelGenerator::getPlayerCount>   },
+   { "setGameTime",      luaW_doMethod<LuaLevelGenerator, &LuaLevelGenerator::setGameTime>      },
+   { "pointCanSeePoint", luaW_doMethod<LuaLevelGenerator, &LuaLevelGenerator::pointCanSeePoint> },
+
+   { "globalMsg",        luaW_doMethod<LuaLevelGenerator, &LuaLevelGenerator::globalMsg>        },
+
+   { NULL, NULL }   
+};
+
+
+const LuaFunctionProfile LuaLevelGenerator::functionArgs[] =
+{
+   { NULL, { }, 0 }
+};
+
+
+REGISTER_LUA_CLASS(LuaLevelGenerator);
+
+
+};
+
