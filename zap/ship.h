6da633863131f90c882225292327030f80515fc2
watusimoto <watusimoto@bitfighter.org>
2012-12-23 14:41:08 +0100

    Take care of TODO, remove unneeded methods, streamline color assignment for ships.
    Once again, what started out as a trivial refactor (making a method variable no longer be public)
    has turned unto an epic refactor.  As before, assuming no new bugs introduces, code should be
    more streamlined, easier to follow, and perform better.

---
+++ zap/ship.h
@@ -0,0 +1,339 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _SHIP_H_
+#define _SHIP_H_
+
+#include "item.h"    // for Item and LuaItem
+#include "BfObject.h"
+#include "moveObject.h"
+#include "LuaScriptRunner.h"
+#include "SoundEffect.h"
+#include "Timer.h"
+#include "shipItems.h"
+#include "gameWeapons.h"
+
+#ifndef ZAP_DEDICATED
+#  include "sparkManager.h"
+#  include "ShipShape.h"
+#endif
+
+namespace Zap
+{
+
+class SpeedZone;
+class Statistics;
+class ClientInfo;
+class Teleporter;
+
+// class derived_class_name: public base_class_name
+class Ship : public MoveObject
+{
+   typedef MoveObject Parent;
+
+private:
+   bool mIsRobot;
+
+   U32 mRespawnTime;
+   Vector<DatabaseObject *> mZones1;    // A list of zones the ship is currently in
+   Vector<DatabaseObject *> mZones2;
+   bool mZones1IsCurrent;
+   bool mFastRecharging;
+
+   SafePtr<Teleporter> mEngineeredTeleporter;
+
+   // Find objects of specified type that may be under the ship, and put them in fillVector
+   void findObjectsUnderShip(U8 typeNumber);
+
+   // Idle helpers
+   void checkForSpeedzones();    
+   void checkForZones();
+
+   Vector<DatabaseObject *> *getCurrZoneList();    // Get list of zones ship is currently in
+   Vector<DatabaseObject *> *getPrevZoneList();    // Get list of zones ship was in last tick
+
+protected:
+   enum MaskBits {
+      MoveMask            = Parent::FirstFreeMask << 0, // New user input
+      HealthMask          = Parent::FirstFreeMask << 1,
+      ModulePrimaryMask   = Parent::FirstFreeMask << 2, // Is module primary component active
+      ModuleSecondaryMask = Parent::FirstFreeMask << 3, // Is module secondary component active
+      LoadoutMask         = Parent::FirstFreeMask << 4,
+      RespawnMask         = Parent::FirstFreeMask << 5, // For when robots respawn
+      TeleportMask        = Parent::FirstFreeMask << 6, // Ship has just teleported
+      ChangeTeamMask      = Parent::FirstFreeMask << 7, // Used for when robots change teams
+      SpawnShieldMask     = Parent::FirstFreeMask << 8, // Used for the spawn shield
+      FirstFreeMask       = Parent::FirstFreeMask << 9
+   };
+
+   SafePtr <ClientInfo> mClientInfo;
+
+   bool mModulePrimaryActive[ModuleCount];       // Is the primary component of the module active at this moment?
+   bool mModuleSecondaryActive[ModuleCount];     // Is the secondary component of the module active?
+
+   Vector<SafePtr<MountableItem> > mMountedItems;    
+
+   ShipModule mModule[ShipModuleCount];   // Modules ship is carrying
+   WeaponType mWeapon[ShipWeaponCount];
+   Point mSpawnPoint;                     // Where ship or robot spawned.  Will only be valid on server, client doesn't currently get this.
+
+   void initialize(const Point &pos);     // Some initialization code needed by both bots and ships
+   void initialize(ClientInfo *clientInfo, S32 team, const Point &pos, bool isRobot);
+
+   bool processArguments(S32 argc, const char **argv, Game *game);
+   string toString(F32 gridSize) const;
+
+public:
+   static const S32 CollisionRadius = 24;
+   static const S32 RepairRadius = 65;
+   static const U32 SpawnShieldTime = 5000;        // Time spawn shields are active
+   static const U32 SpawnShieldFlashTime = 1500;   // Time at which shields start to flash
+   static const S32 PulseMaxVelocity = 2500;       // Maximum speed of Pulse
+   static const S32 PulseMinVelocity = 1000;       // Minimum speed of Pulse
+   static const S32 EnergyMax = 100000;            // Energy when fully charged   TODO: Make this lower resolution
+
+   enum {
+      MaxVelocity = 450,        // points per second
+      Acceleration = 2500,      // points per second per second
+      BoostMaxVelocity = 700,   // points per second
+      BoostAcceleration = 5000, // points per second per second
+
+      VisibilityRadius = 30,
+      KillDeleteDelay = 1500,
+      ExplosionFadeTime = 300,
+      MaxControlObjectInterpDistance = 200,
+      TrailCount = 2,
+
+      // The following are all measured in units of energy/millisecond
+      EnergyRechargeRate = 8,
+      EnergyRechargeRateMovementModifier = -2,
+      EnergyRechargeRateInHostileLoadoutZoneModifier = -20,
+      EnergyRechargeRateInNeutralLoadoutZoneModifier = 4,
+      EnergyRechargeRateInFriendlyLoadoutZoneModifier = 4,
+      EnergyRechargeRateInEnemyLoadoutZoneModifier = -2,
+      EnergyRechargeRateIdleRechargeCycle = 40,
+
+      EnergyShieldHitDrain = 20000,       // Energy loss when shields stop a projectile (currently disabled)
+      EnergyCooldownThreshold = 15000,
+      WeaponFireDecloakTime = 350,
+      SensorZoomTime = 300,
+      SensorCloakInnerDetectionDistance = 300,
+      SensorCloakOuterDetectionDistance = 500,
+      CloakFadeTime = 300,
+      CloakCheckRadius = 200,
+      RepairHundredthsPerSecond = 16,
+      MaxEngineerDistance = 100,
+      WarpFadeInTime = 500,
+   };
+
+   S32 mFireTimer;
+   Timer mWarpInTimer;
+   F32 mHealth;
+   S32 mEnergy;
+
+   bool mCooldownNeeded;
+   Point mImpulseVector;
+
+   virtual bool canAddToEditor();
+
+   F32 getSlipzoneSpeedMoficationFactor();
+
+   SFXHandle mModuleSound[ModuleCount];
+
+   U32 mActiveWeaponIndx;                 // Index of selected weapon on ship
+
+   void selectNextWeapon();                   
+   void selectPrevWeapon();
+   void selectWeapon(S32 weaponIndex);    // Select weapon by index
+   WeaponType getWeapon(U32 indx);        // Returns weapon in slot indx
+   ShipModule getModule(U32 indx);        // Returns module in slot indx
+
+
+   Timer mSensorEquipZoomTimer;
+   Timer mWeaponFireDecloakTimer;
+   Timer mCloakTimer;
+   Timer mSpawnShield;
+   Timer mModuleSecondaryTimer[ModuleCount];  // Timer to prevent accidentally firing in quick succession
+   Timer mSpyBugPlacementTimer;
+   Timer mFastRechargeTimer;
+   static const U32 ModuleSecondaryTimerDelay = 500;
+   static const U32 SpyBugPlacementTimerDelay = 800;
+   static const U32 IdleRechargeCycleTimerDelay = 2000;
+
+   void setChangeTeamMask();
+
+#ifndef ZAP_DEDICATED
+   U32 mSparkElapsed;
+   S32 mLastTrailPoint[TrailCount];  // TrailCount = 2
+   FXTrail mTrail[TrailCount];
+   ShipShape::ShipShapeType mShapeType;
+#endif
+
+   F32 mass;            // Mass of ship, not used
+   bool hasExploded;
+
+   Vector<SafePtr<BfObject> > mRepairTargets;            // TODO: Make this protected
+
+   virtual void render(S32 layerIndex);
+   void calcThrustComponents(F32 *thrust);
+
+   // Constructor
+   Ship(ClientInfo *clientInfo, S32 team, const Point &p, bool isRobot = false);   // Standard constructor   
+   Ship(lua_State *L = NULL);                                                      // Combined Lua / C++ default constructor
+   ~Ship();                                                                        // Destructor
+
+   F32 getHealth();
+   S32 getEnergy();
+   F32 getEnergyFraction();     // Only used by bots
+   S32 getMaxEnergy();
+
+   // Related to mounting and carrying items
+   S32 getMountedItemCount() const;
+
+   bool isCarryingItem(U8 objectType) const;
+   MountableItem *unmountItem(U8 objectType);
+
+   MountableItem *getMountedItem(S32 index) const;
+   void addMountedItem(MountableItem *item);
+   void removeMountedItem(MountableItem *item);
+
+   S32 getFlagIndex();     // Returns index of first flag, or NO_FLAG if ship has no flags
+   S32 getFlagCount();     // Returns the number of flags ship is carrying
+
+
+
+   void onGhostRemove();
+
+   bool isModulePrimaryActive(ShipModule mod);
+   bool isModuleSecondaryActive(ShipModule mod);
+
+   bool hasModule(ShipModule mod);
+
+   bool isDestroyed();
+   bool isItemMounted();    // <== unused
+   bool isVisible(bool viewerHasSensor);
+
+   void creditEnergy(S32 deltaEnergy);
+
+   void setEngineeredTeleporter(Teleporter *teleporter);
+   Teleporter *getEngineeredTeleporter();
+   F32 getSensorZoomFraction();
+   Point getAimVector();
+
+   void deploySpybug();
+
+   void getLoadout(Vector<U8> &loadout);    // Fills loadout
+   bool setLoadout(const Vector<U8> &loadout, bool silent = false);
+   bool isLoadoutSameAsCurrent(const Vector<U8> &loadout);
+   void setDefaultLoadout();                 // Set the ship's loadout to the default values
+
+   ClientInfo *getClientInfo();
+   static string loadoutToString(const Vector<U8> &loadout);
+   static bool stringToLoadout(string loadoutStr, Vector<U8> &loadout);
+
+
+   virtual void idle(IdleCallPath path);
+
+   virtual void processMove(U32 stateIndex);
+
+   WeaponType getSelectedWeapon();   // Return currently selected weapon
+   U32 getSelectedWeaponIndex();     // Return index of currently selected weapon (0, 1, 2)
+
+   void processWeaponFire();
+   void processModules();
+   void rechargeEnergy();
+
+   void updateModuleSounds();
+   void emitMovementSparks();
+   void findRepairTargets();
+   void repairTargets();
+
+   void controlMoveReplayComplete();
+   void onAddedToGame(Game *game);
+
+   void emitShipExplosion(Point pos);
+   //void setActualPos(Point p);
+   void setActualPos(Point p, bool warp);
+   void activateModulePrimary(U32 indx);    // Activate the specified module primary component for the current move
+   void activateModuleSecondary(U32 indx);  // Activate the specified module secondary component for the current move
+
+   virtual void kill(DamageInfo *theInfo);
+   virtual void kill();
+
+   void destroyTeleporter();
+
+   virtual void damageObject(DamageInfo *theInfo);
+
+   static void computeMaxFireDelay();
+
+   void writeControlState(BitStream *stream);
+   void readControlState(BitStream *stream);
+
+   U32 packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream);
+   void unpackUpdate(GhostConnection *connection, BitStream *stream);
+   F32 getUpdatePriority(NetObject *scopeObject, U32 updateMask, S32 updateSkips);
+
+   bool isRobot();
+
+   BfObject *isInZone(U8 zoneType);           // Return whether the ship is currently in a zone of the specified type, and which one
+   //BfObject *isInZone(BfObject *zone);
+   DatabaseObject *isOnObject(U8 objectType); // Returns the object in question if this ship is on an object of type objectType
+
+   bool isOnObject(BfObject *object);         // Return whether or not ship is sitting on a particular object
+
+   virtual Ship *clone() const;
+
+   TNL_DECLARE_CLASS(Ship);
+
+   //// Lua interface
+   LUAW_DECLARE_CLASS_CUSTOM_CONSTRUCTOR(Ship);
+
+	static const char *luaClassName;
+	static const luaL_reg luaMethods[];
+   static const LuaFunctionProfile functionArgs[];
+
+   S32 isAlive(lua_State *L);
+   S32 getAngle(lua_State *L);
+   S32 hasFlag(lua_State *L);
+
+   S32 getEnergy(lua_State *L);  // Return ship's energy as a fraction between 0 and 1
+   S32 getHealth(lua_State *L);  // Return ship's health as a fraction between 0 and 1
+
+   S32 getFlagCount(lua_State *L);
+
+   S32 getPlayerInfo(lua_State *L);
+   S32 isModActive(lua_State *L);
+   S32 getMountedItems(lua_State *L);
+   S32 getCurrLoadout(lua_State *L);
+   S32 getReqLoadout(lua_State *L);
+
+   S32 getActiveWeapon(lua_State *L);                // Get WeaponIndex for current weapon
+};
+
+
+};
+
+#endif
+
---
+++ zap/textItem.cpp
@@ -0,0 +1,481 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "textItem.h"
+#include "gameNetInterface.h"
+#include "gameObjectRender.h"    // For renderTextItem()
+#include "game.h"
+#include "item.h"
+#include "stringUtils.h"
+#include "teamInfo.h"            // For TEAM_NEUTRAL
+
+#ifndef ZAP_DEDICATED
+#  include "ClientGame.h"
+#  include "UIEditorMenus.h"     // For TextItemEditorAttributeMenuUI def
+#  include "OpenglUtils.h"
+#endif
+
+
+#include <math.h>
+
+namespace Zap
+{
+
+TNL_IMPLEMENT_NETOBJECT(TextItem);
+
+
+#ifndef ZAP_DEDICATED
+EditorAttributeMenuUI *TextItem::mAttributeMenuUI = NULL;
+#endif
+
+// Combined Lua / C++ constructor
+TextItem::TextItem(lua_State *L)
+{
+   mNetFlags.set(Ghostable);
+   mObjectTypeNumber = TextItemTypeNumber;
+
+   // Some default values
+   mSize = 20;
+
+   LUAW_CONSTRUCTOR_INITIALIZATIONS;
+}
+
+
+// Destructor
+TextItem::~TextItem()
+{
+   LUAW_DESTRUCTOR_CLEANUP;
+}
+
+
+TextItem *TextItem::clone() const
+{
+   return new TextItem(*this);
+}
+
+
+string TextItem::getAttributeString()
+{
+   return "Text: " + mText;
+}
+
+
+const char *TextItem::getInstructionMsg()
+{
+   return "[Enter] to edit text";
+}
+
+
+void TextItem::newObjectFromDock(F32 gridSize)
+{
+   mText = "Your text here";
+
+   Parent::newObjectFromDock(gridSize);
+
+   recalcTextSize();    // Has to be after Parent::newObjectFromDock(gridSize); that sets the length of the line, which changes text size
+}
+
+
+// In game rendering
+void TextItem::render()
+{
+#ifndef ZAP_DEDICATED
+   ClientGame *game = static_cast<ClientGame *>(getGame());
+   
+   // Don't render opposing team's text items if we are in a game... but in editor preview mode, where
+   // we don't have a connection to the server, text will be rendered normally
+   if(game->getConnectionToServer())
+   {
+      Ship *ship = dynamic_cast<Ship *>(game->getConnectionToServer()->getControlObject());
+      if( (!ship && getTeam() != TEAM_NEUTRAL) || (ship && ship->getTeam() != getTeam() && getTeam() != TEAM_NEUTRAL) )
+         return;
+   }
+
+   renderEditorItem();
+#endif
+}
+
+
+// Called by SimpleItem::renderEditor()
+void TextItem::renderEditorItem()
+{
+   renderTextItem(getVert(0), getVert(1), mSize, mText, getColor());
+}
+
+
+const char *TextItem::getOnScreenName()     { return "Text";      }
+const char *TextItem::getOnDockName()       { return "TextItem";  }
+const char *TextItem::getPrettyNamePlural() { return "TextItems"; }
+const char *TextItem::getEditorHelpString() { return "Draws a bit of text on the map.  Visible only to team, or to all if neutral."; }
+
+
+bool TextItem::hasTeam()      { return true; }
+bool TextItem::canBeHostile() { return true; }
+bool TextItem::canBeNeutral() { return true; }
+
+
+Color TextItem::getEditorRenderColor()
+{
+   return Colors::blue;
+}
+
+
+F32 TextItem::getSize()
+{
+   return mSize;
+}
+
+
+// Set text size subject to min and max defined in TextItem
+void TextItem::setSize(F32 desiredSize)
+{
+   mSize = max(min(desiredSize, (F32)MAX_TEXT_SIZE), (F32)MIN_TEXT_SIZE);
+}
+
+
+string TextItem::getText()
+{
+   return mText;
+}
+
+
+void TextItem::setText(string text)
+{
+   mText = text;
+   onGeomChanged();
+}
+
+
+// This object should be drawn below others
+S32 TextItem::getRenderSortValue()
+{
+   return 1;
+}
+
+
+// Create objects from parameters stored in level file
+// Entry looks like: TextItem 0 50 10 10 11 11 Message goes here
+bool TextItem::processArguments(S32 argc, const char **argv, Game *game)
+{
+   if(argc < 7)
+      return false;
+
+   setTeam(atoi(argv[0]));
+
+   Point pos, dir;
+
+   pos.read(argv + 1);
+   pos *= game->getGridSize();
+
+   dir.read(argv + 3);
+   dir *= game->getGridSize();
+
+   setSize((F32)atof(argv[5]));
+
+   // Assemble any remainin args into a string
+   mText = "";
+   for(S32 i = 6; i < argc; i++)
+   {
+      mText += argv[i];
+      if(i < argc - 1)
+         mText += " ";
+   }
+
+   setGeom(pos, dir);
+
+   return true;
+}
+
+
+void TextItem::setGeom(const Vector<Point> &points)
+{
+   if(points.size() >= 2)
+      setGeom(points[0], points[1]);
+}
+
+
+void TextItem::setGeom(const Point &pos, const Point &dest)
+{
+   setVert(pos, 0);
+   setVert(dest, 1);
+
+   updateExtentInDatabase();
+}
+
+
+string TextItem::toString(F32 gridSize) const
+{
+   return string(appendId(getClassName())) + " " + itos(getTeam()) + " " + geomToString(gridSize) + " " + ftos(mSize, 3) + " " + writeLevelString(mText.c_str());
+}
+
+
+// Editor
+void TextItem::recalcTextSize()
+{
+#ifndef ZAP_DEDICATED
+   const F32 dummyTextSize = 120;
+
+   F32 lineLen = getVert(0).distanceTo(getVert(1));      // In in-game units
+   F32 strWidth = F32(UserInterface::getStringWidth(dummyTextSize, mText.c_str())) / dummyTextSize; 
+   F32 size = lineLen / strWidth;
+
+  setSize(size);
+#endif
+}
+
+
+void TextItem::onAddedToGame(Game *theGame)
+{
+   Parent::onAddedToGame(theGame);
+
+   if(!isGhost())
+      setScopeAlways();
+}
+
+
+// Bounding box for display scoping purposes
+Rect TextItem::calcExtents()
+{
+#ifdef ZAP_DEDICATED
+   // Don't care much about it on the server, as server won't render, and nothing collides with TextItems
+	return(Rect(getVert(0), getVert(1)));
+#else
+   F32 len = UserInterface::getStringWidth(mSize, mText.c_str());
+   //F32 buf = mSize / 2;     // Provides some room to accomodate descenders on letters like j and g.
+
+   F32 angle =  getVert(0).angleTo(getVert(1));
+   F32 sinang = sin(angle);
+   F32 cosang = cos(angle);
+
+   F32 descenderFactor = .35f;    // To account for y, g, j, etc.
+   F32 h = mSize * (1 + descenderFactor);
+   F32 w = len * 1.05f;           // 1.05 adds just a little horizontal padding for certain words with trailing ys or other letters that are just a tiny bit longer than calculated
+   F32 x = getVert(0).x + mSize * descenderFactor * sinang;
+   F32 y = getVert(0).y + mSize * descenderFactor * cosang;
+
+   F32 c1x = x - h * sinang * .5f;
+   F32 c1y = y;
+
+   F32 c2x = x + w * cosang - h * sinang * .5f;
+   F32 c2y = y + w * sinang;
+
+   F32 c3x = x + h * sinang * .5f + w * cosang;
+   F32 c3y = y - h * cosang + w * sinang;
+
+   F32 c4x = x + h * sinang * .5f;
+   F32 c4y = y - h * cosang;
+
+   F32 minx = min(c1x, min(c2x, min(c3x, c4x)));
+   F32 miny = min(c1y, min(c2y, min(c3y, c4y)));
+   F32 maxx = max(c1x, max(c2x, max(c3x, c4x)));
+   F32 maxy = max(c1y, max(c2y, max(c3y, c4y)));
+
+   Rect extent(Point(minx, miny), Point(maxx, maxy));
+
+   return extent;
+#endif
+}
+
+
+const Vector<Point> *TextItem::getCollisionPoly() const
+{
+   return NULL;
+}
+
+
+// Handle collisions with a TextItem.  Easy, there are none.
+bool TextItem::collide(BfObject *hitObject)
+{
+   return false;
+}
+
+
+void TextItem::idle(BfObject::IdleCallPath path)
+{
+   // Laze about, read a book, take a nap, whatever
+}
+
+
+U32 TextItem::packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream)
+{
+   Point pos = getVert(0);
+   Point dir = getVert(1);
+
+   pos.write(stream);
+   dir.write(stream);
+
+   stream->writeRangedU32((U32)mSize, 0, MAX_TEXT_SIZE);
+   writeThisTeam(stream);
+
+   stream->writeString(mText.c_str(), (U8) mText.length());      // Safe to cast text.length to U8 because we've limited it's length to MAX_TEXTITEM_LEN
+
+   return 0;
+}
+
+
+void TextItem::unpackUpdate(GhostConnection *connection, BitStream *stream)
+{
+   char txt[MAX_TEXTITEM_LEN];
+
+   Point pos, dir;
+
+   pos.read(stream);
+   dir.read(stream);
+
+   setVert(pos, 0);
+   setVert(dir, 1);
+
+   mSize = (F32)stream->readRangedU32(0, MAX_TEXT_SIZE);
+   readThisTeam(stream);
+
+   stream->readString(txt);
+
+   mText = txt;
+   updateExtentInDatabase();
+}
+
+
+///// Editor Methods
+
+void TextItem::onAttrsChanging() { onGeomChanged(); }    // Runs when text is being changed in the editor
+void TextItem::onAttrsChanged()  { onGeomChanged(); }
+void TextItem::onGeomChanging()  { onGeomChanged(); }
+                                 
+void TextItem::onGeomChanged()
+{
+   recalcTextSize();
+   Parent::onGeomChanged();
+}
+
+
+#ifndef ZAP_DEDICATED
+// Static method: Provide hook into the object currently being edited with the attrubute editor for callback purposes
+BfObject *TextItem::getAttributeEditorObject()     
+{
+   return mAttributeMenuUI->getObject(); 
+}
+
+
+static void textEditedCallback(string text)
+{
+   TextItem *textItem = dynamic_cast<TextItem *>(TextItem::getAttributeEditorObject());
+   textItem->setText(text);
+}
+
+
+EditorAttributeMenuUI *TextItem::getAttributeMenu()
+{
+   // Lazily initialize this -- if we're in the game, we'll never need this to be instantiated
+   if(!mAttributeMenuUI)
+   {
+      ClientGame *clientGame = static_cast<ClientGame *>(getGame());
+      mAttributeMenuUI = new EditorAttributeMenuUI(clientGame);
+
+      // "Blah" will be overwritten when startEditingAttrs() is called
+      TextEntryMenuItem *menuItem = new TextEntryMenuItem("Text: ", "Blah", "", "", MAX_TEXTITEM_LEN);
+      menuItem->setTextEditedCallback(textEditedCallback);
+
+      mAttributeMenuUI->addMenuItem(menuItem);
+
+      // Add our standard save and exit option to the menu
+      mAttributeMenuUI->addSaveAndQuitMenuItem();
+   }
+
+   return mAttributeMenuUI;
+}
+
+
+// Get the menu looking like what we want
+void TextItem::startEditingAttrs(EditorAttributeMenuUI *attributeMenu)
+{
+   attributeMenu->getMenuItem(0)->setValue(mText);
+}
+
+
+// Retrieve the values we need from the menu
+void TextItem::doneEditingAttrs(EditorAttributeMenuUI *attributeMenu)
+{
+   mText = attributeMenu->getMenuItem(0)->getValue();
+}
+
+#endif
+
+
+//// Lua methods
+
+/**
+  *  @luaclass TextItem
+  *  @brief Display text message in level.
+  *  @descr A %TextItem displays text in a level.  If the %TextItem belongs to a team, it is only visible to players on that team.
+  *         If it is assigned to NeutralTeam (the default), it will be visible to all players.  Text is always displayed in the color
+  *         of the team it belongs to.
+  *
+  * Note that you will likely want to set the text of a new %TextItem (see setText()), as, by default, the display string is blank.
+  *
+  * Geometry for a %TextItem consists of two points representing the start and end points of the item.  Text will be scaled to
+  * fit between these points.
+  */
+//               Fn name     Param profiles       Profile count                           
+#define LUA_METHODS(CLASS, METHOD) \
+   METHOD(CLASS, setText,      ARRAYDEF({{ STR, END }}), 1 ) \
+   METHOD(CLASS, getText,      ARRAYDEF({{      END }}), 1 ) \
+
+GENERATE_LUA_METHODS_TABLE_NEW(TextItem, LUA_METHODS);
+GENERATE_LUA_FUNARGS_TABLE(TextItem, LUA_METHODS);
+
+#undef LUA_METHODS
+
+
+const char *TextItem::luaClassName = "TextItem";
+REGISTER_LUA_SUBCLASS(TextItem, BfObject);
+
+
+/** 
+  *  @luafunc TextItem::setText(text)
+  *  @brief Sets the text of a %TextItem.
+  *  @param text - A \e string specifying what the %TextItem should display.
+  */
+S32 TextItem::lua_setText(lua_State *L)
+{
+   checkArgList(L, functionArgs, "TextItem", "setText");
+
+   setText(getString(L, 1));
+
+   return 0;
+}
+
+
+/** 
+  *  @luafunc text TextItem::getText()
+  *  @brief Sets the text of a %TextItem.
+  *  @return text - A \e string specifying what the %TextItem is currently displaying.
+  */
+S32 TextItem::lua_getText(lua_State *L)
+{
+   return returnString(L, getText().c_str());
+}
+
+
+};
