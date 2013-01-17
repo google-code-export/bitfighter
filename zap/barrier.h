475ee24dc3b89763cdc776c03fd9d92b44409d27
sam8641 <sam8641@localhost>
2013-01-17 17:35:59 -0600

    Fix GoalZone not blinking in Zone Control

---
+++ zap/barrier.h
@@ -0,0 +1,345 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _BARRIER_H_
+#define _BARRIER_H_
+
+#include "BfObject.h"
+#include "polygon.h"       // For PolygonObject def
+#include "LineItem.h"   
+
+#include "Point.h"
+#include "tnlVector.h"
+#include "tnlNetObject.h"
+
+namespace Zap
+{
+
+/// The Barrier class represents rectangular barriers that player controlled
+/// ships cannot pass through... i.e. walls  Barrier objects, once created, never
+/// change state, simplifying the pack/unpack update methods.  Barriers are
+/// constructed as an expanded line segment.
+class Barrier : public BfObject
+{
+   typedef BfObject Parent;
+
+private:
+   Vector<Point> mBufferedObjectPointsForBotZone;
+
+   // Takes a segment and "puffs it out" to a polygon for bot zone generation.
+   // This polygon is the width of the barrier plus the ship's collision radius added to the outside
+   static void computeBufferForBotZone(const Vector<Point> &points, F32 width, bool isPolywall, Vector<Point> &bufferedPoints);
+
+public:
+   // Constructor
+   Barrier(const Vector<Point> &points = Vector<Point>(), F32 width = DEFAULT_BARRIER_WIDTH, bool solid = false);
+
+   Vector<Point> mPoints;  // The points of the barrier --> if only two, first will be start, second end of an old-school segment
+
+   bool mSolid;            // True if this represents a polywall
+
+   // By precomputing and storing, we should ease the rendering cost
+   Vector<Point> mRenderFillGeometry;        // Actual geometry used for rendering fill
+   const Vector<Point> *mRenderOutlineGeometry;     // Actual geometry used for rendering outline
+
+   F32 mWidth;
+
+   static const S32 MIN_BARRIER_WIDTH = 1;         // Clipper doesn't much like 0 width walls
+   static const S32 MAX_BARRIER_WIDTH = 2500;      // Geowar has walls at least 350 units wide, so going lower will break at least one level
+
+   static const S32 DEFAULT_BARRIER_WIDTH = 50;    // The default width of the barrier in game units
+
+   static Vector<Point> mRenderLineSegments;       // The clipped line segments representing this barrier
+   Vector<Point> mBotZoneBufferLineSegments;       // The line segments representing a buffered barrier
+
+   void render(S32 layerIndex);                                           // Renders barrier fill barrier-by-barrier
+   static void renderEdges(S32 layerIndex, const Color &outlineColor);    // Renders all edges in one pass
+
+   // Returns a sorting key for the object.  Barriers should be drawn first so as to appear behind other objects.
+   S32 getRenderSortValue();
+
+   // Returns the collision polygon of this barrier, which is the boundary extruded from the start,end line segment
+   const Vector<Point> *getCollisionPoly() const;
+
+   // Collide always returns true for Barrier objects
+   bool collide(BfObject *otherObject);
+
+
+   const Vector<Point> *getBufferForBotZone();
+
+   // Clips the current set of render lines against the polygon passed as polyPoints, modifies lineSegmentPoints
+   static void clipRenderLinesToPoly(const Vector<DatabaseObject *> &barrierList, Vector<Point> &lineSegmentPoints);
+
+   // Combine multiple barriers into a single complex polygon
+   static bool unionBarriers(const Vector<DatabaseObject *> &barriers, Vector<Vector<Point> > &solution);
+
+
+
+   static void prepareRenderingGeometry(Game *game);
+
+   TNL_DECLARE_CLASS(Barrier);
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class WallItem;
+class PolyWall;
+
+// This is just a small container for the bits of a wall that we transfer from the client to the server
+// I feel as if this should be a parent class for both WallItem and PolyWall, but I can't quite seem to
+// get it to work...
+
+struct WallRec
+{
+   Vector<F32> verts;
+   F32 width;
+   bool solid;
+
+public:
+   WallRec(F32 width, bool solid, const Vector<F32> &verts);   // Constructor
+   WallRec(const WallItem *wallItem);                          // Constructor
+   WallRec(const PolyWall *polyWall);                          // Constructor
+
+   void constructWalls(Game *theGame) const;
+};
+ 
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class WallItem : public CentroidObject
+{
+   typedef CentroidObject Parent;
+
+private:
+   S32 mWidth;
+   bool mAlreadyAdded;
+   void checkIfHasBeenAddedToTheGame(lua_State *L);
+
+public:
+   WallItem(lua_State *L = NULL);   // Combined Lua/C++ constructor
+   ~WallItem();                     // Destructor
+   WallItem *clone() const;
+
+   bool processArguments(S32 argc, const char **argv, Game *game);
+   string toLevelCode(F32 gridSize) const;
+
+   Vector<Point> extendedEndPoints;
+   virtual Rect calcExtents();
+
+   virtual void onGeomChanged();
+   virtual void onItemDragging();
+   virtual void onAddedToGame(Game *game);
+
+   void processEndPoints();  
+
+   void changeWidth(S32 amt);
+
+   void render();
+   void renderEditor(F32 currentScale, bool snappingToWallCornersEnabled);
+
+   // Some properties about the item that will be needed in the editor
+   const char *getEditorHelpString();
+   const char *getPrettyNamePlural();
+   const char *getOnDockName();
+   const char *getOnScreenName();          // Vertices should not be labeled
+   string getAttributeString();
+   const char *getInstructionMsg();
+
+   bool hasTeam();
+   bool canBeHostile();
+   bool canBeNeutral();
+   F32 getEditorRadius(F32 currentScale);  // Basically, the size of our hit target for vertices
+
+   const Color *getEditorRenderColor();    // Unselected wall spine color
+
+   void scale(const Point &center, F32 scale);
+
+   void addToGame(Game *game, GridDatabase *database);
+
+   S32 getWidth() const;
+   void setWidth(S32 width);
+
+   void setSelected(bool selected);
+
+   static const S32 VERTEX_SIZE = 5;
+
+   ///// Lua interface
+   LUAW_DECLARE_CLASS_CUSTOM_CONSTRUCTOR(WallItem);
+
+	static const char *luaClassName;
+	static const luaL_reg luaMethods[];
+   static const LuaFunctionProfile functionArgs[];
+
+   // Get/set wall's thickness
+   S32 getWidth(lua_State *L);
+   S32 setWidth(lua_State *L);
+
+   // Override standard methods basically to add checks to keep us from modifying a wall already in the game
+   S32 setLoc(lua_State *L);
+   S32 setGeom(lua_State *L);
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class PolyWall : public PolygonObject
+{
+   typedef PolygonObject Parent;
+
+private:
+   bool mAlreadyAdded;
+   void checkIfHasBeenAddedToTheGame(lua_State *L);
+
+public:
+   PolyWall(lua_State *L = NULL);      // Combined Lua/C++ constructor
+   ~PolyWall();                        // Destructor
+
+   PolyWall *clone() const;
+
+   bool processArguments(S32 argc, const char **argv, Game *game);
+
+   void renderDock();
+
+   S32 getRenderSortValue();
+
+   void setSelected(bool selected);
+
+   virtual void onGeomChanged();
+   virtual void onItemDragging();
+   virtual void onAddedToGame(Game *game);
+
+
+   void addToGame(Game *game, GridDatabase *database);
+
+
+   /////
+   // Editor methods
+   const char *getEditorHelpString();
+   const char *getPrettyNamePlural();
+   const char *getOnDockName();
+   const char *getOnScreenName();
+   string toLevelCode(F32 gridSize) const;
+
+   F32 getEditorRadius(F32 currentScale);
+
+   TNL_DECLARE_CLASS(PolyWall);
+
+
+   ///// Lua interface
+   LUAW_DECLARE_CLASS_CUSTOM_CONSTRUCTOR(PolyWall);
+
+	static const char *luaClassName;
+	static const luaL_reg luaMethods[];
+   static const LuaFunctionProfile functionArgs[];
+
+   // Override standard methods basically to add checks to keep us from modifying a wall already in the game
+   S32 setLoc(lua_State *L);
+   S32 setGeom(lua_State *L);
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class WallSegment : public DatabaseObject
+{
+private:
+   S32 mOwner;
+   bool mSelected;
+
+  void init(GridDatabase *database, S32 owner);
+  bool invalid;              // A flag for marking segments in need of processing
+
+   Vector<Point> mEdges;    
+   Vector<Point> mCorners;
+   Vector<Point> mTriangulatedFillPoints;
+
+public:
+   WallSegment(GridDatabase *gridDatabase, const Point &start, const Point &end, F32 width, S32 owner = -1);    // Normal wall segment
+   WallSegment(GridDatabase *gridDatabase, const Vector<Point> &points, S32 owner = -1);                        // PolyWall 
+   virtual ~WallSegment();
+
+   S32 getOwner();
+   void invalidate();
+
+   bool isSelected();
+   void setSelected(bool selected);
+
+   void resetEdges();         // Compute basic edges from corner points
+   void computeBoundingBox(); // Computes bounding box based on the corners, updates database
+   
+   void renderFill(const Point &offset);
+
+   const Vector<Point> *getCorners();
+   const Vector<Point> *getEdges();
+   const Vector<Point> *getTriangulatedFillPoints();
+
+   ////////////////////
+   // DatabaseObject methods
+
+   // Note that the poly returned here is different than what you might expect -- it is composed of the edges,
+   // not the corners, and is thus in A-B, C-D, E-F format rather than the more typical A-B-C-D format returned
+   // by getCollisionPoly() elsewhere in the game.  Therefore, it needs to be handled differently.
+   const Vector<Point> *getCollisionPoly() const;
+   bool getCollisionCircle(U32 stateIndex, Point &point, float &radius) const;
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Width of line representing centerline of barriers
+#define WALL_SPINE_WIDTH gLineWidth3
+
+
+class WallEdge : public DatabaseObject
+{
+private:
+   Point mStart, mEnd;
+   Vector<Point> mPoints;
+
+public:
+   WallEdge(const Point &start, const Point &end);
+   virtual ~WallEdge();
+
+   Point *getStart();
+   Point *getEnd();
+
+   // Note that the poly returned here is different than what you might expect -- it is composed of the edges,
+   // not the corners, and is thus in A-B, C-D, E-F format rather than the more typical A-B-C-D format returned
+   // by getCollisionPoly() elsewhere in the game.  Therefore, it needs to be handled differently.
+   const Vector<Point> *getCollisionPoly() const;
+   bool getCollisionCircle(U32 stateIndex, Point &point, float &radius) const;
+};
+
+
+};
+
+#endif
+
---
+++ zap/teamInfo.cpp
@@ -0,0 +1,484 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "teamInfo.h"
+#include "playerInfo.h"
+#include "gameType.h"
+#include "game.h"
+#include "stringUtils.h"
+#include "ClientInfo.h"
+#include "ServerGame.h"
+#include "robot.h"
+
+#ifdef ZAP_DEDICATED
+#   include "Colors.h"
+#endif
+
+namespace Zap
+{
+
+//U32 Team::mNextId = 0; // ???  when does it go back to zero?
+
+// Constructor
+AbstractTeam::AbstractTeam()
+{
+   // Do nothing
+}
+
+
+// Destructor
+AbstractTeam::~AbstractTeam()
+{
+   // Do nothing
+}
+
+
+void AbstractTeam::setColor(F32 r, F32 g, F32 b)
+{
+   mColor.set(r,g,b);
+}
+
+
+void AbstractTeam::setColor(const Color &color)
+{
+   mColor.set(color);
+}
+
+
+const Color *AbstractTeam::getColor() const
+{
+   return &mColor;
+}
+
+
+// Read team from level file params
+bool AbstractTeam::processArguments(S32 argc, const char **argv)
+{
+   if(argc < 5)               // Not enough arguments!
+      return false;
+
+   setName(argv[1]);
+   mColor.read(argv + 2);
+
+   return true;
+}
+
+
+string AbstractTeam::toLevelCode()
+{
+   return string("Team ") + writeLevelString(getName().getString()) + " " + mColor.toRGBString();
+}
+
+
+void AbstractTeam::alterRed(F32 amt) 
+{
+   mColor.r += amt;
+
+   if(mColor.r < 0)
+      mColor.r = 0;
+   else if(mColor.r > 1)
+      mColor.r = 1;
+}
+
+
+void AbstractTeam::alterGreen(F32 amt) 
+{
+   mColor.g += amt;
+
+   if(mColor.g < 0)
+      mColor.g = 0;
+   else if(mColor.g > 1)
+      mColor.g = 1;
+}
+
+
+void AbstractTeam::alterBlue(F32 amt) 
+{
+   mColor.b += amt;
+
+   if(mColor.b < 0)
+      mColor.b = 0;
+   else if(mColor.b > 1)
+      mColor.b = 1;
+}
+
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+// Constructor
+Team::Team()
+{
+   mPlayerCount = 0;
+   mBotCount = 0;
+   mScore = 0;
+   mRating = 0;
+}
+
+
+// Destructor
+Team::~Team()
+{
+   // Do nothing
+}
+
+
+void Team::clearStats()
+{
+   mPlayerCount = 0;
+   mBotCount = 0;
+   mRating = 0;
+}
+
+
+void Team::setName(const char *name)
+{
+   mName.set(name);
+}
+
+
+void Team::setName(StringTableEntry name)
+{
+   mName = name;
+}
+
+
+StringTableEntry Team::getName()
+{
+   return mName;
+}
+
+
+S32 Team::getScore()
+{
+   return mScore;
+}
+
+
+void Team::setScore(S32 score)
+{
+   mScore = score;
+}
+
+
+void Team::addScore(S32 score)
+{
+   mScore += score;
+}
+
+
+F32 Team::getRating()
+{
+   return mRating;
+}
+
+
+void Team::addRating(F32 rating)
+{
+   mRating += rating;
+}
+
+
+S32 Team::getPlayerCount()
+{
+   return mPlayerCount;
+}
+
+
+S32 Team::getBotCount()
+{
+   return mBotCount;
+}
+
+
+S32 Team::getPlayerBotCount()
+{
+   return mPlayerCount + mBotCount;
+}
+
+
+void Team::incrementPlayerCount()
+{
+   mPlayerCount++;
+}
+
+
+void Team::incrementBotCount()
+{
+   mBotCount++;
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Constructor
+EditorTeam::EditorTeam()
+{
+   mNameEditor.mMaxLen = MAX_TEAM_NAME_LENGTH;
+}
+
+
+// Constructor II
+EditorTeam::EditorTeam(const TeamPreset &preset)
+{
+   mNameEditor.mMaxLen = MAX_TEAM_NAME_LENGTH;
+   mNameEditor.setString(preset.name);
+   mColor = Color(preset.r, preset.g, preset.b);
+}
+
+
+// Destructor
+EditorTeam::~EditorTeam()
+{
+   // Do nothing
+}
+
+
+LineEditor *EditorTeam::getLineEditor()
+{
+   return &mNameEditor;
+}
+
+
+void EditorTeam::setName(const char *name)
+{
+   mNameEditor.setString(name);
+}
+
+
+StringTableEntry EditorTeam::getName()
+{
+   return StringTableEntry(mNameEditor.c_str());
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+const char LuaTeamInfo::className[] = "TeamInfo";      // Class name as it appears to Lua scripts
+
+// Define the methods we will expose to Lua
+Lunar<LuaTeamInfo>::RegType LuaTeamInfo::methods[] =
+{
+   method(LuaTeamInfo, getName),
+   method(LuaTeamInfo, getIndex),
+   method(LuaTeamInfo, getPlayerCount),
+   method(LuaTeamInfo, getScore),
+   method(LuaTeamInfo, getPlayers),
+
+   {0,0}    // End method list 
+};
+
+
+// Lua constructor
+LuaTeamInfo::LuaTeamInfo(lua_State *L)
+{
+   static const char *methodName = "TeamInfo constructor";
+   checkArgCount(L, 1, methodName);
+
+   // Lua thinks first team has index 1... we know better, but we need to play along...
+   U32 teamIndx = (U32) getInt(L, 1, methodName, 1, gServerGame->getTeamCount()) - 1;
+
+   mTeam = (Team *)gServerGame->getTeam(teamIndx);
+   mTeamIndex = teamIndx;
+}
+
+
+// C++ constructor
+LuaTeamInfo::LuaTeamInfo(Team *team)
+{
+   mTeam = team;
+
+   const char *teamName = team->getName().getString();
+
+   Vector<boost::shared_ptr<Team> > teams = gServerGame->getTeamCount();
+
+   for(S32 i = 0; i < gServerGame->getTeamCount(); i++)
+      if(!strcmp(gServerGame->getTeam(i)->getName().getString(), teamName))
+      {
+         mTeamIndex = i;
+         break;
+      }
+}
+
+// Destructor
+LuaTeamInfo::~LuaTeamInfo()
+{
+   logprintf(LogConsumer::LogLuaObjectLifecycle, "deleted LuaTeamInfo (%p)\n", this);     // Never gets run...
+}
+
+
+// We'll add 1 to the index to allow the first team in Lua to have index of 1, and the first team in C++ to have an index of 0
+S32 LuaTeamInfo::getIndex(lua_State *L) { return returnInt(L, mTeamIndex + 1); }                   // getTeamIndex() ==> return team's index (returns int)
+S32 LuaTeamInfo::getName(lua_State *L)  { return returnString(L, mTeam->getName().getString()); }  // getTeamName() ==> return team name (returns string)
+S32 LuaTeamInfo::getScore(lua_State *L) { return returnInt(L, mTeam->getScore()); }                // getScore() ==> return team score (returns int)
+
+
+S32 LuaTeamInfo::getPlayerCount(lua_State *L)         // number getPlayerCount() ==> return player count
+{
+   gServerGame->countTeamPlayers();    // Make sure player counts are up-to-date
+   return returnInt(L, mTeam->getPlayerBotCount());
+}
+
+
+// Return a table listing all players on this team
+S32 LuaTeamInfo::getPlayers(lua_State *L)
+{
+   ServerGame *game = gServerGame;
+
+   TNLAssert(game->getPlayerCount() == game->getClientCount(), "Mismatched player counts!");
+
+   S32 pushed = 0;
+
+   lua_newtable(L);    // Create a table, with no slots pre-allocated for our data
+
+   for(S32 i = 0; i < game->getClientCount(); i++)
+   {
+      ClientInfo *clientInfo = game->getClientInfo(i);
+
+      if(clientInfo->getTeamIndex() == mTeamIndex)
+      {
+         clientInfo->getPlayerInfo()->push(L);
+         pushed++;      // Increment pushed before using it because Lua uses 1-based arrays
+         lua_rawseti(L, 1, pushed);
+      }
+   }
+
+   for(S32 i = 0; i < game->getRobotCount(); i ++)
+   {
+      if(game->getBot(i)->getTeam() == mTeamIndex)
+      {
+         game->getBot(i)->getPlayerInfo()->push(L);
+         pushed++;      // Increment pushed before using it because Lua uses 1-based arrays
+         lua_rawseti(L, 1, pushed);
+      }
+   }
+
+   return 1;
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+// Destructor
+TeamManager::~TeamManager()
+{
+   clearTeams();
+}
+
+
+S32 TeamManager::getTeamCount()
+{
+   return mTeams.size();
+}
+
+
+extern Color gNeutralTeamColor;
+extern Color gHostileTeamColor;
+
+const Color *TeamManager::getTeamColor(S32 index) const
+{
+   if(index == TEAM_NEUTRAL)
+      return &gNeutralTeamColor;
+   else if(index == TEAM_HOSTILE)
+      return &gHostileTeamColor;
+   else if((U32)index < (U32)mTeams.size())     // Using U32 lets us handle goofball negative team numbers without explicitly checking for them
+      return mTeams[index]->getColor();
+   else
+      return &Colors::magenta;                  // Use a rare color to let user know an object has an out of range team number
+}
+
+
+AbstractTeam *TeamManager::getTeam(S32 teamIndex)
+{
+   return mTeams[teamIndex];
+}
+
+
+void TeamManager::removeTeam(S32 teamIndex)
+{
+   mTeams.deleteAndErase(teamIndex);
+   mTeamHasFlagList.resize(mTeams.size());
+}
+
+
+void TeamManager::addTeam(AbstractTeam *team)
+{
+   mTeams.push_back(team);
+   mTeamHasFlagList.resize(mTeams.size());
+   mTeamHasFlagList[mTeamHasFlagList.size() - 1] = false;
+}
+
+
+void TeamManager::addTeam(AbstractTeam *team, S32 index)
+{
+   mTeams.insert(index);
+   mTeams[index] = team;
+   mTeamHasFlagList.resize(mTeams.size());
+   mTeamHasFlagList[index] = false;
+}
+
+
+bool TeamManager::getTeamHasFlag(S32 teamIndex) const
+{
+   return mTeamHasFlagList[teamIndex] != 0;
+}
+
+
+void TeamManager::setTeamHasFlag(S32 teamIndex, bool hasFlag)
+{
+   mTeamHasFlagList[teamIndex] = hasFlag ? 1 : 0;
+}
+
+
+void TeamManager::clearTeamHasFlagList()
+{
+   for(S32 i = 0; i < mTeamHasFlagList.size(); i++)
+      mTeamHasFlagList[i] = 0;
+}
+
+
+void TeamManager::replaceTeam(AbstractTeam *team, S32 index)
+{
+   delete mTeams[index];  // delete old team that is no longer used
+   mTeams[index] = team;
+}
+
+
+void TeamManager::clearTeams()
+{
+   mTeams.deleteAndClear();
+   mTeamHasFlagList.clear();
+}
+
+
+};
+
+
