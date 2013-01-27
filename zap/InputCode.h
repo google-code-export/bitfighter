e7b23673ea59dbc4b1919f6a21742195268fa4fd
buckyballreaction <buckyballreaction@gmail.com>
2013-01-26 18:26:59 -0700

    Fix no kickback when blowing up own spybugs

---
+++ zap/InputCode.h
@@ -0,0 +1,212 @@
+//---------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _INPUTCODE_H_
+#define _INPUTCODE_H_
+
+#include "input.h"            // For InputMode enum
+#include "tnlVector.h"
+#include <string>
+
+#ifndef ZAP_DEDICATED
+#include "Joystick.h"
+#endif
+
+using namespace std;
+
+namespace Zap
+{
+
+#include "InputCodeEnum.h"    // Include inside Zap namespace
+
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+struct BindingSet;
+
+class InputCodeManager 
+{
+private:
+   bool mBindingsHaveKeypadEntry;
+   InputMode mInputMode;             // Joystick or Keyboard
+
+   BindingSet *mCurrentBindingSet;     
+   TNL::Vector<BindingSet> mBindingSets;
+
+public:
+   enum JoystickJoysticks {
+      JOYSTICK_DPAD,
+      JOYSTICK_STICK_1,
+      JOYSTICK_STICK_2
+   };
+
+
+   enum BindingName {
+      BINDING_SELWEAP1,    // Name def depends on these being in this order from here...
+      BINDING_SELWEAP2,
+      BINDING_SELWEAP3,
+      BINDING_ADVWEAP,
+      BINDING_ADVWEAP2,
+      BINDING_PREVWEAP,
+      BINDING_CMDRMAP,
+      BINDING_TEAMCHAT,
+      BINDING_GLOBCHAT,
+      BINDING_QUICKCHAT,
+      BINDING_CMDCHAT,
+      BINDING_LOADOUT,
+      BINDING_MOD1,
+      BINDING_MOD2,
+      BINDING_FIRE,
+      BINDING_DROPITEM,
+      BINDING_TOGVOICE,
+      BINDING_UP,
+      BINDING_DOWN,
+      BINDING_LEFT,
+      BINDING_RIGHT,
+      BINDING_SCRBRD,
+      BINDING_HELP,
+      BINDING_OUTGAMECHAT,
+      BINDING_MISSION,
+      BINDING_FPS,
+      BINDING_DIAG,        // ...to here
+      BINDING_DEFINEABLE_KEY_COUNT,
+      
+      BINDING_NONE,
+      BINDING_DUMMY_MOVE_SHIP_KEYS_UD,
+      BINDING_DUMMY_MOVE_SHIP_KEYS_LR,
+      BINDING_DUMMY_MOVE_SHIP_KEYS_MOUSE,
+      BINDING_DUMMY_STICK_LEFT,
+      BINDING_DUMMY_STICK_RIGHT,
+      BINDING_DUMMY_MSG_MODE,
+      BINDING_DUMMY_SS_MODE
+   };
+
+   InputCodeManager();     // Constructor
+
+   static const char *inputCodeToString(InputCode inputCode);
+   static InputCode stringToInputCode(const char *inputName);
+
+   static void setState(InputCode inputCode, bool state);   // Set key state (t=down, f=up)
+   static bool getState(InputCode inputCode);               // Return current key state (t=down, f=up)
+   static void resetStates();                               // Initialize key states
+   static void dumpInputCodeStates();                       // Log key states for testing
+   static void initializeKeyNames();
+
+   //static S32 getBindingCount();
+   static string getBindingName(BindingName binding);
+   
+   InputCode getKeyBoundToBindingCodeName(string name);
+   
+
+   // Some converters
+   InputCode filterInputCode(InputCode inputCode);    // Calls filters below
+
+#ifndef ZAP_DEDICATED
+   static InputCode joyHatToInputCode(int hatDirectionMask);
+   static InputCode joystickButtonToInputCode(Joystick::Button button);
+#endif
+
+   static InputCode convertJoystickToKeyboard(InputCode inputCode);
+private:
+   static InputCode convertNumPadToNum(InputCode inputCode);
+   bool checkIfBindingsHaveKeypad();
+
+public:
+   static string getCurrentInputString(InputCode inputCode);
+
+   static bool checkModifier(InputCode mod1);            
+   static bool checkModifier(InputCode mod1, InputCode mod2);            
+   static bool checkModifier(InputCode mod1, InputCode mod2, InputCode mod3);            
+
+   void setInputMode(InputMode inputMode);
+   InputMode getInputMode();
+   string getInputModeString();     // Returns display-friendly mode designator like "Keyboard" or "Joystick 1"
+
+   #ifndef ZAP_DEDICATED
+      static InputCode sdlKeyToInputCode(int key);             // Convert SDL keys to InputCode
+      static int inputCodeToSDLKey(InputCode inputCode);       // Take a InputCode and return the SDL equivalent
+   #endif
+
+   static char keyToAscii(int unicode, InputCode inputCode);   // Return a printable ascii char, if possible
+   static bool isControllerButton(InputCode inputCode);        // Does inputCode represent a controller button?
+   static bool isKeypadKey(InputCode inputCode);               // Is inputCode on the numeric keypad?
+   static bool isMouseAction(InputCode inputCode);             // Is inputCode related to the mouse?
+   static bool isKeyboardKey(InputCode inputCode);             // Is inputCode a key on the keyboard?
+   static bool isModifier(InputCode inputCode);                // Is inputCode a modifier key?
+
+   InputCode getBinding(BindingName binding);
+   InputCode getBinding(BindingName bindingName, InputMode inputMode);
+   void setBinding(BindingName bindingName, InputCode key);
+   void setBinding(BindingName bindingName, InputMode inputMode, InputCode key);
+};
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+struct BindingSet
+{
+   BindingSet();     // Constructor
+   bool hasKeypad();
+
+   InputCode getBinding(InputCodeManager::BindingName bindingName);
+   void setBinding(InputCodeManager::BindingName bindingName, InputCode key);
+
+   InputCode inputSELWEAP1;
+   InputCode inputSELWEAP2;
+   InputCode inputSELWEAP3;
+   InputCode inputADVWEAP;
+   InputCode inputADVWEAP2;
+   InputCode inputPREVWEAP;
+   InputCode inputCMDRMAP;
+   InputCode inputTEAMCHAT;
+   InputCode inputGLOBCHAT;
+   InputCode inputQUICKCHAT;
+   InputCode inputCMDCHAT;
+   InputCode inputLOADOUT;
+   InputCode inputMOD1;
+   InputCode inputMOD2;
+   InputCode inputFIRE;
+   InputCode inputDROPITEM;
+   InputCode inputTOGVOICE;
+   InputCode inputUP;
+   InputCode inputDOWN;
+   InputCode inputLEFT;
+   InputCode inputRIGHT;
+   InputCode inputSCRBRD;
+   InputCode keyHELP;
+   InputCode keyOUTGAMECHAT;
+   InputCode keyMISSION;
+   InputCode keyFPS;
+   InputCode keyDIAG;
+};
+
+
+
+};     // namespace Zap
+
+#endif
+
---
+++ zap/goalZone.cpp
@@ -0,0 +1,317 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "goalZone.h"
+#include "gameType.h"
+#include "gameObjectRender.h"
+#include "game.h"
+#include "stringUtils.h"
+
+
+namespace Zap
+{
+
+TNL_IMPLEMENT_NETOBJECT(GoalZone);
+
+/**
+*  @luafunc GoalZone::GoalZone()
+*  @luafunc GoalZone::GoalZone(polyGeom team)
+*  @brief %GoalZone constructor.
+*  @descr Default team is Neutral.
+*/
+// Combined Lua / C++ constructor
+GoalZone::GoalZone(lua_State *L)
+{
+   mNetFlags.set(Ghostable);
+   mObjectTypeNumber = GoalZoneTypeNumber;
+
+   mFlashCount = 0;
+
+   mHasFlag = false;
+   mScore = 1;             // For now...  may someday let GoalZones have different scoring values
+   mCapturer = NULL;
+   setTeam(TEAM_NEUTRAL); 
+
+   if(L)    // Coming from Lua -- grab params from L
+   {
+      static LuaFunctionArgList constructorArgList = { {{ END }, { POLY, TEAM_INDX, END }}, 2 };
+      S32 profile = checkArgList(L, constructorArgList, "GoalZone", "constructor");
+
+      if(profile == 1)        // Geom, Team
+      {
+         setGeom(L,  1);
+         setTeam(L, -1);
+      }
+   }
+
+   LUAW_CONSTRUCTOR_INITIALIZATIONS;
+}
+ 
+
+GoalZone::~GoalZone()
+{
+   LUAW_DESTRUCTOR_CLEANUP;
+}
+
+
+GoalZone *GoalZone::clone() const
+{
+   GoalZone *goalZone = new GoalZone(*this);
+   return goalZone;
+}
+
+
+void GoalZone::render()
+{
+   GameType *gt = getGame()->getGameType();
+   F32 glow = gt->mZoneGlowTimer.getFraction();
+
+   // Check if to make sure that the zone matches the glow team if we're glowing
+   if(gt->mGlowingZoneTeam >= 0 && gt->mGlowingZoneTeam != getTeam())
+      glow = 0;
+
+   bool useOldStyle = getGame()->getSettings()->getIniSettings()->oldGoalFlash;
+   renderGoalZone(*getColor(), getOutline(), getFill(), getCentroid(), getLabelAngle(), isFlashing(), glow, mScore, 
+                  mFlashCount ? F32(mFlashTimer.getCurrent()) / FlashDelay : 0, useOldStyle);
+}
+
+
+void GoalZone::renderEditor(F32 currentScale, bool snappingToWallCornersEnabled)
+{
+   bool useOldStyle = getGame()->getSettings()->getIniSettings()->oldGoalFlash;
+
+   renderGoalZone(*getColor(), getOutline(), getFill(), getCentroid(), getLabelAngle(), false, 0, 0, 0, useOldStyle);
+   PolygonObject::renderEditor(currentScale, snappingToWallCornersEnabled);
+}
+
+
+void GoalZone::renderDock()
+{
+  renderGoalZone(*getColor(), getOutline(), getFill());
+}
+
+
+bool GoalZone::processArguments(S32 argc2, const char **argv2, Game *game)
+{
+   // Need to handle or ignore arguments that starts with letters,
+   // so a possible future version can add parameters without compatibility problem.
+   S32 argc = 0;
+   const char *argv[65]; // 32 * 2 + 1 = 65
+   for(S32 i = 0; i < argc2; i++)  // the idea here is to allow optional R3.5 for rotate at speed of 3.5
+   {
+      char c = argv2[i][0];
+
+      if((c < 'a' || c > 'z') && (c < 'A' || c > 'Z'))
+      {
+         if(argc < 65)
+         {  argv[argc] = argv2[i];
+            argc++;
+         }
+      }
+   }
+
+   if(argc < 7)
+      return false;
+
+   setTeam(atoi(argv[0]));     // Team is first arg
+   return Parent::processArguments(argc - 1, argv + 1, game);
+}
+
+
+const char *GoalZone::getOnScreenName()     { return "Goal";       }
+const char *GoalZone::getOnDockName()       { return "Goal";       }
+const char *GoalZone::getPrettyNamePlural() { return "Goal Zones"; }
+const char *GoalZone::getEditorHelpString() { return "Target area used in a variety of games."; }
+
+bool GoalZone::hasTeam()      { return true; }
+bool GoalZone::canBeHostile() { return true; }
+bool GoalZone::canBeNeutral() { return true; }
+
+
+string GoalZone::toLevelCode(F32 gridSize) const
+{
+   return string(appendId(getClassName())) + " " + itos(getTeam()) + " " + geomToLevelCode(gridSize);
+}
+
+
+bool GoalZone::didRecentlyChangeTeam()
+{
+   return mFlashCount != 0;
+}
+
+
+void GoalZone::setTeam(S32 team)
+{
+   Parent::setTeam(team);
+   setMaskBits(TeamMask);
+}
+
+
+// This just here to provide a signature at this level
+void GoalZone::setTeam(lua_State *L, S32 index)
+{
+   Parent::setTeam(L, index);
+}
+
+
+ClientInfo *GoalZone::getCapturer()
+{
+   return mCapturer;
+}
+
+
+void GoalZone::setCapturer(ClientInfo *clientInfo)
+{
+   mCapturer = clientInfo;
+}
+
+
+void GoalZone::onAddedToGame(Game *theGame)
+{
+   Parent::onAddedToGame(theGame);
+
+   if(!isGhost())
+      setScopeAlways();
+}
+
+
+const Vector<Point> *GoalZone::getCollisionPoly() const
+{
+   return getOutline();
+}
+
+
+bool GoalZone::collide(BfObject *hitObject)
+{
+   if( !isGhost() && (isShipType(hitObject->getObjectTypeNumber())) )
+   {
+      Ship *s = static_cast<Ship *>(hitObject); 
+      getGame()->getGameType()->shipTouchZone(s, this);
+   }
+
+   return false;
+}
+
+
+bool GoalZone::isFlashing()
+{
+   return mFlashCount & 1;
+}
+
+
+void GoalZone::setFlashCount(S32 i)
+{
+   mFlashCount = i;
+}
+
+
+S32 GoalZone::getScore()
+{
+   return mScore;
+}
+
+
+void GoalZone::setHasFlag(bool hasFlag)
+{
+   mHasFlag = hasFlag;
+}
+
+
+U32 GoalZone::packUpdate(GhostConnection *connection, U32 updateMask, BitStream *stream)
+{
+   Parent::packUpdate(connection, updateMask, stream);      // Handles Geomand Team
+   
+   if(stream->writeFlag(updateMask & InitialMask))
+      stream->write(mScore);
+
+   return 0;
+}
+
+
+void GoalZone::unpackUpdate(GhostConnection *connection, BitStream *stream)
+{
+   S32 oldTeam = getTeam();
+
+   Parent::unpackUpdate(connection, stream);
+
+   if(stream->readFlag()) 
+      stream->read(&mScore);
+
+   // Some special handling if we've changed teams
+   if(getTeam() != oldTeam && !isInitialUpdate() && getTeam() != TEAM_NEUTRAL)   // Team will be neutral on touchdown, and we don't want to flash then!
+   {
+      mFlashTimer.reset(FlashDelay);
+      mFlashCount = FlashCount;
+      linkToIdleList(&getGame()->idlingObjects); // Add to idle list to make GoalZone blink
+   }
+}
+
+
+void GoalZone::idle(BfObject::IdleCallPath path)
+{
+   if(path != BfObject::ClientIdleMainRemote || mFlashCount == 0)
+      return;
+
+   if(mFlashTimer.update(mCurrentMove.time))
+   {
+      mFlashTimer.reset(FlashDelay);
+      mFlashCount--;
+      if(mFlashCount == 0)
+         unlinkFromIdleList();  // Have nothing more to idle
+   }
+}
+
+
+/////
+// Lua interface
+/**
+  *  @luaclass GoalZone
+  *  @brief Place to deposit flags or get the ball to, depending on game type.
+  */
+//               Fn name       Param profiles  Profile count                           
+#define LUA_METHODS(CLASS, METHOD) \
+   METHOD(CLASS, hasFlag,     ARRAYDEF({{ END }}), 1 ) \
+
+GENERATE_LUA_METHODS_TABLE(GoalZone, LUA_METHODS);
+GENERATE_LUA_FUNARGS_TABLE(GoalZone, LUA_METHODS);
+
+#undef LUA_METHODS
+
+const char *GoalZone::luaClassName = "GoalZone";
+REGISTER_LUA_SUBCLASS(GoalZone, Zone);
+
+
+/**
+  *  @luafunc bool GoalZone::hasFlag()
+  *  @brief Does the zone have a flag?
+  *  @descr GoalZones can hold flags in some game types.  If the current game type does not feature
+  *         zones that hold flags (e.g. Soccer, ZoneControl), then the function will return false.
+  *  @return \e bool - True if the %GoalZone is currently holding a flag, false otherwise.
+  */
+S32 GoalZone::hasFlag(lua_State *L) { return returnBool(L, mHasFlag); }
+
+};
+
+
