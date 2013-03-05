7d98087865822f528ddd89312044524d27c7fb1c
watusimoto <watusimoto@bitfighter.org>
2013-03-05 20:22:54 +0100

    Whitespace and formatting

---
+++ zap/Geometry_Base.h
@@ -0,0 +1,117 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+
+#ifndef _GEOMETRY_BASE_H_
+#define _GEOMETRY_BASE_H_
+
+#include "Rect.h"
+
+namespace TNL
+{
+   class GhostConnection; // speeds up compiler, or use #include "tnlGhostConnection.h"
+   class BitStream;
+}
+
+namespace boost{ template<class T> class shared_ptr; }      // or use #include "boost/smart_ptr/shared_ptr.hpp"
+
+using namespace std;
+using namespace TNL;
+
+
+namespace Zap
+{
+
+enum GeomType {           
+   geomPoint,        // One point      
+   geomSimpleLine,   // Two points   
+   geomPolyLine,     // Many points
+   geomPolygon,      // Many points, closed loop
+   geomNone,         // Singularity   
+};
+
+
+// Geometry is essentially an interface class that all geometric objects implement
+class Geometry
+{
+public:
+   static const S32 MAX_POLY_POINTS = 32;       // Max number of points we can have in Walls, Nexuses, LoadoutZones, etc.
+
+   Geometry();           // Constructor
+   virtual ~Geometry();  // Destructor
+
+   virtual GeomType getGeomType() const;
+
+   virtual Point getVert(S32 index) const;
+   virtual void setVert(const Point &pos, S32 index);
+   virtual S32 getVertCount() const;
+   virtual S32 getMinVertCount() const;      // Minimum number of vertices geometry needs to be viable
+   virtual void clearVerts();
+   virtual bool addVert(const Point &point, bool ignoreMaxPointsLimit = false);
+   virtual bool addVertFront(Point vert);
+   virtual bool deleteVert(S32 vertIndex);
+   virtual bool insertVert(Point vertex, S32 vertIndex);
+   virtual void reverseWinding();
+
+   virtual bool anyVertsSelected();
+
+   virtual void selectVert(S32 vertIndex);
+   virtual void aselectVert(S32 vertIndex);   // Select another vertex (remember cmdline ArcInfo?)
+   virtual void unselectVert(S32 vertIndex);
+   virtual void unselectVerts();
+   virtual bool vertSelected(S32 vertIndex);
+
+   virtual const Vector<Point> *getOutline() const;
+   virtual const Vector<Point> *getFill() const;
+   virtual Point getCentroid() const;
+   virtual F32 getLabelAngle() const;
+                                                            
+   virtual void packGeom(GhostConnection *connection, BitStream *stream);
+   virtual void unpackGeom(GhostConnection *connection, BitStream *stream);
+   virtual void setGeom(const Vector<Point> &points);
+
+   virtual string geomToLevelCode(F32 gridSize) const;
+   virtual void readGeom(S32 argc, const char **argv, S32 firstCoord, F32 gridSize);
+
+   virtual void disableTriangulation();
+
+   Geometry *newCopy();
+
+   virtual Rect calcExtents();
+
+   virtual void onPointsChanged();
+
+   // These functions are declered in Geometry.cpp
+   void rotateAboutPoint(const Point &center, F32 angle);
+   void flip(F32 center, bool isHoriz);                   // Do a horizontal or vertical flip about line at center
+   void scale(const Point &center, F32 scale);
+   void moveTo(const Point &pos, S32 vertexIndexToBePositionedAtPos);
+   void offset(const Point &offset);
+};
+
+
+};
+
+#endif
---
+++ zap/engineerHelper.cpp
@@ -0,0 +1,301 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "engineerHelper.h"
+#include "EngineeredItem.h"      // For EngineerModuleDeployer
+#include "UIGame.h"
+#include "Color.h"                  // For Color def
+#include "Colors.h"
+#include "ClientGame.h"
+#include "JoystickRender.h"
+#include "config.h"
+#include "gameObjectRender.h"       // For drawSquare
+
+#include "ship.h"
+
+#include "OpenglUtils.h"
+
+namespace Zap
+{
+
+
+EngineerConstructionItemInfo engineerItemInfo[] = {
+      { EngineeredTurret,             KEY_1, BUTTON_1, true,  "Turret",          "", "Aim at a spot on a wall, and activate the module again." },
+      { EngineeredForceField,         KEY_2, BUTTON_2, true,  "Force Field",     "", "Aim at a spot on a wall, and activate the module again." },
+      { EngineeredTeleporterEntrance, KEY_3, BUTTON_3, true,  "Teleporter",      "", "Aim at a spot in open space, and activate the module again." },
+      { EngineeredTeleporterExit,     KEY_4, BUTTON_4, false, "Teleporter Exit", "", "Aim at a spot in open space, and activate the module again." },
+};
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Constructor
+EngineerHelper::EngineerHelper(ClientGame *clientGame) : Parent(clientGame)
+{
+   mSelectedItem = -1;
+   mEngineerCostructionItemInfos = Vector<EngineerConstructionItemInfo>(engineerItemInfo, ARRAYSIZE(engineerItemInfo));
+}
+
+
+// Destructor
+EngineerHelper::~EngineerHelper()
+{
+   // Do nothing
+}
+
+
+void EngineerHelper::setSelectedEngineeredObject(U32 objectType)
+{
+   for(S32 i = 0; i < mEngineerCostructionItemInfos.size(); i++)
+      if(mEngineerCostructionItemInfos[i].mObjectType == objectType)
+         mSelectedItem = i;
+}
+
+
+void EngineerHelper::onMenuShow()
+{
+   mSelectedItem = -1;
+}
+
+
+bool EngineerHelper::isEngineerHelper()
+{
+   return true;
+}
+
+
+static Point deployPosition, deployNormal;
+
+void EngineerHelper::render()
+{
+   S32 yPos = MENU_TOP;
+   const S32 fontSize = 15;
+   const Color engineerMenuHeaderColor (Colors::red);
+
+   if(isMenuBeingDisplayed())    // Haven't selected an item yet, so show the menu
+   {
+      const S32 xPos = UserInterface::horizMargin + 50;
+
+      drawMenuBorderLine(yPos, engineerMenuHeaderColor);
+
+      glColor(engineerMenuHeaderColor);
+      drawString(UserInterface::horizMargin, yPos, fontSize, "What do you want to Engineer?");
+      yPos += fontSize + 10;
+
+      GameSettings *settings = getGame()->getSettings();
+      InputMode inputMode = settings->getInputCodeManager()->getInputMode();
+
+      bool showKeys = settings->getIniSettings()->showKeyboardKeys || inputMode == InputModeKeyboard;
+
+      for(S32 i = 0; i < mEngineerCostructionItemInfos.size(); i++)
+      {
+         // Don't show an option that shouldn't be shown!
+         if(!mEngineerCostructionItemInfos[i].showOnMenu)
+            continue;
+
+         // Draw key controls for selecting the object to be created
+         U32 joystickIndex = Joystick::SelectedPresetIndex;
+
+         if(inputMode == InputModeJoystick)     // Only draw joystick buttons when in joystick mode
+            JoystickRender::renderControllerButton(F32(UserInterface::horizMargin + (showKeys ? 0 : 20)), (F32)yPos, 
+                                                   joystickIndex, mEngineerCostructionItemInfos[i].mButton, false);
+
+         if(showKeys)
+         {
+            glColor(Colors::white);     // Render key in white
+            JoystickRender::renderControllerButton((F32)UserInterface::horizMargin + 20, (F32)yPos, 
+                                                   joystickIndex, mEngineerCostructionItemInfos[i].mKey, false);
+         }
+
+         glColor(0.1, 1.0, 0.1);     
+         S32 x = drawStringAndGetWidth(xPos, yPos, fontSize, mEngineerCostructionItemInfos[i].mName); 
+
+         glColor(.2, .8, .8);    
+         drawString(xPos + x, yPos, fontSize, mEngineerCostructionItemInfos[i].mHelp);      // The help string, if there is one
+
+         yPos += fontSize + 7;
+      }
+
+      yPos += 2;
+
+      drawMenuBorderLine(yPos - fontSize - 2, engineerMenuHeaderColor);
+      yPos += 8;
+
+      drawMenuCancelText(yPos, engineerMenuHeaderColor, fontSize);
+   }
+   else     // Have selected a module, need to indicate where to deploy
+   {
+      S32 xPos = UserInterface::horizMargin;
+      glColor(Colors::green);
+      drawStringf(xPos, yPos, fontSize, "Placing %s.", mEngineerCostructionItemInfos[mSelectedItem].mName);
+      yPos += fontSize + 7;
+      drawString(xPos, yPos, fontSize, mEngineerCostructionItemInfos[mSelectedItem].mInstruction);
+   }
+}
+
+
+bool EngineerHelper::isMenuBeingDisplayed()
+{
+   return mSelectedItem == -1;
+}
+
+
+// Return true if key did something, false if key had no effect
+// Runs on client
+bool EngineerHelper::processInputCode(InputCode inputCode)
+{
+   if(Parent::processInputCode(inputCode))    // Check for cancel keys
+      return true;
+
+   InputCodeManager *inputCodeManager = getGame()->getSettings()->getInputCodeManager();
+   GameConnection *gc = getGame()->getConnectionToServer();
+
+   if(isMenuBeingDisplayed())    // Menu is being displayed, so interpret keystrokes as menu items
+   {
+      for(S32 i = 0; i < mEngineerCostructionItemInfos.size(); i++)
+      {
+         // Disallow selecting unselectable items
+         if(!mEngineerCostructionItemInfos[i].showOnMenu)
+            continue;
+
+         if(inputCode == mEngineerCostructionItemInfos[i].mKey || inputCode == mEngineerCostructionItemInfos[i].mButton)
+         {
+            mSelectedItem = i;
+            return true;
+         }
+      }
+
+      Ship *ship = dynamic_cast<Ship *>(gc->getControlObject());
+      TNLAssert(ship, "Will this ever be true?  If not, we can replace with static cast/assert. If this does assert, "
+                      "please document and remove this assert.");
+
+      if(!ship || (inputCode == inputCodeManager->getBinding(InputCodeManager::BINDING_MOD1) && ship->getModule(0) == ModuleEngineer) ||
+                  (inputCode == inputCodeManager->getBinding(InputCodeManager::BINDING_MOD2) && ship->getModule(1) == ModuleEngineer))
+      {
+         exitHelper();
+         return true;
+      }
+   }
+   else                       // Placing item
+   {
+      Ship *ship = dynamic_cast<Ship *>(gc->getControlObject());
+      if(ship && ((inputCode == inputCodeManager->getBinding(InputCodeManager::BINDING_MOD1) && ship->getModule(0) == ModuleEngineer) ||
+                  (inputCode == inputCodeManager->getBinding(InputCodeManager::BINDING_MOD2) && ship->getModule(1) == ModuleEngineer)))
+      {
+         EngineerModuleDeployer deployer;
+
+         // Check deployment status on client; will be checked again on server,
+         // but server will only handle likely valid placements
+         if(deployer.canCreateObjectAtLocation(getGame()->getGameObjDatabase(), ship, mEngineerCostructionItemInfos[mSelectedItem].mObjectType))
+         {
+            // Send command to server to deploy, and deduct energy
+            if(gc)
+            {
+               gc->c2sEngineerDeployObject(mEngineerCostructionItemInfos[mSelectedItem].mObjectType);
+               S32 energyCost = Game::getModuleInfo(ModuleEngineer)->getPrimaryPerUseCost();
+               ship->creditEnergy(-energyCost);    // Deduct energy from engineer
+            }
+         }
+         // If location is bad, show error message
+         else
+            getGame()->displayErrorMessage(deployer.getErrorMessage().c_str());
+
+         // Normally we'd exit the helper menu here, but we don't since teleport has two parts.
+         // We therefore let a server command dictate what we do (see GameConnection::s2cEngineerResponseEvent)
+
+         return true;
+      }
+   }
+
+   return false;
+}
+
+
+void EngineerHelper::exitHelper()
+{
+   if(mSelectedItem != -1 && mEngineerCostructionItemInfos[mSelectedItem].mObjectType == EngineeredTeleporterExit)
+   {
+      GameConnection *gameConnection = getGame()->getConnectionToServer();
+
+      if(gameConnection)
+         gameConnection->c2sEngineerInterrupted(EngineeredTeleporterExit);
+   }
+
+   Parent::exitHelper();
+}
+
+
+// Basically draws a red box where the ship is pointing
+void EngineerHelper::renderDeploymentMarker(Ship *ship)
+{
+   // Only render wall mounted items (not teleport)
+   if(mSelectedItem != -1 &&
+         EngineerModuleDeployer::findDeployPoint(ship, mEngineerCostructionItemInfos[mSelectedItem].mObjectType, deployPosition, deployNormal))
+   {
+      EngineerModuleDeployer deployer;
+      bool canDeploy = deployer.canCreateObjectAtLocation(getGame()->getGameObjDatabase(), ship, mEngineerCostructionItemInfos[mSelectedItem].mObjectType);
+
+      switch(mEngineerCostructionItemInfos[mSelectedItem].mObjectType)
+      {
+         case EngineeredTurret:
+         case EngineeredForceField:
+            if(canDeploy)
+               glColor(Colors::green);
+            else
+               glColor(Colors::red);
+
+            drawSquare(deployPosition, 5);
+            break;
+
+         case EngineeredTeleporterEntrance:
+         case EngineeredTeleporterExit:
+            if(canDeploy)
+               renderTeleporterOutline(deployPosition, 75.f, Colors::green);
+            else
+               renderTeleporterOutline(deployPosition, 75.f, Colors::red);
+            break;
+
+         default:
+            break;
+      }
+   }
+}
+
+
+const char *EngineerHelper::getCancelMessage()
+{
+   return "Engineered item not deployed";
+}
+
+
+// When a menu is not active, we'll allow players to enter chat
+bool EngineerHelper::isChatDisabled()
+{
+   return isMenuBeingDisplayed();
+}
+
+};
+
