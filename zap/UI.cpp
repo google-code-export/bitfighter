2fdfbe222a26a2292b02df4d7759721d1184ed1c
watusimoto <watusimoto@bitfighter.org>
2013-03-15 00:50:02 +0100

    Consolidate scissors code in new ScissorsManager class

---
+++ zap/UI.cpp
@@ -0,0 +1,489 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+#include "tnl.h"
+
+using namespace TNL;
+using namespace std;
+
+#include "UI.h"
+#include "move.h"
+#include "InputCode.h"
+#include "UIMenus.h"
+#include "UIDiagnostics.h"
+#include "UIChat.h"
+#include "input.h"               // For MaxJoystickButtons const
+#include "config.h"
+#include "ClientGame.h"
+#include "Console.h"             // For console rendering
+#include "Colors.h"
+#include "ScreenInfo.h"
+#include "Joystick.h"
+#include "masterConnection.h"    // For MasterServerConnection def
+#include "VideoSystem.h"
+#include "OpenglUtils.h"
+#include "MathUtils.h"           // For RADIANS_TO_DEGREES def
+
+#include <string>
+
+#include <math.h>
+
+
+using namespace std;
+namespace Zap
+{
+
+#ifdef TNL_OS_XBOX
+   S32 UserInterface::horizMargin = 50;
+   S32 UserInterface::vertMargin = 38;
+#else
+   S32 UserInterface::horizMargin = 15;
+   S32 UserInterface::vertMargin = 15;
+#endif
+
+extern S32 gLoadoutIndicatorHeight;
+
+S32 UserInterface::messageMargin = UserInterface::vertMargin + gLoadoutIndicatorHeight + 5;
+
+extern Vector<ClientGame *> gClientGames;
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Declare statics
+bool UserInterface::mDisableShipKeyboardInput = true;
+
+
+// Constructor
+UserInterface::UserInterface(ClientGame *clientGame)
+{
+   mClientGame = clientGame;
+   mTimeSinceLastInput = 0;
+
+   mInternalMenuID = InvalidUI;
+}
+
+
+UserInterface::~UserInterface()
+{
+   // Do nothing
+}
+
+
+ClientGame *UserInterface::getGame()
+{
+   return mClientGame;
+}
+
+
+UIManager *UserInterface::getUIManager() const 
+{ 
+   TNLAssert(mClientGame, "mGame is NULL!");
+   return mClientGame->getUIManager(); 
+}
+
+
+bool UserInterface::usesEditorScreenMode()
+{
+   return false;
+}
+
+
+void UserInterface::activate()
+{
+   onActivate(); 
+}
+
+
+void UserInterface::reactivate()
+{
+   onReactivate();
+}
+
+
+// Set interface's name.  This name is used internally only for debugging
+// and to identify interfaces when searching for matches.  Each interface
+// should have a unique id.
+
+void UserInterface::setMenuID(UIID menuID)
+{
+   mInternalMenuID = menuID;
+}
+
+
+// Retrieve interface's id
+UIID UserInterface::getMenuID() const
+{
+   return mInternalMenuID;
+}
+
+
+// Retrieve previous interface's id
+UIID UserInterface::getPrevMenuID() const
+{
+   if(getUIManager()->hasPrevUI())
+      return getUIManager()->getPrevUI()->mInternalMenuID;
+   else
+      return InvalidUI;
+}
+
+
+void UserInterface::onActivate()          { /* Do nothing */ }
+void UserInterface::onReactivate()        { /* Do nothing */ }
+void UserInterface::onDisplayModeChange() { /* Do nothing */ }
+
+
+void UserInterface::onDeactivate(bool nextUIUsesEditorScreenMode)
+{
+   if(nextUIUsesEditorScreenMode != usesEditorScreenMode())
+      VideoSystem::actualizeScreenMode(getGame()->getSettings(), true, nextUIUsesEditorScreenMode);
+}
+
+
+U32 UserInterface::getTimeSinceLastInput()
+{
+   return mTimeSinceLastInput;
+}
+
+
+void UserInterface::playBoop()
+{
+   SoundSystem::playSoundEffect(SFXUIBoop, 1);
+}
+
+
+// Render master connection state if we're not connected
+void UserInterface::renderMasterStatus()
+{
+   MasterServerConnection *conn = mClientGame->getConnectionToMaster();
+
+   if(conn && conn->getConnectionState() != NetConnection::Connected)
+   {
+      glColor(Colors::white);
+      drawStringf(10, 550, 15, "Master Server - %s", GameConnection::getConnectionStateString(conn->getConnectionState()));
+   }
+}
+
+
+void UserInterface::renderConsole()
+{
+#ifndef BF_NO_CONSOLE
+   // Temporarily disable scissors mode so we can use the full width of the screen
+   // to show our console text, black bars be damned!
+   bool scissorMode = glIsEnabled(GL_SCISSOR_TEST);
+
+   if(scissorMode) 
+      glDisable(GL_SCISSOR_TEST);
+
+   gConsole.render();
+
+   if(scissorMode) 
+      glEnable(GL_SCISSOR_TEST);
+#endif
+}
+
+
+extern ScreenInfo gScreenInfo;
+
+void UserInterface::renderMessageBox(const char *title, const char *instr, string message[], S32 msgLines, S32 vertOffset, S32 style)
+{
+   const S32 canvasWidth  = gScreenInfo.getGameCanvasWidth();
+   const S32 canvasHeight = gScreenInfo.getGameCanvasHeight();
+
+   S32 inset = 100;                    // Inset for left and right edges of box
+   const S32 titleSize = 30;           // Size of title
+   const S32 titleGap = titleSize / 3; // Spacing between title and first line of text
+
+   S32 textSize;
+
+   if(style == 1)
+      textSize = 18;            // Size of text and instructions
+   else if(style == 2)
+      textSize = 30;
+
+   const S32 textGap = textSize / 3;   // Spacing between text lines
+   const S32 instrGap = 15;            // Gap between last line of text and instruction line
+
+   S32 titleSpace = titleSize + titleGap;
+   S32 boxHeight = titleSpace + 2 * vertMargin + (msgLines + 1) * (textSize + textGap) + instrGap;
+
+   if(strcmp(instr, "") == 0)
+      boxHeight -= (instrGap + textSize);
+
+   if(strcmp(title, "") == 0)
+   {
+      boxHeight -= titleSpace;
+      titleSpace = 0;
+   }
+
+   S32 boxTop = (canvasHeight - boxHeight) / 2 + vertOffset;
+
+   S32 maxLen = 0;
+   for(S32 i = 0; i < msgLines; i++)
+   {
+      S32 len = getStringWidth(textSize, message[i].c_str()) + 20;     // 20 gives a little breathing room on the edges
+      if(len > maxLen)
+         maxLen = len;
+   }
+
+   if(canvasWidth - 2 * inset < maxLen)
+      inset = (canvasWidth - maxLen) / 2;
+
+
+   if(style == 1)       
+   {
+      // Solid red box with white border
+      glDisable(GL_BLEND);
+      drawFilledRect(inset, boxTop, canvasWidth - inset, boxTop + boxHeight, Colors::red30, Colors::white);
+      glEnable(GL_BLEND);
+      glColor(Colors::white);
+   }
+   else if(style == 2)
+   {
+      // Hollow blue box
+      drawHollowRect(inset, boxTop, canvasWidth - inset, boxTop + boxHeight, Colors::blue);
+      glColor(Colors::blue);
+   }
+
+   // Draw title, message, and footer
+   drawCenteredString(boxTop + vertMargin, titleSize, title);
+
+   for(S32 i = 0; i < msgLines; i++)
+      drawCenteredString(boxTop + vertMargin + titleSpace + i * (textSize + textGap), textSize, message[i].c_str());
+
+   drawCenteredString(boxTop + boxHeight - vertMargin - textSize, textSize, instr);
+}
+
+
+// This function could use some further cleaning; currently only used for the delayed spawn notification
+void UserInterface::renderUnboxedMessageBox(const char *title, const char *instr, string message[], S32 msgLines, S32 vertOffset)
+{
+   dimUnderlyingUI();
+
+   const S32 canvasWidth = gScreenInfo.getGameCanvasWidth();
+   const S32 canvasHeight = gScreenInfo.getGameCanvasHeight();
+
+   static const S32 titleSize = 30;              // Size of title
+   static const S32 titleGap = titleSize / 3;    // Spacing between title and first line of text
+   static const S32 textSize = 36;               // Size of text and instructions
+   static const S32 textGap = textSize / 3;      // Spacing between text lines
+   static const S32 instrGap = 15;               // Gap between last line of text and instruction line
+
+   S32 actualLines = 0;
+   for(S32 i = msgLines - 1; i >= 0; i--)
+      if(message[i] != "")
+      {
+         actualLines = i + 1;
+         break;
+      }
+
+
+   S32 titleSpace = titleSize + titleGap;
+   S32 boxHeight = titleSpace + actualLines * (textSize + textGap) + instrGap;
+
+   if(strcmp(instr, "") == 0)
+      boxHeight -= instrGap;
+
+   if(strcmp(title, "") == 0)
+   {
+      boxHeight -= titleSpace;
+      titleSpace = 0;
+   }
+
+   S32 boxTop = (canvasHeight - boxHeight) / 2;
+
+   // Draw title, message, and footer
+   glColor(Colors::blue);
+   drawCenteredString(boxTop + vertMargin, titleSize, title);
+
+   S32 boxWidth = 500;
+   drawHollowRect((canvasWidth - boxWidth) / 2, boxTop - vertMargin, canvasWidth - ((canvasWidth - boxWidth) / 2), boxTop + boxHeight + vertMargin);
+
+   for(S32 i = 0; i < msgLines; i++)
+      drawCenteredString(boxTop + titleSpace + i * (textSize + textGap), textSize, message[i].c_str());
+
+   drawCenteredString(boxTop + boxHeight / 2 - textSize, textSize, instr);
+}
+
+
+void UserInterface::dimUnderlyingUI(F32 amount)
+{
+   glColor(Colors::black, amount); 
+
+   TNLAssert(glIsEnabled(GL_BLEND), "Blending should be enabled here!");
+
+   drawFilledRect (0, 0, gScreenInfo.getGameCanvasWidth(), gScreenInfo.getGameCanvasHeight());
+}
+
+
+void UserInterface::drawMenuItemHighlight(S32 x1, S32 y1, S32 x2, S32 y2, bool disabled)
+{
+   if(disabled)
+      drawFilledRect(x1, y1, x2, y2, Color(0.4), Color(0.8));
+   else
+      drawFilledRect(x1, y1, x2, y2, Colors::blue40, Colors::blue);
+}
+
+
+// These will be overridden in child classes if needed
+void UserInterface::render() 
+{ 
+   // Do nothing -- probably never even gets called
+}
+
+
+void UserInterface::idle(U32 timeDelta)
+{ 
+   mTimeSinceLastInput += timeDelta;
+}
+
+
+void UserInterface::onMouseMoved()                         
+{ 
+   mTimeSinceLastInput = 0;
+}
+
+
+void UserInterface::onMouseDragged()  { /* Do nothing */ }
+
+
+InputCode UserInterface::getInputCode(GameSettings *settings, InputCodeManager::BindingName binding)
+{
+   return settings->getInputCodeManager()->getBinding(binding);
+}
+
+
+void UserInterface::setInputCode(GameSettings *settings, InputCodeManager::BindingName binding, InputCode inputCode)
+{
+   settings->getInputCodeManager()->setBinding(binding, inputCode);
+}
+
+
+bool UserInterface::checkInputCode(GameSettings *settings, InputCodeManager::BindingName binding, InputCode inputCode)
+{
+   return getInputCode(settings, binding) == settings->getInputCodeManager()->filterInputCode(inputCode);
+}
+
+
+const char *UserInterface::getInputCodeString(GameSettings *settings, InputCodeManager::BindingName binding)
+{
+   return InputCodeManager::inputCodeToString(getInputCode(settings, binding));
+}
+
+ 
+bool UserInterface::onKeyDown(InputCode inputCode)
+{ 
+   mTimeSinceLastInput = 0;
+
+   bool handled = false;
+
+   GameSettings *settings = getGame()->getSettings();
+   UIManager *uiManager = getGame()->getUIManager();
+
+   if(checkInputCode(settings, InputCodeManager::BINDING_DIAG, inputCode))              // Turn on diagnostic overlay
+   { 
+      if(uiManager->isCurrentUI(DiagnosticsScreenUI))
+         return false;
+
+      uiManager->activate(DiagnosticsScreenUI);
+
+      playBoop();
+      
+      handled = true;
+   }
+   else if(checkInputCode(settings, InputCodeManager::BINDING_OUTGAMECHAT, inputCode))  // Turn on Global Chat overlay
+   {
+      // Don't activate if we're already in chat or if we're on the Name Entry
+      // screen (since we don't have a nick yet)
+      if(uiManager->isCurrentUI(GlobalChatUI) || uiManager->isCurrentUI(NameEntryUI))
+         return false;
+
+      getGame()->getUIManager()->activate(GlobalChatUI);
+      playBoop();
+
+      handled = true;
+   }
+
+   return handled;
+}
+
+
+void UserInterface::onKeyUp(InputCode inputCode) { /* Do nothing */ }
+void UserInterface::onTextInput(char ascii)      { /* Do nothing */ }
+
+
+
+// Dumps any keys and raw stick button inputs depressed to the screen when in diagnostic mode.
+// This should make it easier to see what happens when users press joystick buttons.
+void UserInterface::renderDiagnosticKeysOverlay()
+{
+   if(gClientGames[0]->getSettings()->getIniSettings()->diagnosticKeyDumpMode)
+   {
+     S32 vpos = gScreenInfo.getGameCanvasHeight() / 2;
+     S32 hpos = horizMargin;
+
+     glColor(Colors::white);
+
+     // Key states
+     for (U32 i = 0; i < MAX_INPUT_CODES; i++)
+        if(InputCodeManager::getState((InputCode) i))
+           hpos += drawStringAndGetWidth( hpos, vpos, 18, InputCodeManager::inputCodeToString((InputCode) i) );
+
+      vpos += 23;
+      hpos = horizMargin;
+      glColor(Colors::magenta);
+
+      for(U32 i = 0; i < Joystick::MaxSdlButtons; i++)
+         if(Joystick::ButtonMask & (1 << i))
+         {
+            drawStringf( hpos, vpos, 18, "RawBut [%d]", i );
+            hpos += getStringWidthf(18, "RawBut [%d]", i ) + 5;
+         }
+   }
+}   
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+void UserInterfaceData::get()
+{
+   vertMargin  = UserInterface::vertMargin;
+   horizMargin = UserInterface::horizMargin;
+   chatMargin  = UserInterface::messageMargin;
+}
+
+
+void UserInterfaceData::set()
+{
+   UserInterface::vertMargin  = vertMargin;
+   UserInterface::horizMargin = horizMargin;
+   UserInterface::messageMargin = chatMargin;
+}
+
+
+};
+
---
+++ zap/helperMenu.cpp
@@ -0,0 +1,342 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+
+#include "helperMenu.h"
+#include "UIGame.h"     // For mGameUserInterface
+#include "UIManager.h"
+#include "UIInstructions.h"
+#include "ClientGame.h"
+#include "JoystickRender.h"
+#include "RenderUtils.h"
+
+#include "OpenglUtils.h"
+
+namespace Zap
+{
+
+// Constructor
+HelperMenu::HelperMenu()
+{
+   mAnimationTimer.setPeriod(150);    // Transition time, in ms
+   mTransitionTimer.setPeriod(65);
+   mTransitioning = false;
+}
+
+
+// Destructor
+HelperMenu::~HelperMenu()
+{
+   // Do nothing
+}
+
+
+void HelperMenu::initialize(ClientGame *game, HelperManager *manager)
+{
+   mHelperManager = manager;
+   mClientGame = game;
+}
+
+
+const char *HelperMenu::getCancelMessage()
+{
+   return "";
+}
+
+
+InputCode HelperMenu::getActivationKey()
+{
+   return KEY_NONE;
+}
+
+
+// Exit helper mode by entering play mode
+void HelperMenu::exitHelper() 
+{ 
+   mAnimationTimer.invert();
+   mActivating = false;
+   mClientGame->getUIManager()->getGameUserInterface()->exitHelper();
+}
+
+
+F32 HelperMenu::getFraction()
+{
+   //if(getActivationAnimationTime() == 0)
+   //   return 0;
+   //else
+      return mActivating ? mAnimationTimer.getFraction() : 1 - mAnimationTimer.getFraction();
+}
+
+
+extern void drawVertLine  (S32 x,  S32 y1, S32 y2);
+extern void drawHorizLine (S32 x1, S32 x2, S32 y );
+
+void HelperMenu::drawItemMenu(S32 xPos, S32 yPos, S32 width, const char *title, const OverlayMenuItem *items, S32 count,
+                              const char **legendText, const Color **legendColors, S32 legendCount)
+{
+   static const Color baseColor(Colors::red);
+
+   const S32 MENU_PADDING = 5;         // Padding around outer edge of overlay
+
+   TNLAssert(glIsEnabled(GL_BLEND), "Expect blending to be on");
+
+   S32 displayItems = 0;
+
+   // Count how many items we will be displaying -- some may be hidden
+   for(S32 i = 0; i < count; i++)
+      if(items[i].showOnMenu)
+         displayItems++;
+
+   // Frame the menu
+   S32 interiorMenuHeight = MENU_FONT_SIZE + MENU_FONT_SPACING + MENU_PADDING +                       // Title
+                            displayItems * (MENU_FONT_SIZE + MENU_FONT_SPACING) + 2 * MENU_PADDING +  // Menu items
+                            (legendCount > 0 ? MENU_LEGEND_FONT_SIZE + 2 * MENU_FONT_SPACING : 0) +   // Legend
+                            2 * MENU_PADDING +                                                        // Post-legend gap
+                            MENU_LEGEND_FONT_SIZE;                                                    // Instructions at bottom
+
+   S32 top1   = yPos - MENU_PADDING;
+   S32 top2   = yPos - MENU_PADDING + MENU_FONT_SIZE + MENU_FONT_SPACING + MENU_PADDING;
+   S32 bottom = yPos + interiorMenuHeight + MENU_PADDING;
+
+   if(mTransitionTimer.getCurrent() > 0)
+      bottom += (mOldBottom - bottom) * mTransitionTimer.getFraction();
+   else
+      mOldBottom = bottom;
+
+   S32 interiorEdge = xPos + width + ITEM_INDENT + ITEM_HELP_PADDING + MENU_PADDING + MENU_PADDING;
+
+   const S32 CORNER_SIZE = 15;
+   Point p[] = { Point(0, top1), Point(interiorEdge - CORNER_SIZE, top1),     // Top
+                 Point(interiorEdge, top1 + CORNER_SIZE),                     // Edge
+                 Point(interiorEdge, bottom), Point(0, bottom) };             // Bottom
+
+   Vector<Point> points(p, ARRAYSIZE(p));
+
+   // Fill
+   glColor(Colors::black, 0.70f);
+   renderPointVector(&points, GL_POLYGON);
+
+   // Border
+   glColor(Color(.35,0,0));
+   renderPointVector(&points, GL_LINE_STRIP);
+
+   // Gray line
+   glColor(Colors::gray20);
+   drawHorizLine(20, interiorEdge - 20, top2);
+
+   // Draw the title
+   glColor(baseColor);
+   drawString(xPos, yPos, MENU_FONT_SIZE, title);
+   yPos += MENU_FONT_SIZE + MENU_FONT_SPACING + MENU_PADDING;
+
+
+   // Determine whether to show keys or joystick buttons on menu
+   GameSettings *settings = getGame()->getSettings();
+   InputMode inputMode    = settings->getInputCodeManager()->getInputMode();
+   bool showKeys = settings->getIniSettings()->showKeyboardKeys || inputMode == InputModeKeyboard;
+
+   yPos += 2;    // Aesthetics -- these pixels will be compensated for below
+
+   for(S32 i = 0; i < count; i++)
+   {
+      // Don't show an option that shouldn't be shown!
+      if(!items[i].showOnMenu)
+         continue;
+      
+      // Draw key controls for selecting the object to be created
+      U32 joystickIndex = Joystick::SelectedPresetIndex;
+
+      if(inputMode == InputModeJoystick)     // Only draw joystick buttons when in joystick mode
+         JoystickRender::renderControllerButton(F32(xPos + (showKeys ? 5 : 25)), (F32)yPos, 
+                                                joystickIndex, items[i].button, false);
+
+      if(showKeys)
+      {
+         // Render key in white, or, if there is a legend, in the color of the adjacent item
+         glColor(legendCount > 0 ? items[i].itemColor : &Colors::white); 
+         JoystickRender::renderControllerButton((F32)xPos + 30, (F32)yPos, 
+                                                joystickIndex, items[i].key, false);
+      }
+
+      glColor(items[i].itemColor);  
+
+      S32 x = drawStringAndGetWidth(xPos + ITEM_INDENT, yPos, MENU_FONT_SIZE, items[i].name); 
+
+      // Render help string, if one is available
+      if(strcmp(items[i].help, "") != 0)
+      {
+         glColor(items[i].helpColor);    
+         drawString(xPos + ITEM_INDENT + ITEM_HELP_PADDING + x, yPos, MENU_FONT_SIZE, items[i].help);
+      }
+
+      yPos += MENU_FONT_SIZE + MENU_FONT_SPACING;
+   }
+
+   if(legendCount > 0)
+   {
+      yPos += MENU_FONT_SPACING;
+      S32 x = xPos + 20;
+      for(S32 i = 0; i < legendCount; i++)
+      {
+         glColor(legendColors[i]);
+         x += drawStringAndGetWidth(x, yPos, MENU_LEGEND_FONT_SIZE, legendText[i]);
+      }
+
+      yPos += MENU_LEGEND_FONT_SIZE + MENU_FONT_SPACING;
+   }
+
+   yPos += 2 * MENU_PADDING;
+
+   yPos -= 2;    // Pixel compensation
+
+   glColor(baseColor);
+
+   // RenderedSize will be -1 if the button is not defined
+   if(settings->getInputCodeManager()->getInputMode() == InputModeKeyboard)
+      drawStringf(xPos, yPos, MENU_LEGEND_FONT_SIZE, 
+                  "Press [%s] to cancel", InputCodeManager::inputCodeToString(KEY_ESCAPE));
+   else
+   {
+      S32 butSize = JoystickRender::getControllerButtonRenderedSize(Joystick::SelectedPresetIndex, BUTTON_BACK);
+
+      xPos += drawStringAndGetWidth(xPos, yPos, MENU_LEGEND_FONT_SIZE, "Press ") + 4;
+      JoystickRender::renderControllerButton(F32(xPos + 4), F32(yPos), Joystick::SelectedPresetIndex, BUTTON_BACK, false);
+      xPos += butSize;
+      glColor(baseColor);
+      drawString(xPos, yPos, MENU_LEGEND_FONT_SIZE, "to cancel");
+   }
+}
+
+
+// Calculate the width of the widest item in items
+S32 HelperMenu::getWidth(const OverlayMenuItem *items, S32 count)
+{
+   S32 width = -1;
+   for(S32 i = 0; i < count; i++)
+   {
+      S32 w = getStringWidth(MENU_FONT_SIZE, items[i].name) + getStringWidth(MENU_FONT_SIZE, items[i].help);
+      if(w > width)
+         width = w;
+   }
+
+   return width;
+}
+
+
+S32 HelperMenu::getAnimationPeriod() const
+{
+   return mAnimationTimer.getPeriod();
+}
+
+
+ClientGame *HelperMenu::getGame()
+{
+   return mClientGame;
+}
+
+
+// Returns true if key was handled, false if it should be further processed
+bool HelperMenu::processInputCode(InputCode inputCode)
+{
+   // First, check navigation keys.  When in keyboard mode, we allow the loadout key to toggle menu on and off...
+   // we can't do this in joystick mode because it is likely that the loadout key is also used to select items
+   // from the loadout menu.
+   if(inputCode == KEY_ESCAPE  || inputCode == KEY_BACKSPACE    ||
+      inputCode == KEY_LEFT    || inputCode == BUTTON_DPAD_LEFT ||
+      inputCode == BUTTON_BACK || 
+      (getGame()->getSettings()->getInputCodeManager()->getInputMode() == InputModeKeyboard && inputCode == getActivationKey()) )
+   {
+      exitHelper();      
+
+      if(getGame()->getSettings()->getIniSettings()->verboseHelpMessages)
+         mClientGame->displayMessage(Colors::paleRed, getCancelMessage());
+
+      return true;
+   }
+
+   return false;
+}
+
+
+void HelperMenu::onTextInput(char ascii)
+{
+   // Do nothing (overridden by ChatHelper)
+}
+
+
+void HelperMenu::activateHelp(UIManager *uiManager)
+{
+    uiManager->activate(InstructionsUI);
+}
+
+
+// Return true if menu is playing the closing animation
+bool HelperMenu::isClosing() const
+{
+   return !mActivating && mAnimationTimer.getCurrent() > 0;
+}
+
+
+bool HelperMenu::isMovementDisabled() { return false; }
+bool HelperMenu::isChatDisabled()     { return true;  }
+
+
+void HelperMenu::idle(U32 deltaT) 
+{
+   if(mAnimationTimer.update(deltaT) && !mActivating)
+      mHelperManager->doneClosingHelper();
+
+   if(mTransitionTimer.update(deltaT))
+      mTransitioning = false;
+}
+
+
+// Used for loadout-style menus
+S32 HelperMenu::getLeftEdgeOfMenuPos()
+{
+   S32 trueLeftEdge = UserInterface::horizMargin;
+
+   //if(getActivationAnimationTime() == 0)
+   //   return trueLeftEdge;
+
+   // else calculate where the left edge should be based on state of animation timer
+
+   // Magic number that seems to work well... no matter that the real menu might be a different width... by
+   // using this constant, menus appear at a consistent rate.
+   F32 width = 400;     
+
+   return trueLeftEdge - width + (mActivating ? width - mAnimationTimer.getFraction() * width : 
+                                                        mAnimationTimer.getFraction() * width);
+}
+
+
+void HelperMenu::onActivated()    
+{
+   mAnimationTimer.invert();
+   mActivating = true;
+   mTransitioning = false;
+}
+
+};
