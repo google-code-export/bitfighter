7b3bd107f3b29234afc9b55ac6cd385a954bb4c1
watusimoto <watusimoto@bitfighter.org>
2013-04-27 21:57:19 +0200

    Add some const declarations

---
+++ zap/UI.h
@@ -0,0 +1,168 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _UI_H_
+#define _UI_H_
+
+#ifdef ZAP_DEDICATED
+#  error "UI.h shouldn't be included in dedicated build"
+#endif
+
+#include "UIManager.h"
+#include "InputCode.h"
+#include "SharedConstants.h"
+#include "Timer.h"
+#include "lineEditor.h"
+#include "config.h"           // For DisplayMode enum
+#include "stringUtils.h"      // For itos
+
+#include "RenderUtils.h"
+
+#include "tnl.h"
+#include "tnlLog.h"
+
+#ifdef TNL_OS_MOBILE
+#  include "SDL_opengles.h"
+#else
+#  include "SDL_opengl.h"
+#endif
+
+#include <string>
+
+using namespace TNL;
+using namespace std;
+
+namespace Zap
+{
+
+extern F32 gLineWidth1;
+extern F32 gDefaultLineWidth;
+extern F32 gLineWidth3;
+extern F32 gLineWidth4;
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+class Game;
+class ClientGame;
+class UIManager;
+
+class UserInterface
+{
+   friend class UIManager;    // Give UIManager access to private and protected members
+
+private:
+   UIID mInternalMenuID;                     // Unique interface ID
+
+   static void doDrawAngleString(F32 x, F32 y, F32 size, F32 angle, const char *string, bool autoLineWidth = true);
+   static void doDrawAngleString(S32 x, S32 y, F32 size, F32 angle, const char *string, bool autoLineWidth = true);
+
+   ClientGame *mClientGame;
+
+   U32 mTimeSinceLastInput;
+
+   // Activate menus via the UIManager, please!
+   void activate();
+   void reactivate();
+
+protected:
+   static bool mDisableShipKeyboardInput;    // Disable ship movement while user is in menus
+   void setMenuID(UIID menuID);              // Set interface's name
+
+public:
+   explicit UserInterface(ClientGame *game);       // Constructor
+   virtual ~UserInterface();                       // Destructor
+   static const S32 MAX_PASSWORD_LENGTH = 32;      // Arbitrary, doesn't matter, but needs to be _something_
+
+   UIID getMenuID() const;                   // Retrieve interface's name
+   UIID getPrevMenuID() const;               // Retrieve previous interface's name
+
+   ClientGame *getGame() const;
+
+   UIManager *getUIManager() const;
+
+   static S32 vertMargin, horizMargin;
+   static S32 messageMargin;
+
+   U32 getTimeSinceLastInput();
+
+   virtual void render();
+   virtual void idle(U32 timeDelta);
+   virtual void onActivate();
+   virtual void onDeactivate(bool nextUIUsesEditorScreenMode);
+   virtual void onReactivate();
+   virtual void onDisplayModeChange();
+
+
+   virtual bool usesEditorScreenMode();   // Returns true if the UI attempts to use entire screen like editor, false otherwise
+
+   void renderConsole();                  // Render game console
+   virtual void renderMasterStatus();     // Render master server connection status
+
+   // Helpers to simplify dealing with key bindings
+   InputCode getInputCode(GameSettings *settings, InputCodeManager::BindingName binding);
+   void setInputCode(GameSettings *settings, InputCodeManager::BindingName binding, InputCode inputCode);
+   bool checkInputCode(GameSettings *settings, InputCodeManager::BindingName, InputCode inputCode);
+   const char *getInputCodeString(GameSettings *settings, InputCodeManager::BindingName binding);
+
+
+   // Input event handlers
+   virtual bool onKeyDown(InputCode inputCode);
+   virtual void onKeyUp(InputCode inputCode);
+   virtual void onTextInput(char ascii);
+   virtual void onMouseMoved();
+   virtual void onMouseDragged();
+
+   void renderMessageBox(const char *title, const char *instr, string message[], S32 msgLines, S32 vertOffset = 0, S32 style = 1);
+   void renderUnboxedMessageBox(const char *title, const char *instr, string message[], S32 msgLines, S32 vertOffset = 0);
+
+   static void renderFancyBox(S32 boxTop, S32 boxHeight, S32 inset, Color borderColor, F32 alpha);
+
+   void dimUnderlyingUI(F32 amount = 0.75f);
+
+   static void renderDiagnosticKeysOverlay();
+
+   static void drawMenuItemHighlight(S32 x1, S32 y1, S32 x2, S32 y2, bool disabled = false);
+   static void playBoop();    // Make some noise!
+};
+
+
+
+// Used only for multiple mClientGame in one instance
+struct UserInterfaceData
+{
+   S32 vertMargin, horizMargin;
+   S32 chatMargin;
+
+   void get();
+   void set();
+};
+
+};
+
+#endif
+
+
---
+++ zap/gameObjectRender.h
@@ -0,0 +1,342 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef _GAMEOBJECTRENDER_H_
+#define _GAMEOBJECTRENDER_H_
+
+#ifndef ZAP_DEDICATED
+
+#include "tnl.h"
+
+#include "Geometry.h"
+
+#include "Rect.h"
+#include "Color.h"
+#include "SharedConstants.h"     // For MeritBadges enum
+#include "ShipShape.h"
+
+#include <string>
+
+using namespace TNL;
+using namespace std;
+
+namespace Zap
+{
+
+extern const Color *HIGHLIGHT_COLOR;
+extern const Color *SELECT_COLOR;
+extern const Color *PLAIN_COLOR;
+
+static const S32 NO_NUMBER = -1;
+
+class Ship;
+class WallItem;
+
+//////////
+// Primitives
+extern void drawFilledCircle(const Point &pos, F32 radius, const Color *color = NULL);
+extern void drawFilledSector(const Point &pos, F32 radius, F32 start, F32 end);
+extern void drawCentroidMark(const Point &pos, F32 radius);
+extern void renderTwoPointPolygon(const Point &p1, const Point &p2, F32 width, S32 mode);
+
+extern void drawRoundedRect(const Point &pos, F32 width, F32 height, F32 radius);
+extern void drawRoundedRect(const Point &pos, S32 width, S32 height, S32 radius);
+
+
+extern void drawFilledRoundedRect(const Point &pos, S32 width, S32 height, const Color &fillColor, 
+                                  const Color &outlineColor, S32 radius, F32 alpha = 1.0);
+extern void drawFilledRoundedRect(const Point &pos, F32 width, F32 height, const Color &fillColor, 
+                                  const Color &outlineColor, F32 radius, F32 alpha = 1.0);
+
+extern void drawArc(const Point &pos, F32 radius, F32 startAngle, F32 endAngle);
+
+extern void drawEllipse(const Point &pos, F32 width, F32 height, F32 angle);
+extern void drawEllipse(const Point &pos, S32 width, S32 height, F32 angle);
+
+extern void drawFilledEllipse(const Point &pos, F32 width, F32 height, F32 angle);
+extern void drawFilledEllipse(const Point &pos, S32 width, S32 height, F32 angle);
+
+extern void drawPolygon(const Point &pos, S32 sides, F32 radius, F32 angle);
+
+extern void drawStar(const Point &pos, S32 points, F32 radius, F32 innerRadius);
+
+
+extern void drawAngledRay(const Point &center, F32 innerRadius, F32 outerRadius, F32 angle);
+extern void drawAngledRayCircle(const Point &center, F32 innerRadius, F32 outerRadius, S32 rayCount, F32 offsetAngle);
+extern void drawAngledRayArc(const Point &center, F32 innerRadius, F32 outerRadius, F32 centralAngle, S32 rayCount, F32 offsetAngle);
+extern void drawDashedArc(const Point &center, F32 radius, F32 centralAngle, S32 dashCount, F32 dashSpaceCentralAngle, F32 offsetAngle);
+extern void drawDashedHollowCircle(const Point &center, F32 innerRadius, F32 outerRadius, S32 dashCount, F32 dashSpaceCentralAngle, F32 offsetAngle);
+extern void drawDashedCircle(const Point &center, F32 radius, S32 dashCount, F32 dashSpaceCentralAngle, F32 offsetAngle);
+extern void drawHollowArc(const Point &center, F32 innerRadius, F32 outerRadius, F32 centralAngle, F32 offsetAngle);
+
+extern void drawSquare(const Point &pos, F32 radius, bool filled = false);
+extern void drawSquare(const Point &pos, S32 radius, bool filled = false);
+extern void drawHollowSquare(const Point &pos, F32 radius, const Color *color = NULL);
+extern void drawFilledSquare(const Point &pos, F32 radius, const Color *color = NULL);
+extern void drawFilledSquare(const Point &pos, S32 radius, const Color *color = NULL);
+
+extern void renderSmallSolidVertex(F32 currentScale, const Point &pos, bool snapping);
+
+extern void renderVertex(char style, const Point &v, S32 number);
+extern void renderVertex(char style, const Point &v, S32 number,           F32 scale);
+extern void renderVertex(char style, const Point &v, S32 number,           F32 scale, F32 alpha);
+extern void renderVertex(char style, const Point &v, S32 number, S32 size, F32 scale, F32 alpha);
+
+void renderLine(const Vector<Point> *points, const Color *color);
+
+extern void drawHorizLine(S32 x1, S32 x2, S32 y);
+extern void drawVertLine (S32 x,  S32 y1, S32 y2);
+extern void drawHorizLine(F32 x1, F32 x2, F32 y);
+extern void drawVertLine (F32 x,  F32 y1, F32 y2);
+
+extern void renderSquareItem(const Point &pos, const Color *c, F32 alpha, const Color *letterColor, char letter);
+
+extern void drawCircle(const Point &pos, F32 radius, const Color *color = NULL, F32 alpha = 1.0);
+extern void drawCircle(F32 x, F32 y,     F32 radius, const Color *color = NULL, F32 alpha = 1.0);
+
+extern void drawDivetedTriangle(F32 height, F32 len);
+extern void drawGear(const Point &pos, S32 teeth, F32 r1, F32 r2, F32 ang1, F32 ang2, F32 innerCircleRadius);
+
+
+//////////
+// Some things for rendering on screen display
+void renderEnergyGuage(S32 energy);
+
+extern F32 renderCenteredString(const Point &pos, S32 size, const char *string);
+extern F32 renderCenteredString(const Point &pos, F32 size, const char *string);
+
+// Renders the core ship, good for instructions and such
+extern void renderShip(ShipShape::ShipShapeType shapeType, const Color *shipColor, F32 alpha, F32 thrusts[], F32 health, F32 radius, U32 sensorTime,
+                       bool shieldActive, bool sensorActive, bool repairActive, bool hasArmor);
+
+// Renders the ship and all the fixins
+extern void renderShip(S32 layerIndex, const Point &renderPos, const Point &actualPos, const Point &vel, 
+                       F32 angle, F32 deltaAngle, ShipShape::ShipShapeType shape, const Color *color, F32 alpha, 
+                       U32 renderTime, const string &shipName, F32 warpInScale, bool isLocalShip, bool isBusy, 
+                       bool isAuthenticated, bool showCoordinates, F32 health, F32 radius, S32 team, 
+                       bool boostActive, bool shieldActive, bool repairActive, bool sensorActive, 
+                       bool hasArmor, bool engineeringTeleport);
+
+extern void renderSpawnShield(const Point &pos, U32 shieldTime, U32 renderTime);
+
+// Render repair rays to all the repairing objects
+extern void renderShipRepairRays(const Point &pos, const Ship *ship, Vector<SafePtr<BfObject> > &repairTargets, F32 alpha);   
+
+extern void renderShipCoords(const Point &coords, bool localShip, F32 alpha);
+
+extern void renderAimVector();
+extern void drawFourArrows(const Point &pos);
+
+extern void renderTeleporter(const Point &pos, U32 type, bool spiralInwards, S32 time, F32 zoomFraction, F32 radiusFraction, F32 radius, F32 alpha, 
+                             const Vector<Point> *dests, U32 trackerCount = 100);
+extern void renderTeleporterOutline(const Point &center, F32 radius, const Color &color);
+extern void renderSpyBugVisibleRange(const Point &pos, const Color &color, F32 currentScale = 1);
+extern void renderTurretFiringRange(const Point &pos, const Color &color, F32 currentScale);
+extern void renderTurret(const Color &c, Point anchor, Point normal, bool enabled, F32 health, F32 barrelAngle, S32 healRate = 0);
+
+extern void renderFlag(const Point &pos, const Color *flagColor);
+extern void renderFlag(const Point &pos, F32 scale, const Color *flagColor);
+extern void renderFlag(F32 x, F32 y, const Color *flagColor);
+extern void renderFlag(const Color *flagColor);
+extern void renderFlag(const Point &pos, const Color *flagColor, const Color *mastColor, F32 alpha);
+extern void doRenderFlag(F32 x, F32 y, F32 scale, const Color *flagColor, const Color *mastColor, F32 alpha);
+
+
+//extern void renderFlag(Point pos, Color c, F32 timerFraction);
+extern void renderSmallFlag(const Point &pos, const Color &c, F32 parentAlpha);
+extern void renderFlagSpawn(const Point &pos, F32 currentScale, const Color *color);
+
+extern void renderZone(const Color *c, const Vector<Point> *outline, const Vector<Point> *fill);   
+
+extern void renderLoadoutZone(const Color *c, const Vector<Point> *outline, const Vector<Point> *fill, 
+                              const Point &centroid, F32 angle, F32 scaleFact = 1);
+
+extern void renderLoadoutZoneIcon(const Point &pos);
+
+extern void renderNavMeshZone(const Vector<Point> *outline, const Vector<Point> *fill,
+                              const Point &centroid, S32 zoneId, bool isConvex, bool isSelected = false);
+
+class Border;
+extern void renderNavMeshBorder(const Border &border, F32 scaleFact, const Color &color, F32 fillAlpha, F32 width);
+
+class ZoneBorder;
+extern void renderNavMeshBorders(const Vector<ZoneBorder> &borders, F32 scaleFact = 1);
+
+class NeighboringZone;
+extern void renderNavMeshBorders(const Vector<NeighboringZone> &borders, F32 scaleFact = 1);
+
+extern void drawStars(const Point *stars, S32 numStars, F32 alphaFrac, bool starsInDistance, Point cameraPos, Point visibleExtent);
+
+
+// Some things we use internally, but also need from UIEditorInstructions for consistency
+extern const Color BORDER_FILL_COLOR;
+extern const F32 BORDER_FILL_ALPHA;
+extern const F32 BORDER_WIDTH;
+extern float gDefaultLineWidth;
+
+extern void renderPolygonOutline(const Vector<Point> *outline);
+extern void renderPolygonOutline(const Vector<Point> *outlinePoints, const Color *outlineColor, F32 alpha = 1, F32 lineThickness = gDefaultLineWidth);
+extern void renderPolygonFill(const Vector<Point> *fillPoints, const Color *fillColor, F32 alpha = 1);
+extern void renderPolygon(const Vector<Point> *fillPoints, const Vector<Point> *outlinePoints,
+                          const Color *fillColor, const Color *outlineColor, F32 alpha = 1);
+
+extern void renderGoalZone(const Color &c, const Vector<Point> *outline, const Vector<Point> *fill);     // No label version
+extern void renderGoalZone(const Color &c, const Vector<Point> *outline, const Vector<Point> *fill, Point centroid, F32 labelAngle,
+                           bool isFlashing, F32 glowFraction, S32 score, F32 flashCounter, bool useOldStyle);
+
+extern void renderNexus(const Vector<Point> *outline, const Vector<Point> *fill, Point centroid, F32 labelAngle, 
+                        bool open, F32 glowFraction, F32 scaleFact = 1);
+
+extern void renderNexus(const Vector<Point> *outline, const Vector<Point> *fill, bool open, F32 glowFraction);
+
+
+
+extern void renderSlipZone(const Vector<Point> *bounds, const Vector<Point> *boundsFill, const Point &centroid);
+extern void renderPolygonLabel(const Point &centroid, F32 angle, F32 size, const char *text, F32 scaleFact = 1);
+
+extern void renderProjectile(const Point &pos, U32 type, U32 time);
+extern void renderSeeker(const Point &pos, F32 angleRadians, F32 speed, U32 timeRemaining);
+
+extern void renderMine(const Point &pos, bool armed, bool visible);
+extern void renderGrenade(const Point &pos, F32 lifeLeft);
+extern void renderSpyBug(const Point &pos, const Color &teamColor, bool visible, bool drawOutline);
+
+extern void renderRepairItem(const Point &pos);
+extern void renderRepairItem(const Point &pos, bool forEditor, const Color *overrideColor, F32 alpha);
+
+extern void renderEnergyItem(const Point &pos); 
+
+extern void renderWallFill(const Vector<Point> *points, const Color &fillColor, bool polyWall);
+extern void renderWallFill(const Vector<Point> *points, const Color &fillColor, const Point &offset, bool polyWall);
+
+extern void renderEnergyItem(const Point &pos, bool forEditor);
+extern void renderEnergySymbol();                                   // Render lightning bolt symbol
+extern void renderEnergySymbol(const Point &pos, F32 scaleFactor);  // Another signature
+
+// Wall rendering
+void renderWallEdges(const Vector<Point> &edges, const Color &outlineColor, F32 alpha = 1.0);
+void renderWallEdges(const Vector<Point> &edges, const Point &offset, const Color &outlineColor, F32 alpha = 1.0);
+
+//extern void renderSpeedZone(Point pos, Point normal, U32 time);
+void renderSpeedZone(const Vector<Point> &pts, U32 time);
+
+void renderTestItem(const Point &pos, S32 size, F32 alpha = 1);
+//void renderTestItem(const Point &pos, S32 size, F32 alpha = 1);
+void renderTestItem(const Vector<Point> &points, F32 alpha = 1);
+
+void renderWorm(const Point &pos);
+
+void renderAsteroid(const Point &pos, S32 design, F32 scaleFact);
+void renderAsteroid(const Point &pos, S32 design, F32 scaleFact, const Color *color, F32 alpha = 1);
+
+void renderAsteroidSpawn(const Point &pos, S32 time);
+void renderAsteroidSpawnEditor(const Point &pos, F32 scale = 1.0);
+
+void renderResourceItem(const Vector<Point> &points, F32 alpha = 1);
+//void renderResourceItem(const Point &pos, F32 scaleFactor, const Color *color, F32 alpha);
+
+struct PanelGeom;
+void renderCore(const Point &pos, const Color *coreColor, U32 time, 
+                PanelGeom *panelGeom, F32 panelHealth[10], F32 panelStartingHealth);
+
+void renderCoreSimple(const Point &pos, const Color *coreColor, S32 width);
+
+void renderSoccerBall(const Point &pos, F32 size);
+void renderSoccerBall(const Point &pos);
+
+void renderTextItem(const Point &pos, const Point &dir, F32 size, const string &text, const Color *color);
+
+// Editor support items
+extern void renderPolyLineVertices(BfObject *obj, bool snapping, F32 currentScale);
+extern void renderGrid(F32 curentScale, const Point &offset, const Point &origin, F32 gridSize, bool fadeLines, bool showMinorGridLines);
+
+extern void renderForceFieldProjector(Point pos, Point normal);
+extern void renderForceFieldProjector(Point pos, Point normal, const Color *teamColor, bool enabled);
+extern void renderForceFieldProjector(const Vector<Point> *geom, const Color *teamColor, bool enabled, S32 healRate = 0);
+extern void renderForceField(Point start, Point end, const Color *c, bool fieldUp, F32 scale = 1);
+
+extern void renderBitfighterLogo(S32 yPos, F32 scale, U32 mask = 1023);
+extern void renderBitfighterLogo(const Point &pos, F32 size, U32 letterMask = 1023);
+extern void renderStaticBitfighterLogo();
+
+extern void renderBadge(F32 x, F32 y, F32 rad, MeritBadges badge);
+
+extern void renderWalls(const GridDatabase *wallSegmentDatabase, const Vector<Point> &wallEdgePoints, 
+                        const Vector<Point> &selectedWallEdgePoints, const Color &outlineColor, 
+                        const Color &fillColor, F32 currentScale, bool dragMode, bool drawSelected,
+                        const Point &selectedItemOffset, bool previewMode, bool showSnapVertices, F32 alpha);
+
+
+extern void renderWallOutline(WallItem *wallItem, const Vector<Point> *outline, const Color *color, 
+                              F32 currentScale, bool snappingToWallCornersEnabled);
+
+extern void drawLetter(char letter, const Point &pos, const Color &color, F32 alpha);
+extern void renderCircleSpawn(const Point &pos, F32 scale = 1.0);
+extern void renderSpawn(const Point &pos, F32 scale, const Color *color);
+extern void renderFlightPlan(const Point &from, const Point &to, const Vector<Point> &flightPlan);
+extern void renderHeavysetArrow(const Point &pos, const Point &dest, const Color &color, bool isSelected, bool isLitUp);
+extern void renderTeleporterEditorObject(const Point &pos, S32 radius, const Color &color);
+};
+
+#else
+
+// for ZAP_DEDICATED, we will just define blank functions, and don't compile gameObjectRender.cpp
+
+#define renderSoccerBall
+#define renderNexus
+#define renderTextItem
+#define renderSpeedZone
+#define renderSlipZone
+#define renderProjectile
+#define renderGrenade
+#define renderMine
+#define renderSpyBug
+#define renderLoadoutZone
+#define renderZone
+#define renderGoalZone
+#define renderForceFieldProjector
+#define renderForceField
+#define renderTurret
+#define renderSquareItem
+#define renderNavMeshZone
+#define renderNavMeshBorders
+#define renderRepairItem
+#define renderEnergyItem
+#define renderAsteroid
+#define renderCore
+#define renderWorm
+#define renderTestItem
+#define renderResourceItem
+#define renderFlag
+#define renderWallFill
+#define renderWallEdges
+#define renderPolygonFill
+#define renderPolygonOutline
+#define drawCircle
+#define drawSquare
+
+
+#endif
+#endif
