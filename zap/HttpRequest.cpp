445f97a4c4183936ea8438ee775e46e7b7c68ddd
buckyballreaction <buckyballreaction@gmail.com>
2013-05-12 17:52:31 -0600

    Add icon to Windows EXE

---
+++ zap/HttpRequest.cpp
@@ -0,0 +1,299 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "HttpRequest.h"
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+using namespace TNL;
+
+namespace Zap
+{
+
+const string HttpRequest::GetMethod = "GET";
+const string HttpRequest::PostMethod = "POST";
+
+HttpRequest::HttpRequest(string url, TNL::Socket* socket, TNL::Address* localAddress, TNL::Address* remoteAddress)
+   : mUrl(url), mMethod("GET"), mResponseCode(0), mTimeout(30000)
+{
+   // hostname is anything before the first '/'
+   TNL::U32 index = mUrl.find('/');
+   string host = mUrl.substr(0, index);
+   string addressString = "ip:" + host + ":80";
+
+   mLocalAddress = localAddress ? localAddress : new Address(TCPProtocol, Address::Any, 0);
+   mRemoteAddress = remoteAddress ? remoteAddress : new Address(addressString.c_str());
+   mSocket = socket ? socket : new Socket(*mLocalAddress);
+}
+
+// Destructor
+HttpRequest::~HttpRequest()
+{
+   // Do nothing
+}
+
+
+bool HttpRequest::send()
+{
+   // check that TNL understands the supplied address
+   if(!mRemoteAddress->isValid())
+   {
+      return false;
+   }
+
+   // initiate the connection. this will block if DNS resolution is required
+   if(mSocket->connect(*mRemoteAddress) != UnknownError)
+   {
+      return false;
+   }
+
+   buildRequest();
+   if(!sendRequest(mRequest))
+   {
+      return false;
+   }
+
+   string response = receiveResponse();
+   if(response == "")
+   {
+      return false;
+   }
+
+   parseResponse(response);
+   if(getResponseCode() == 0)
+   {
+      return false;
+   }
+
+   return true;
+}
+
+
+string HttpRequest::getResponseBody()
+{
+   return mResponseBody;
+}
+
+
+string HttpRequest::getResponseHead()
+{
+   return mResponseHead;
+}
+
+
+S32 HttpRequest::getResponseCode()
+{
+   return mResponseCode;
+}
+
+
+void HttpRequest::parseResponse(string response)
+{
+   U32 seperatorIndex = response.find("\r\n\r\n");
+   if(seperatorIndex == string::npos || response == "")
+   {
+      // seperator not found, this response isn't valid
+      return;
+   }
+
+   mResponseHead = response.substr(0, seperatorIndex);
+
+   U32 bodyIndex = seperatorIndex + 4;
+   mResponseBody = response.substr(bodyIndex, response.length());
+
+   U32 responseCodeStart = mResponseHead.find(" ") + 1;
+   U32 responseCodeEnd = mResponseHead.find("\r\n", responseCodeStart);
+   string responseCode = mResponseHead.substr(responseCodeStart, responseCodeEnd - responseCodeStart);
+   mResponseCode = atoi(responseCode.c_str());
+}
+
+
+string HttpRequest::urlEncodeChar(char c)
+{
+   U32 ordinal = c;
+   string result;
+   // see if the character is unreserved
+   if(
+      (ordinal >= 0x41 && ordinal <= 0x5A) || // lowercase
+      (ordinal >= 0x61 && ordinal <= 0x7A) || // uppercase
+      (ordinal >= 0x30 && ordinal <= 0x39) || // digits
+      ordinal == 0x2D ||                      // hyphen
+      ordinal == 0x2E ||                      // period
+      ordinal == 0x5F ||                      // underscore
+      ordinal == 0x7E                         // tilde
+   )
+   {
+      result = ordinal;
+   }
+   else
+   {
+      char buffer[4];
+      // Convert ordinal to a two character hex number in the range [0, 255],
+      // prefixed by a percentage sign
+      dSprintf(buffer, 16, (const char*) "%%%0.2x", (U32) ordinal & 0xFF);
+      result = buffer;
+   }
+   return result;
+}
+
+
+string HttpRequest::urlEncode(const string& str)
+{
+   string result;
+   string::const_iterator it;
+
+   for(it = str.begin(); it < str.end(); it++)
+   {
+      result += urlEncodeChar(*it);
+   }
+   return result;
+}
+
+
+void HttpRequest::setData(const string& key, const string& value)
+{
+   mData.erase(key);
+   mData[key] = value;
+}
+
+
+void HttpRequest::setMethod(const string& method)
+{
+   mMethod = method;
+}
+
+
+string HttpRequest::buildRequest()
+{
+   // location is anything that comes after the first '/'
+   TNL::U32 index = mUrl.find('/');
+   string location = mUrl.substr(index, mUrl.length() - index);
+
+   // construct the request
+   mRequest = "";
+
+   // request line
+   mRequest += mMethod + " " + location + " HTTP/1.0";
+
+   // content type and data encoding for POST requests
+   if(mMethod == PostMethod)
+   {
+      mRequest += "\r\nContent-Type: application/x-www-form-urlencoded";
+
+      string encodedData;
+      map<string, string>::iterator it;
+      for(it = mData.begin(); it != mData.end(); it++)
+      {
+         encodedData += urlEncode((*it).first) + "=" + urlEncode((*it).second) + "&";
+      }
+
+      char contentLengthHeaderBuffer[1024];
+      dSprintf(contentLengthHeaderBuffer, 1024, "\r\nContent-Length: %d", encodedData.length());
+
+      mRequest += contentLengthHeaderBuffer;
+      mRequest += "\r\n\r\n";
+      mRequest += encodedData;
+   }
+   else
+   {
+      mRequest += "\r\n\r\n";
+   }
+   return mRequest;
+}
+
+
+bool HttpRequest::sendRequest(string request)
+{
+   U32 startTime = Platform::getRealMilliseconds();
+   while(Platform::getRealMilliseconds() - startTime < mTimeout)
+   {
+      Platform::sleep(PollInterval);
+      NetError sendError;
+      sendError = mSocket->send((unsigned char *) mRequest.c_str(), mRequest.size());
+
+      if(sendError == WouldBlock)
+      {
+         // need to wait
+         continue;
+      }
+      else if(sendError == NoError)
+      {
+         // data was transmitted
+         return true;
+      }
+
+      // an error occured
+      return false;
+   }
+   return false;
+}
+
+
+string HttpRequest::receiveResponse()
+{
+   mResponse = "";
+   S32 startTime = Platform::getRealMilliseconds();
+   while(Platform::getRealMilliseconds() - startTime < mTimeout)
+   {
+      Platform::sleep(50);
+      TNL::NetError recvError;
+      S32 bytesRead = 0;
+      char receiveBuffer[HttpRequest::BufferSize] = { 0 };
+      recvError = mSocket->recv((unsigned char*) receiveBuffer, HttpRequest::BufferSize, &bytesRead);
+
+      if(recvError == TNL::WouldBlock)
+      {
+         // need to wait
+         continue;
+      }
+
+      if(recvError == TNL::UnknownError)
+      {
+         // there was an error, ignore partial responses
+         mResponse = "";
+         break;
+      }
+
+      mResponse.append(receiveBuffer, 0, bytesRead);
+
+      if(bytesRead == 0)
+      {
+         break;
+      }
+
+      // more data to read
+   }
+   return mResponse;
+}
+
+void HttpRequest::setTimeout(U32 timeout)
+{
+   mTimeout = timeout;
+}
+
+}
---
+++ zap/SymbolShape.cpp
@@ -0,0 +1,339 @@
+﻿//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#include "SymbolShape.h"
+
+#include "FontManager.h"
+#include "InputCode.h"
+
+#include "gameObjectRender.h"
+#include "Colors.h"
+
+#include "OpenglUtils.h"
+#include "RenderUtils.h"
+
+using namespace TNL;
+
+
+namespace Zap { namespace UI {
+
+
+S32 SymbolShape::getWidth() const
+{
+   return mWidth;
+}
+
+
+void SymbolShape::updateWidth(S32 fontSize, FontContext fontContext)
+{
+   // Do nothing (is overridden)
+}
+
+
+
+// Constructor
+SymbolRoundedRect::SymbolRoundedRect(S32 width, S32 height, S32 radius)
+{
+   mWidth = width;
+   mHeight = height;
+   mRadius = radius;
+}
+
+
+void SymbolRoundedRect::render(const Point &center, S32 fontSize, FontContext fontContext) const
+{
+   drawRoundedRect(center, mWidth, mHeight, mRadius);
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+// Constructor
+SymbolHorizEllipse::SymbolHorizEllipse(S32 width, S32 height)
+{
+   mWidth = width;
+   mHeight = height;
+}
+
+
+void SymbolHorizEllipse::render(const Point &center, S32 fontSize, FontContext fontContext) const
+{
+   // First the fill
+   drawFilledEllipse(center, mWidth, mHeight, 0);
+
+   // Outline in white
+   glColor(Colors::white);
+   drawEllipse(center, mWidth, mHeight, 0);
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+// Constructor
+SymbolRightTriangle::SymbolRightTriangle(S32 width)
+{
+   mWidth = width;
+}
+
+
+static void drawButtonRightTriangle(const Point &center)
+{
+   Point p1(center + Point(-15, -9));
+   Point p2(center + Point(-15, 10));
+   Point p3(center + Point(12, 0));
+
+   F32 vertices[] = {
+         p1.x, p1.y,
+         p2.x, p2.y,
+         p3.x, p3.y
+   };
+   renderVertexArray(vertices, ARRAYSIZE(vertices) / 2, GL_LINE_LOOP);
+}
+
+
+void SymbolRightTriangle::render(const Point &center, S32 fontSize, FontContext fontContext) const
+{
+   Point cen(center.x -mWidth / 4, center.y);  // Need to off-center the label slightly for this button
+   drawButtonRightTriangle(cen);
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+// Constructor
+SymbolCircle::SymbolCircle(S32 radius)
+{
+   mWidth = radius * 2;
+   mHeight = radius * 2;
+}
+
+void SymbolCircle::render(const Point &center, S32 fontSize, FontContext fontContext) const
+{
+   drawCircle(center, (F32)mWidth / 2);
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+// Constructor
+SymbolGear::SymbolGear() : Parent(0)
+{
+   // Do nothing
+}
+
+
+void SymbolGear::updateWidth(S32 fontSize, FontContext fontContext)
+{
+   mWidth = S32(1.333f * fontSize);    // mWidth is effectively a diameter; we'll use mWidth / 2 for our rendering radius
+   mHeight = mWidth;
+}
+
+
+void SymbolGear::render(const Point &center, S32 fontSize, FontContext fontContext) const
+{
+   renderLoadoutZoneIcon(center + Point(0,2), mWidth / 2);    // Slight downward adjustment to position to better align with text
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+SymbolText::SymbolText(const string &text)
+{
+   mText = text;
+   mWidth = -1;
+}
+
+
+void SymbolText::updateWidth(S32 fontSize, FontContext fontContext)
+{
+   mWidth = getStringWidth(fontContext, fontSize, mText.c_str());
+}
+
+
+void SymbolText::render(const Point &center, S32 fontSize, FontContext fontContext) const
+{
+   FontManager::pushFontContext(fontContext);
+   drawString(center.x - mWidth / 2, center.y - fontSize / 2, fontSize, mText.c_str());
+   FontManager::popFontContext();
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+SymbolKey::SymbolKey(const string &text) : Parent(text)
+{
+   // Do nothing
+}
+
+
+void SymbolKey::updateWidth(S32 fontSize, FontContext fontContext)
+{
+   mWidth = getStringWidth(KeyContext, fontSize, mText.c_str());
+}
+
+
+void SymbolKey::render(const Point &center, S32 fontSize, FontContext fontContext) const
+{
+   Parent::render(center, fontSize, KeyContext);
+}
+
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+
+SymbolString::SymbolString(const Vector<SymbolShape *> &symbols, S32 fontSize, FontContext fontContext)
+{
+   mSymbols     = symbols;
+   mFontSize    = fontSize;
+   mFontContext = fontContext;
+
+   mWidth = 0;
+
+   for(S32 i = 0; i < mSymbols.size(); i++)
+   {
+      mSymbols[i]->updateWidth(fontSize, fontContext);
+      mWidth += mSymbols[i]->getWidth();
+   }
+}
+
+
+SymbolString::~SymbolString()
+{
+   mSymbols.clear();    // Clean up those pointers
+}
+
+
+S32 SymbolString::getWidth() const
+{ 
+   return mWidth;
+}
+
+
+void SymbolString::renderCenter(const Point &center) const
+{
+   S32 x = (S32)center.x - getWidth() / 2;
+   S32 y = (S32)center.y;
+
+   FontManager::pushFontContext(mFontContext);
+
+   for(S32 i = 0; i < mSymbols.size(); i++)
+   {
+      mSymbols[i]->render(Point(x + mSymbols[i]->getWidth() / 2, y + mFontSize / 2), mFontSize, mFontContext);
+      x += mSymbols[i]->getWidth();
+   }
+
+   FontManager::popFontContext();
+}
+
+
+// Locally defined class, used only here, and only for ensuring objects are cleaned up on exit
+class SymbolHolder
+{
+private:
+   static Vector<SymbolShape *> mKeySymbols;
+   static SymbolGear *mSymbolGear;
+
+public:
+   SymbolHolder() 
+   {
+      if(mKeySymbols.size() == 0)                     // Should always be the case
+         mKeySymbols.resize(LAST_KEYBOARD_KEY + 1);   // Values will be initialized to NULL
+   }
+
+
+   ~SymbolHolder()
+   {
+      mKeySymbols.deleteAndClear();                   // Delete objects created in getSymbol
+      delete mSymbolGear;
+   }
+
+
+   SymbolShape *getSymbol(InputCode inputCode)
+   {
+      // Lazily initialize -- we're unlikely to actually need more than a few of these during a session
+      if(!mKeySymbols[inputCode])
+      {
+         string stuff = InputCodeManager::inputCodeToGlyph(inputCode);
+         if(stuff != "")
+            mKeySymbols[inputCode] = new SymbolKey(stuff);
+         else
+            mKeySymbols[inputCode] = new SymbolText("[" + string(InputCodeManager::inputCodeToString(inputCode)) + "]");
+      }
+
+      return mKeySymbols[inputCode];
+   }
+
+
+   SymbolShape *getSymbolGear()
+   {
+      if(!mSymbolGear)
+         mSymbolGear = new SymbolGear();
+
+      return mSymbolGear;
+   }
+
+};
+
+// Statics used by SymbolHolder
+Vector<SymbolShape *> SymbolHolder::mKeySymbols;      
+SymbolGear *SymbolHolder::mSymbolGear = NULL;
+
+static SymbolHolder symbolHolder;
+
+
+// Static method
+SymbolShape *SymbolString::getControlSymbol(InputCode inputCode)
+{
+   if(InputCodeManager::isKeyboardKey(inputCode))
+      return symbolHolder.getSymbol(inputCode);
+   else
+   { 
+      TNLAssert(false, "Deal with it!");
+      return NULL;      // Certain to crash
+   }
+}
+
+
+SymbolShape *SymbolString::getSymbolGear()
+{
+   return symbolHolder.getSymbolGear();
+}
+
+
+} } // Nested namespace
+
+
