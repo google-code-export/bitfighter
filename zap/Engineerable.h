21c745a68bff1ddef8d7beea4b5de18080d81eaf
watusimoto <watusimoto@bitfighter.org>
2013-07-26 22:02:32 +0200

    Clean up variable names and such

---
+++ zap/Engineerable.h
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+#ifndef ENGINEERABLE_H_
+#define ENGINEERABLE_H_
+
+#include "Point.h"
+#include "tnlNetBase.h"
+
+namespace Zap
+{
+
+// Forward declaraions
+class MountableItem;
+class GridDatabase;
+
+// Things you can build with Engineer
+
+/**
+ * @luaenum EngineerBuildObject(1,2)
+ * The EngineerBuildObject enum represents different things you can build with the engineer module.
+ */
+
+//                Enum                    Enum in Lua            Nice name for docs
+#define ENGINEER_BUILD_OBJECTS_TABLE \
+   ENGR_OBJ(EngineeredTurret,             "Turret",              "Turret"               ) \
+   ENGR_OBJ(EngineeredForceField,         "ForceFieldProjector", "Force Field Projector") \
+   ENGR_OBJ(EngineeredTeleporterEntrance, "Teleporter",          "Teleporter"           ) \
+   ENGR_OBJ(EngineeredTeleporterExit,     "TelerporterExit",     "Teleporter exit point") \
+
+// Define an enum from the first values in EVENT_TABLE
+enum EngineerBuildObjects {
+#define ENGR_OBJ(a, b, c) a,
+    ENGINEER_BUILD_OBJECTS_TABLE
+#undef ENGR_OBJ
+    EngineeredItemCount
+};
+
+
+// These are events used with engineering an object over the client/server protocols
+enum EngineerResponseEvent {
+   EngineerEventTurretBuilt,
+   EngineerEventForceFieldBuilt,
+   EngineerEventTeleporterEntranceBuilt,
+   EngineerEventTeleporterExitBuilt,
+   EngineerEventCount
+};
+
+// This class is sort of an interface for engineer-specific members and methods
+class Engineerable
+{
+protected:
+   bool mEngineered;
+   SafePtr<MountableItem> mResource;
+
+public:
+   Engineerable();           // Constructor
+   virtual ~Engineerable();  // Destructor
+
+   void setEngineered(bool isEngineered);
+   bool isEngineered(); // Was this engineered by a player?
+
+   void setResource(MountableItem *resource);
+   void releaseResource(const Point &releasePos, GridDatabase *database);
+
+   virtual void computeExtent() = 0;  // The object must have extents recomputed before being added
+   virtual void onConstructed() = 0;  // Call this once the object has been added to the game
+};
+
+
+}
+
+
+#endif /* ENGINEERABLE_H_ */
---
+++ zap/oglconsole.h
@@ -0,0 +1,68 @@
+/* oglconsole -- gpl license here */
+
+#ifndef _OGLCONSOLE_H
+#define _OGLCONSOLE_H
+
+#ifdef TNL_OS_MOBILE
+#define BF_NO_CONSOLE
+#endif
+
+#ifndef BF_NO_CONSOLE
+
+/* Opaque to you you lowly user */
+struct _OGLCONSOLE_Console;
+typedef _OGLCONSOLE_Console *OGLCONSOLE_Console;
+
+
+
+#define MAX_CONSOLE_OUTPUT_LENGTH 4096
+
+
+/* Initialize/uninitialize OGLConsole */
+OGLCONSOLE_Console OGLCONSOLE_Create();
+void OGLCONSOLE_Destroy(OGLCONSOLE_Console console);
+void OGLCONSOLE_Quit();
+
+void OGLCONSOLE_ShowConsole();
+void OGLCONSOLE_HideConsole();
+
+/* Set console which has PROGRAMMER focus (not application focus) */
+
+/* This function renders the console */
+void OGLCONSOLE_Draw();
+void OGLCONSOLE_Render(OGLCONSOLE_Console console);
+
+/* Set whether cursor should be visible -- can be used to make cursor blink */
+void OGLCONSOLE_setCursor(int drawCursor);      
+
+/* Handle resize window events */
+void OGLCONSOLE_Reshape();
+
+void OGLCONSOLE_Output(OGLCONSOLE_Console console, const char *s, ...);
+
+/* Register a callback with the console */
+void OGLCONSOLE_EnterKey(void(*cbfun)(OGLCONSOLE_Console console, char *cmd));
+
+/* This function tries to handle the incoming keydown event. In the future there may
+ * be non-SDL analogs for input systems such as GLUT. Returns true if the event
+ * was handled by the console. If console is hidden, no events are handled. */
+int OGLCONSOLE_KeyEvent(int key, int mod);
+int OGLCONSOLE_CharEvent(int unicode);
+
+/* Sets the dimensions of the console in lines and columns of characters. */
+void OGLCONSOLE_SetDimensions(int width, int height);
+
+/* Use this if you want to populate console command history yourself */
+void OGLCONSOLE_AddHistory(OGLCONSOLE_Console console, char *s);
+
+/* Show or hide the console */
+void OGLCONSOLE_SetVisibility(int visible);
+int OGLCONSOLE_GetVisibility();
+
+/* Create the console font */
+int OGLCONSOLE_CreateFont();
+
+#endif // BF_NO_CONSOLE
+
+#endif // _OGLCONSOLE_H
+
