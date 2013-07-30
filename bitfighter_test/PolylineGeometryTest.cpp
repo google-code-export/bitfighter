5a78869e5b2711564644f86449b814805c3f090e
kaen <bkconrad@gmail.com>
2013-07-30 16:17:06 -0700

    add bitfighter-client target to speed up compile times when working with tests

---
+++ bitfighter_test/PolylineGeometryTest.cpp
@@ -0,0 +1,67 @@
+#include "Geometry.h"
+
+#include "gtest/gtest.h"
+
+using namespace Zap;
+class PolylineGeometryTest: public testing::Test
+{
+
+};
+
+TEST_F(PolylineGeometryTest, setGeomTest)
+{
+   Vector<Point> geom(0);
+   geom.push_back(Point(0, 0));
+   geom.push_back(Point(1, 0));
+   geom.push_back(Point(1, 1));
+
+   PolylineGeometry pg;
+   pg.setGeom(geom);
+
+   const Vector<Point> &newGeom = *pg.getOutline();
+
+   // Any points with NaN coordinates should be ignored
+   EXPECT_EQ(newGeom.size(), 3);
+   for(S32 i = 0; i < newGeom.size(); i++)
+   {
+      EXPECT_EQ(geom[i], newGeom[i]);
+   }
+
+   Vector<Point> secondGeom(0);
+   secondGeom.push_back(Point(0, 0));
+   secondGeom.push_back(Point(2, 0));
+   secondGeom.push_back(Point(2, 2));
+   pg.setGeom(secondGeom);
+
+   // Any points with NaN coordinates should be ignored
+   EXPECT_EQ(newGeom.size(), 3);
+   for(S32 i = 0; i < newGeom.size(); i++)
+   {
+      EXPECT_EQ(secondGeom[i], newGeom[i]);
+   }
+}
+
+TEST_F(PolylineGeometryTest, setGeomWithNanPointTest)
+{
+   Vector<Point> geom(0);
+   geom.push_back(Point(0, 0));
+   geom.push_back(Point(1, 0));
+
+   // 0.0 / 0.0 results in NaN rather than a division by zero error
+   F32 nan = 0.0;
+   nan = nan / nan;
+   geom.push_back(Point(nan, nan));  // Visual C++ 2008 won't compile "0.0 / 0.0" (compile error)
+
+   PolylineGeometry pg;
+   pg.setGeom(geom);
+
+   const Vector<Point> &newGeom = *pg.getOutline();
+
+   // Any points with NaN coordinates should be ignored
+   EXPECT_EQ(newGeom.size(), 2);
+   for(S32 i =0; i < newGeom.size(); i++)
+   {
+      // comparing against itself is false with NaN coordinates
+      EXPECT_EQ(newGeom[i], newGeom[i]);
+   }
+}
