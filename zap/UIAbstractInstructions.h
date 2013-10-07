495f4ae2e830d08fa8c942a4777eeef83b47ce3b
watusimoto <watusimoto@bitfighter.org>
2013-10-07 23:35:42 +0200

    Added event handlers for stick added/removed events, simplify stick logic for SDL2+.  Fixes in-game crashing related to sticks, still crashes on exit.

---
+++ zap/UIAbstractInstructions.h
@@ -0,0 +1,78 @@
+//-----------------------------------------------------------------------------------
+//
+// Bitfighter - A multiplayer vector graphics space game
+// Based on Zap demo released for Torque Network Library by GarageGames.com
+//
+// Derivative work copyright (C) 2008-2009 Chris Eykamp
+// Original work copyright (C) 2004 GarageGames.com, Inc.
+// Other code copyright as noted
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful (and fun!),
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//------------------------------------------------------------------------------------
+
+
+#ifndef _UIABSTRACTINSTRUCTIONS_H_
+#define _UIABSTRACTINSTRUCTIONS_H_
+
+
+#include "UI.h"
+#include "SymbolShape.h"
+
+
+namespace Zap {
+
+
+class AbstractInstructionsUserInterface : public UserInterface
+{
+   typedef UserInterface Parent;
+
+public:
+   struct ControlStringsEditor {
+      string command;
+      string binding;
+   };
+
+   static const S32 HeaderFontSize = 20;
+   static const S32 FontSize = 18;
+   static const S32 LineGap = 8;
+
+   static const Color *txtColor;
+   static const Color *keyColor;
+   static const Color *secColor;
+   static const Color *groupHeaderColor;
+
+protected:
+   void render(const char *header, S32 page, S32 pages);
+   void renderConsoleCommands(const UI::SymbolStringSet &instructions, const ControlStringsEditor *cmdList);
+
+   UI::SymbolStringSet 
+         mSpecialKeysInstrLeft,   mSpecialKeysBindingsLeft, 
+         mSpecialKeysInstrRight,  mSpecialKeysBindingsRight,
+         mWallInstr,              mWallBindings;
+
+   void pack(UI::SymbolStringSet &Instrs, UI::SymbolStringSet &Bindings, 
+            const ControlStringsEditor *helpBindings, S32 bindingCount, GameSettings *settings);
+
+public:
+   explicit AbstractInstructionsUserInterface(ClientGame *clientGame);  // Constructor
+   virtual ~AbstractInstructionsUserInterface();                        // Destructor
+};
+
+}
+
+#endif
+
+
