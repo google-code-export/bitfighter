734bd5d9064434146a84a03fa12bd4aab7e1982e
buckyballreaction <buckyballreaction@gmail.com>
2013-10-16 00:16:25 -0600

    Fix potentially fatal bug in building triangulation geometry

---
+++ resource/scripts/levelgen_helper_functions.lua
@@ -0,0 +1,93 @@
+-------------------------------------------------------------------------------
+--
+-- Bitfighter - A multiplayer vector graphics space game
+-- Based on Zap demo released for Torque Network Library by GarageGames.com
+--
+-- Copyright (C) 2008-2009 Chris Eykamp
+-- Other code copyright as noted
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful (and fun!),
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+--
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- These functions will be included with every levelgen script automatically.
+-- Do not tinker with these unless you are sure you know what you are doing!!
+-- And even then, be careful!
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+--
+-- And two more
+--
+function subscribe(event)
+   bf:subscribe(event)
+end   
+
+function unsubscribe(event)
+   bf:unsubscribe(event)
+end  
+
+function globalMsg(message)
+   levelgen:globalMsg(message)
+end   
+
+
+--
+-- Add backwards compatibility for some API changes
+--
+--
+-- Deprecation started in 019
+--
+function levelgen:findObjectById(id)
+    return bf:findObjectById(id)
+end
+
+function levelgen:addItem(object)
+    return bf:addItem(object)
+end
+
+function levelgen:pointCanSeePoint(p1, p2)
+    printDeprecationWarning("levelgen:pointCanSeePoint(p1, p2)", "bf:pointCanSeePoint(p1, p2)")
+    return bf:pointCanSeePoint(p1, p2)
+end
+
+function GameInfo()
+    printDeprecationWarning("GameInfo()", "bf:getGameInfo()")
+    return bf:getGameInfo()
+end
+
+function TeamInfo(index)
+    printDeprecationWarning("TeamInfo(index)", "bf:getGameInfo():getTeam(index)")
+    return bf:getGameInfo():getTeam(index)
+end
+
+--
+-- Deprecation started in 018
+--
+function Point(x, y)
+    printDeprecationWarning("Point(x,y)", "point.new(x,y)")
+    return point.new(x, y)
+end
+
+--
+-- Let the log know that this file was processed correctly
+--
+-- logprint("Loaded levelgen helper functions")
