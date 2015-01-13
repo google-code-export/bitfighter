8810bd3d8289598f7ac7ba3ff35446ee6cadebfb
sam8641 <sam8641@localhost>
2015-01-12 20:52:47 -0600

    Merge, some conflicts on gridDB and config fixed

---
+++ bitfighter_test/LevelFilesForTesting.h
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+// Copyright Chris Eykamp
+// See LICENSE.txt for full copyright information
+//------------------------------------------------------------------------------
+
+#ifndef _LEVEL_FILES_FOR_TESTING_H_
+#define _LEVEL_FILES_FOR_TESTING_H_
+
+#include "LevelSource.h"
+
+#include <string>
+
+namespace Zap
+{
+
+using namespace std;
+using namespace TNL;
+
+string getLevelCode1();
+string getLevelCodeForTestingEngineer1();
+string getLevelCodeForEmptyLevelWithBots(const string &botspec);
+string getLevelCodeForEngineeredItemSnapping();
+string getLevelCodeForEngineeredItemSnapping2();
+
+string getGenericHeader();
+
+pair<Vector<string>, Vector<LevelInfo> > getLevels();
+
+};
+
+#endif
---
+++ zap/InputModeEnum.h
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// Copyright Chris Eykamp
+// See LICENSE.txt for full copyright information
+//------------------------------------------------------------------------------
+
+#ifndef _INPUT_H_
+#define _INPUT_H_
+
+
+namespace Zap
+{
+
+enum InputMode {
+   InputModeKeyboard = 0,
+   InputModeJoystick = 1
+};
+
+};
+
+#endif
